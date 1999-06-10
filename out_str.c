41e860d86e76eeeb701255d1c672c5047332ff71
Paul Zimmermann <Paul.Zimmermann@inria.fr>
1999-06-10 08:58:23 +0000

    function to print a mpfr to a stream. Initial revision.
    [From SVN r12 (trunk)]

---
+++ out_str.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <math.h>
+#include "gmp.h"
+#include "gmp-impl.h"
+#include "mpfr.h"
+
+size_t mpfr_out_str (FILE *stream, int base, size_t n_digits, mpfr_srcptr op,
+		     unsigned char rnd_mode)
+{
+  char *s; size_t l; mp_exp_t e;
+
+  s = mpfr_get_str(NULL, &e, base, n_digits, op, rnd_mode);
+  /* for op=3.1416 we have s = "31416" and e = 1 */
+  
+  l = strlen(s)+1;
+  if (*s == '-') fputc(*s++, stream);
+
+  fputc(*s++, stream); e--; /* writes leading digit */
+  fputc('.', stream);       /* decimal point */
+  fputs(s, stream);         /* rest of mantissa */
+  if (e) {
+    fputc((base>10) ? '@' : 'e', stream); l++;
+    sprintf(s, "%d", e);
+    l += strlen(s);
+    fprintf(stream, "%s", s);
+  }
+
+  return l;
+}
