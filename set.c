cad9de5f909f0e11c988688af6935caa3367e2cb
Paul Zimmermann <Paul.Zimmermann@inria.fr>
1999-06-21 11:24:33 +0000

    now deal with carry from mpfr_round_raw
    [From SVN r111 (trunk)]

--- set.c
+++ set.c
@@ -6,7 +6,19 @@
 void mpfr_set(a, b, rnd_mode) 
 mpfr_ptr a; mpfr_srcptr b; unsigned char rnd_mode;
 {
-  mpfr_round_raw(MANT(a), b->_mp_d, rnd_mode, b->_mp_size, PREC(a));
+  int carry, an, preca = PREC(a), sh; mp_limb_t *ap = MANT(a);
+
+  carry = mpfr_round_raw(ap, b->_mp_d, rnd_mode, b->_mp_size, preca);
   EXP(a) = EXP(b);
+  if (carry) {
+    an = (preca-1)/BITS_PER_MP_LIMB + 1;
+    sh = an * BITS_PER_MP_LIMB - preca;
+    if ((*ap >> sh) & 1) {
+      fprintf(stderr, "unable to round in mpfr_set\n"); exit(1);
+    }
+    mpn_rshift(ap, ap, an, 1);
+    ap[an-1] |= (mp_limb_t) 1 << (BITS_PER_MP_LIMB-1);
+    EXP(a)++;
+  }
   if (SIGN(a) != SIGN(b)) CHANGE_SIGN(a);
 }
