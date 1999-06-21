1d4f6f233fb27d303ce0a485f42a0022d3641468
Guillaume Hanrot <Guillaume.Hanrot@ens-lyon.fr>
1999-06-21 15:07:36 +0000

    Print the rounding mode.
    [From SVN r115 (trunk)]

---
+++ print_rnd_mode.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "gmp.h"
+#include "gmp-impl.h"
+#include "mpfr.h"
+
+char *
+print_rnd_mode(unsigned char rnd_mode)
+{
+  switch (rnd_mode)
+    {
+    case GMP_RNDD: return("GMP_RNDD"); 
+    case GMP_RNDU: return("GMP_RNDU"); 
+    case GMP_RNDN: return("GMP_RNDN"); 
+    case GMP_RNDZ: return("GMP_RNDZ"); 
+    default: return("unknown rounding mode"); 
+    }
+}
