6a5db705f485839daae94587eed5dca0447cfe83
Paul Zimmermann <Paul.Zimmermann@inria.fr>
1999-06-21 11:27:29 +0000

    added one test
    [From SVN r112 (trunk)]

--- tests/tmul_ui.c
+++ tests/tmul_ui.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "gmp.h"
+#include "gmp-impl.h"
 #include "mpfr.h"
 #include "time.h"
 
@@ -10,6 +11,15 @@ main(int argc, char **argv)
   mpfr_t x;
   
   mpfr_init2(x, 53);
+
+  /* checks that result is normalized */
+  mpfr_set_d(x, 6.93147180559945286227e-01, GMP_RNDZ);
+  mpfr_mul_ui(x, x, 1, GMP_RNDZ);
+  if (MANT(x)[PREC(x)/BITS_PER_MP_LIMB] >> (BITS_PER_MP_LIMB-1) == 0) {
+    fprintf(stderr, "Error in mpfr_mul_ui: result not normalized\n");
+    exit(1);
+  }
+
   mpfr_set_d(x, 1.0/3.0, GMP_RNDZ); 
   mpfr_print_raw(x); putchar('\n'); 
   
