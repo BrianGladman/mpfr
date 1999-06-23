a402b1df904d7a2abc3b5c6475a969559d1581a0
Paul Zimmermann <Paul.Zimmermann@inria.fr>
1999-06-23 09:50:41 +0000

    cleaned local variables
    [From SVN r133 (trunk)]

--- mul.c
+++ mul.c
@@ -14,7 +14,7 @@ void mpfr_mul(a, b, c, rnd_mode)
 mpfr_ptr a; mpfr_srcptr b, c; unsigned char rnd_mode;
 {
   unsigned int bn, cn, an, k; int cc;
-  mp_limb_t *ap, *bp=MANT(b), *cp=MANT(c), b1;
+  mp_limb_t *ap=MANT(a), *bp=MANT(b), *cp=MANT(c), *tmp, b1;
   long int sign_product;
   TMP_DECL(marker); 
 
@@ -27,21 +27,20 @@ mpfr_ptr a; mpfr_srcptr b, c; unsigned char rnd_mode;
   cn = (PREC(c)-1)/mp_bits_per_limb+1; /* number of significant limbs of c */
   k = bn+cn; /* effective nb of limbs used by b*c */
   TMP_MARK(marker); 
-  ap = (mp_limb_t*) TMP_ALLOC(k*BYTES_PER_MP_LIMB);
+  tmp = (mp_limb_t*) TMP_ALLOC(k*BYTES_PER_MP_LIMB);
 
-  /* step 1: multiplies two mantissa */
-  if (bn>=cn) b1=mpn_mul(ap, bp, bn, cp, cn);
-  else b1=mpn_mul(ap, cp, cn, bp, bn);
+  /* multiplies two mantissa in temporary allocated space */
+  b1 = (bn>=cn) ? mpn_mul(tmp, bp, bn, cp, cn) : mpn_mul(tmp, cp, cn, bp, bn);
 
-  /* now ap[0]..ap[k-1] contains the product of both mantissa,
-     with ap[k-1]>=2^(mp_bits_per_limb-2) */
+  /* now tmp[0]..tmp[k-1] contains the product of both mantissa,
+     with tmp[k-1]>=2^(mp_bits_per_limb-2) */
   an = (PREC(a)-1)/mp_bits_per_limb+1; /* number of significant limbs of a */
   b1 >>= mp_bits_per_limb-1; /* msb from the product */
 
-  if (b1==0) mpn_lshift(ap, ap, k, 1);
-  cc = mpfr_round_raw(MANT(a), ap, rnd_mode, (sign_product<0) ? k ^ (1<<31) : k, PREC(a));
+  if (b1==0) mpn_lshift(tmp, tmp, k, 1);
+  cc = mpfr_round_raw(ap, tmp, rnd_mode, (sign_product<0) ? k ^ (1<<31) : k, PREC(a));
   if (cc) { /* cc = 1 ==> result is a power of two */
-    MANT(a)[an-1] = (mp_limb_t) 1 << (BITS_PER_MP_LIMB-1);
+    ap[an-1] = (mp_limb_t) 1 << (BITS_PER_MP_LIMB-1);
   }
   EXP(a) = EXP(b) + EXP(c) + b1 - 1 + cc;
   if (sign_product * SIGN(a)<0) CHANGE_SIGN(a);
