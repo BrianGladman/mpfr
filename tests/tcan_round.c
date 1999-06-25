39cea0532cd4f6ce37a7af2b6fcef021277f1f4f
Paul Zimmermann <Paul.Zimmermann@inria.fr>
1999-06-25 16:08:32 +0000

    do not print anything on stdout
    [From SVN r200 (trunk)]

--- tests/tcan_round.c
+++ tests/tcan_round.c
@@ -11,7 +11,7 @@ main()
      bit to zero in case of equal distance */
    mpfr_init2(x, 59);
    mpfr_set_str_raw(x, "-0.10010001010111000011110010111010111110000000111101100111111E663"); 
-   mpfr_print_raw(x); 
-
-   printf("mpfr_can_round(x, 54, Z, Z, 53) = %d\n", mpfr_can_round(x, 54, GMP_RNDZ, GMP_RNDZ, 53)); 
+   if (mpfr_can_round(x, 54, GMP_RNDZ, GMP_RNDZ, 53) != 0) {
+     fprintf(stderr, "Error in mpfr_can_round\n"); exit(1);
+   }
 }
