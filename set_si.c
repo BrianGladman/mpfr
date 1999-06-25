30fba075f090ba5bf1d21aaf00ff7bdfbe5d3a73
Paul Zimmermann <Paul.Zimmermann@inria.fr>
1999-06-25 12:57:10 +0000

    use PREC(x) instead of ABSSIZE(x) to get the number of significant limbs
    [From SVN r183 (trunk)]

--- set_si.c
+++ set_si.c
@@ -7,20 +7,20 @@
 void
 mpfr_set_si(mpfr_ptr x, long i, unsigned char rnd_mode)
 {
-  unsigned long xsize, ai, cnt; 
+  unsigned long xn, ai, cnt; 
 
   if (i==0) { SET_ZERO(x); return; }
-  xsize = ABSSIZE(x);
+  xn = (PREC(x)-1)/BITS_PER_MP_LIMB;
   ai = ABS(i); 
 
   count_leading_zeros(cnt, ai); 
 
-  x -> _mp_d[xsize - 1] = ai << cnt;
+  x -> _mp_d[xn] = ai << cnt;
   /* don't forget to put zero in lower limbs */
-  MPN_ZERO(MANT(x), xsize-1);
+  MPN_ZERO(MANT(x), xn);
   x -> _mp_exp = BITS_PER_MP_LIMB - cnt;
   /* warning: don't change the precision of x! */
-  x -> _mp_size = xsize; if (i < 0) CHANGE_SIGN(x);
+  if (i*SIGN(x) < 0) CHANGE_SIGN(x);
 
   return; 
 }
@@ -28,18 +28,18 @@ mpfr_set_si(mpfr_ptr x, long i, unsigned char rnd_mode)
 void
 mpfr_set_ui(mpfr_ptr x, unsigned long i, unsigned char rnd_mode)
 {
-  unsigned int xsize, cnt; 
+  unsigned int xn, cnt; 
 
   if (i==0) { SET_ZERO(x); return; }
-  xsize = ABSSIZE(x);
+  xn = (PREC(x)-1)/BITS_PER_MP_LIMB;
   count_leading_zeros(cnt, i); 
 
-  x -> _mp_d[xsize - 1] = i << cnt; 
+  x -> _mp_d[xn] = i << cnt; 
   /* don't forget to put zero in lower limbs */
-  MPN_ZERO(MANT(x), xsize-1);
+  MPN_ZERO(MANT(x), xn);
   x -> _mp_exp = BITS_PER_MP_LIMB - cnt;
   /* warning: don't change the precision of x! */
-  x -> _mp_size = xsize; 
+  if (i*SIGN(x) < 0) CHANGE_SIGN(x);
 
   return; 
 }
