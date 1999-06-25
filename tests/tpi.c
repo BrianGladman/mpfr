558ce922c0051d0b224ce9a982517c374d6ee78e
Paul Zimmermann <Paul.Zimmermann@inria.fr>
1999-06-25 09:38:52 +0000

    added test for prec=53 bits, removed output for argc==1
    [From SVN r167 (trunk)]

--- tests/tpi.c
+++ tests/tpi.c
@@ -11,8 +11,11 @@ main(argc, argv) int argc; char *argv[];
   rnd = (argc>2) ? atoi(argv[2]) : GMP_RNDZ;
   mpfr_init2(x, p);
   mpfr_pi(x, rnd);
-  if (argc<=3 || atoi(argv[3])!=0) {
+  if (argc>=2) {
     printf("Pi="); mpfr_out_str(stdout, 10, 0, x, rnd); putchar('\n');
+  }
+  else if (mpfr_get_d(x) != 3.141592653589793116) {
+    fprintf(stderr, "mpfr_pi failed for prec=53\n"); exit(1);
   }
   mpfr_clear(x);
 }
