6c4736ba06107aa799be4a102c187073e61ca257
Paul Zimmermann <Paul.Zimmermann@inria.fr>
1999-06-25 16:30:07 +0000

    do not print anything when no argument
    [From SVN r202 (trunk)]

--- tests/tlog2.c
+++ tests/tlog2.c
@@ -1,9 +1,10 @@
+#include <stdlib.h>
 #include "gmp.h"
 #include "mpfr.h"
 
 /* tlog2 [prec] [rnd] [0 = no print] */
 
-main(argc, argv) int argc; char *argv[];
+int main(argc, argv) int argc; char *argv[];
 {
   mpfr_t x; int p; unsigned char rnd;
 
@@ -11,8 +12,12 @@ main(argc, argv) int argc; char *argv[];
   rnd = (argc>2) ? atoi(argv[2]) : GMP_RNDZ;
   mpfr_init2(x, p);
   mpfr_log2(x, rnd);
-  if (argc<=3 || atoi(argv[3])!=0) {
+  if (argc>=2) {
     printf("log(2)="); mpfr_out_str(stdout, 10, 0, x, rnd); putchar('\n');
   }
+  else if (mpfr_get_d(x) != 6.9314718055994530941e-1)  {
+    fprintf(stderr, "mpfr_log2 failed for prec=53\n"); exit(1);
+  }
   mpfr_clear(x);
+  return 0;
 }
