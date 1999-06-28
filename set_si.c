87eafadc7c4cc9419d80fdfad7595c665aaea658
Guillaume Hanrot <Guillaume.Hanrot@ens-lyon.fr>
1999-06-28 07:37:06 +0000

    Yet another corrected prototype.
    [From SVN r206 (trunk)]

--- set_si.c
+++ set_si.c
@@ -33,7 +33,14 @@ mpfr_set_si(x, i, rnd_mode)
 }
 
 void
+#if __STDC__
 mpfr_set_ui(mpfr_ptr x, unsigned long i, unsigned char rnd_mode)
+#else
+mpfr_set_ui(x, i, rnd_mode)
+     mpfr_ptr x;
+     long i;
+     unsigned char rnd_mode;
+#endif  
 {
   unsigned int xn, cnt; 
 
