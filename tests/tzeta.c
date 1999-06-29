ed2bae401a9c067ddbe48e49aae77b99ee6a144a
Jean-Luc Rémy <Jean-Luc.Remy@loria.fr>
1999-06-29 15:57:01 +0000

    test file for mpfr_zeta
    [From SVN r217 (trunk)]

---
+++ tests/tzeta.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <math.h>
+#include "gmp.h"
+#include "gmp-impl.h"
+#include "longlong.h"
+#include "mpfr.h"
+
+int main()
+{mpfr_t p,result,res_p; int succes; size_t t;
+mpfr_init2(result, 53);
+mpfr_init2(res_p, 53);
+mpfr_init2(p, 53); mpfr_set_d(p, 2.0, GMP_RNDN);
+
+mpfr_zeta(result,p,GMP_RNDN);
+/*printf("%d\n",succes);*/
+printf("Valeur de zeta(2) avec prec=53 et arrondi au plus pres:\n");
+mpfr_print_raw(result);printf("\n");
+t=mpfr_out_str(stdout,10,0,result,GMP_RNDN);printf("\n");
+/*Test de comparaison avec pi^2/6*/
+mpfr_set_prec(p, 67);
+mpfr_pi(p,GMP_RNDN);
+mpfr_mul(p,p,p,GMP_RNDN);
+mpfr_set_ui(res_p,6,GMP_RNDN);
+mpfr_div(p,p,res_p,GMP_RNDN);
+/*mpfr_print_raw(p);printf("\n");
+t=mpfr_out_str(stdout,10,0,p,GMP_RNDN);printf("\n");*/
+succes=mpfr_can_round(p,63,GMP_RNDN,GMP_RNDN,53);
+mpfr_set(res_p,p,GMP_RNDN);
+/*printf("%d\n",succes);*/
+printf("Valeur de pi^2/6 avec prec=53 et arrondi au plus pres:\n");
+mpfr_print_raw(res_p);printf("\n");
+t=mpfr_out_str(stdout,10,0,res_p,GMP_RNDN);printf("\n");
+
+mpfr_clear(p);
+mpfr_clear(result);
+mpfr_clear(res_p);
+return 0;
+}
