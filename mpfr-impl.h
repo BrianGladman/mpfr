3b101e2ce804bd165e3b560823b9c077139eef14
Paul Zimmermann <Paul.Zimmermann@inria.fr>
1999-07-05 10:51:37 +0000

    include file for mpfr developers only
    [From SVN r281 (trunk)]

---
+++ mpfr-impl.h
@@ -0,0 +1,23 @@
+/* generate a random double using the whole range of possible values,
+   including denormalized numbers, NaN, infinities, ... */
+double drand()
+{
+  double d; int *i;
+
+  i = (int*) &d;
+  i[0] = lrand48();
+  i[1] = lrand48();
+  if (lrand48()%2) d=-d; /* generates negative numbers */
+  return d;
+}
+
+/* returns the number of ulp's between a and b */
+int ulp(a,b) double a,b;
+{
+  double eps=1.1102230246251565404e-16; /* 2^(-53) */
+  b = (a-b)/a;
+  if (b>0)
+    return (int) floor(b/eps);
+  else
+    return (int) ceil(b/eps);
+}
