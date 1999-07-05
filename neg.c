182a52082b5449ab5c2cf09198070904b15aa274
Paul Zimmermann <Paul.Zimmermann@inria.fr>
1999-07-05 14:06:18 +0000

    use mpfr_set4 now (no modification of const parameters any more)
    [From SVN r301 (trunk)]

--- neg.c
+++ neg.c
@@ -5,18 +5,15 @@
 
 void 
 #if __STDC__
-mpfr_neg(mpfr_t a, mpfr_t b, unsigned char rnd_mode)
+mpfr_neg(mpfr_ptr a, mpfr_srcptr b, unsigned char rnd_mode)
 #else
 mpfr_neg(a, b, rnd_mode)
-     mpfr_t a; 
-     mpfr_t b; 
+     mpfr_ptr a; 
+     mpfr_srcptr b; 
      unsigned char rnd_mode; 
 #endif
 {
-  CHANGE_SIGN(b);
-  if (a != b) {
-    mpfr_set(a, b, rnd_mode);
-    CHANGE_SIGN(b);
-  }
+  if (a != b) mpfr_set4(a, b, rnd_mode, -SIGN(b));
+  else CHANGE_SIGN(a);
   return; 
 }
