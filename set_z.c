86303ff19542447c162bdef60f855e84635bcaa7
Paul Zimmermann <Paul.Zimmermann@inria.fr>
1999-07-09 18:00:39 +0000

    fixed bug in case sh=0
    [From SVN r361 (trunk)]

--- set_z.c
+++ set_z.c
@@ -14,38 +14,40 @@ mpfr_set_z (f, z, rnd)
      unsigned char rnd;
 #endif
 {
-  int fn, zn, k, dif, sign_z, sh; mp_limb_t *fp = MANT(f), cc, c2;
+  int fn, zn, k, dif, sign_z, sh; mp_limb_t *fp = MANT(f), *zp, cc, c2;
 
   sign_z = mpz_cmp_ui(z,0);
   if (sign_z==0) return (SIZE(f)=0);
-  fn = 1 + (PREC(f)-1)/mp_bits_per_limb;
+  fn = 1 + (PREC(f)-1)/BITS_PER_MP_LIMB;
   zn = SIZ(z);
   dif = zn-fn;
-  count_leading_zeros(k, PTR(z)[zn-1]);
-  EXP(f) = zn*mp_bits_per_limb-k;
+  zp = PTR(z);
+  count_leading_zeros(k, zp[zn-1]);
+  EXP(f) = zn*BITS_PER_MP_LIMB-k;
   if (SIGN(f)*sign_z<0) CHANGE_SIGN(f);
   if (dif>=0) { /* number has to be truncated */
     if (k) {
-      mpn_lshift(fp, PTR(z) + dif, fn, k);
-      if (dif) *fp += PTR(z)[dif-1] >> (mp_bits_per_limb-k);
+      mpn_lshift(fp, zp + dif, fn, k);
+      if (dif) *fp += zp[dif-1] >> (BITS_PER_MP_LIMB-k);
     }
-    else MPN_COPY(fp, PTR(z) + dif, fn);
-    sh = fn*mp_bits_per_limb-PREC(f);
+    else MPN_COPY(fp, zp + dif, fn);
+    sh = fn*BITS_PER_MP_LIMB-PREC(f);
     cc = *fp & (((mp_limb_t)1 << sh) - 1);
     *fp = *fp & ~cc;
     if (rnd==GMP_RNDN) {
       if (sh) c2 = (mp_limb_t)1 << (sh-1);
       else { /* sh=0 */
-	c2 = (mp_limb_t)1 << (mp_bits_per_limb-1);
+	c2 = ((mp_limb_t)1) << (BITS_PER_MP_LIMB-1);
 	dif--;
-	cc = (dif>=0) ? PTR(z)[dif] : 0;
+	cc = (dif>=0) ? ((zp[dif])<<k) : 0;
+	if (dif>0 && k) cc += zp[dif-1] >> (BITS_PER_MP_LIMB-k);
       }
       /* now compares cc to c2 */
       if (cc>c2) { mpfr_add_one_ulp(f); return cc; }
       else if (cc<c2) goto towards_zero;
       else {
 	cc=0;
-	while (dif>0 && (cc=PTR(z)[dif-1])==0) dif--;
+	while (dif>0 && (cc=zp[dif-1])==0) dif--;
 	if (cc) { mpfr_add_one_ulp(f); return cc; }
 	else /* exactly in middle: inexact in both cases */
 	  if (*fp & ((mp_limb_t)1<<sh)) { mpfr_add_one_ulp(f); return 1; }
@@ -55,22 +57,22 @@ mpfr_set_z (f, z, rnd)
     else if ((sign_z>0 && rnd==GMP_RNDU) || (sign_z<0 && rnd==GMP_RNDD)) {
       /* round towards infinity */
       /* result is exact iff all remaining bits are zero */
-      if (dif>0 && cc==0) cc=PTR(z)[--dif]<<k;
-      while (cc==0 && dif>0) cc=PTR(z)[--dif];
+      if (dif>0 && cc==0) cc=zp[--dif]<<k;
+      while (cc==0 && dif>0) cc=zp[--dif];
       if (cc) { mpfr_add_one_ulp(f); return 1; }
       else return 0;
     }
     else { /* round towards zero */
       /* result is exact iff all remaining bits are zero */
     towards_zero:
-      if (cc==0 && dif>0) cc=PTR(z)[--dif]<<k;
-      while (cc==0 && dif>0) cc=PTR(z)[--dif];
+      if (cc==0 && dif>0) cc=zp[--dif]<<k;
+      while (cc==0 && dif>0) cc=zp[--dif];
       return cc;
     }
   }
   else {
-    if (k) mpn_lshift(fp-dif, PTR(z), zn, k);
-    else MPN_COPY(fp-dif, PTR(z), zn);
+    if (k) mpn_lshift(fp-dif, zp, zn, k);
+    else MPN_COPY(fp-dif, zp, zn);
     /* fill with zeroes */
     MPN_ZERO(fp, -dif);
     return 0; /* result is exact */
