a57e61ab33daafed500286f58ecdbc72c72d3d62
Paul Zimmermann <Paul.Zimmermann@inria.fr>
1999-10-07 08:08:48 +0000

    replaced implementation using Goldschmidt's algorithm by that using directly mpn_divrem (previously div3.c)
    [From SVN r372 (trunk)]

--- div.c
+++ div.c
@@ -25,108 +25,221 @@ MA 02111-1307, USA. */
 #include "gmp.h"
 #include "gmp-impl.h"
 #include "mpfr.h"
+#include "longlong.h"
 
-/* q <- n/d using Goldschmidt's iteration
-   returns 0 iff result is exact
- */
-int 
-#if __STDC__
-mpfr_div(mpfr_ptr Q, mpfr_srcptr n, mpfr_srcptr d, unsigned char rnd_mode) 
-#else
-mpfr_div(Q, n, d, rnd_mode) 
-     mpfr_ptr Q; 
-     mpfr_srcptr n; 
-     mpfr_srcptr d; 
-     unsigned char rnd_mode;
-#endif
+/* #define DEBUG */
+
+void
+mpfr_div (mpfr_ptr r, mpfr_srcptr u, mpfr_srcptr v, unsigned char rnd_mode)
 {
-  mpfr_t eps, tmp, one; mp_limb_t cc; mpfr_ptr q;
-  int expd, i, prec, precq, err, maxprec, exact=0;
-  TMP_DECL(marker);
+  mp_srcptr up, vp;
+  mp_ptr rp, tp, tp0, tmp;
+  mp_size_t usize, vsize, rrsize;
+  mp_size_t rsize;
+  mp_size_t sign_quotient;
+  mp_size_t prec, err;
+  mp_limb_t q_limb;
+  mp_exp_t rexp;
+  long k, mult, vn; 
+  unsigned long cc = 0, rw, nw; 
+  char can_round = 0; 
+  TMP_DECL (marker);
 
-  if (FLAG_NAN(n) || FLAG_NAN(d)) { SET_NAN(Q); return 1; }
-
-  if (!NOTZERO(n)) { SET_ZERO(Q); return 0; }
-  if (!NOTZERO(d)) { fprintf(stderr, "division by zero\n"); exit(1); }
-
-  TMP_MARK(marker);
-  q = (mpfr_ptr) TMP_ALLOC(sizeof(__mpfr_struct));
-  mpfr_init2(q, PREC(Q));
+  if (FLAG_NAN(u) || FLAG_NAN(v)) { SET_NAN(r); return; }
   
-  prec = precq = PREC(q);
-  /* maxprec is the maximum number of consecutive 0's or 1's in the quotient */
-  maxprec = PREC(n); if (PREC(d)>maxprec) maxprec=PREC(d);
-  for (i=0;i<2;i++)
-    prec = precq + (int) ceil(log(2.0*ceil(log((double)prec)/log(2.0))+7.0)/
-			   log(2.0));
-  err = prec-precq; /* the error is at most 2^err ulp */
-  /* adjust to use complete limbs: the following formula guarantees we get
-     at least one guard bit */
-  prec = (1 + prec/BITS_PER_MP_LIMB)*BITS_PER_MP_LIMB;
-  mpfr_init2(eps, prec); mpfr_init2(tmp, prec); mpfr_init2(one, prec);
-  do {
-  mpfr_set_prec(q, prec);
-  mpfr_set_prec(eps, prec); mpfr_set_prec(tmp, prec); mpfr_set_prec(one, prec);
-  mpfr_set(q, n, GMP_RNDZ);
-  expd = EXP(d);
-  if (mpfr_cmp_si_2exp(d, SIGN(d), expd-1)==0) {
-    /* d is an exact power of two */
-    if (--expd>=0) mpfr_div_2exp(q, n, expd, rnd_mode);
-    else mpfr_mul_2exp(q, n, -expd, rnd_mode);
-    if (SIGN(d)<0) mpfr_neg(q, q, rnd_mode);
-    mpfr_clear(eps); mpfr_clear(tmp); mpfr_clear(one);
-    mpfr_set(Q, q, rnd_mode);
-    mpfr_clear(q);
-    TMP_FREE(marker);
-    return exact;
-  }
-  mpfr_set_ui(one, 1, GMP_RNDZ); 
-  mpfr_mul_2exp(eps, one, expd, GMP_RNDZ); /* eps = 2^expd */
-  if (SIGN(d)<0) mpfr_add(tmp, eps, d, GMP_RNDZ);
-  else mpfr_sub(tmp, eps, d, GMP_RNDZ);
-  i=0; while (i<ABSSIZE(tmp) && MANT(tmp)[i]==MANT(d)[i]) i++;
-  if (i==ABSSIZE(tmp)) {
-    /* if tmp=abs(d), then eps=2*d whence d is an exact power of two */
-    PREC(q) = precq;
-    mpfr_set(q, n, rnd_mode);
-    EXP(q) -= expd-1;
-    if (SIGN(d)<0) CHANGE_SIGN(q);
-    mpfr_clear(eps); mpfr_clear(tmp); mpfr_clear(one);
-    mpfr_set(Q, q, rnd_mode);
-    mpfr_clear(q);
-    TMP_FREE(marker);
-    return exact;
-  }
-  mpfr_mul_2exp(eps, tmp, -expd, GMP_RNDZ);
-  for (;;) {
-    /* q[n+1] = q[n] * (1 + e[n])
-       Numerically, it's better to compute q + (q*e) than q * (1+e).
-       Rounding towards zero guarantees we get a lower bound of n/d.
-     */
-    mpfr_mul(tmp, q, eps, GMP_RNDZ);
-    mpfr_add(one, q, tmp, GMP_RNDZ);
-    mpfr_set(q, one, GMP_RNDZ);
-    /* e[n+1] = e[n]^2 */
-    mpfr_mul(tmp, eps, eps, GMP_RNDZ);
-    mpfr_set(eps, tmp, GMP_RNDZ);
-    if (-EXP(eps)>prec) break; /* further iterations won't change
-					   q since we round towards zero. */
-  }
-     if (SIGN(d)<0) CHANGE_SIGN(q);
-     cc = mpfr_can_round(q, prec-err, GMP_RNDZ, rnd_mode, precq);
-     if (cc==0) {
-       if (prec>precq+maxprec+err) {
-	 rnd_mode=GMP_RNDN; cc=1; exact=0; /* result is exact */
-       }
-     }
-  } while (cc==0 && (prec += BITS_PER_MP_LIMB));
-  mpfr_round(q, rnd_mode, precq);
-  mpfr_mul_2exp(q, q, -expd, rnd_mode);
-  mpfr_set_prec(q, precq);
-  mpfr_clear(eps); mpfr_clear(tmp); mpfr_clear(one);
-  mpfr_set(Q, q, rnd_mode);
-  mpfr_clear(q);
-  TMP_FREE(marker);
+  usize = (PREC(u) - 1)/BITS_PER_MP_LIMB + 1; 
+  vsize = (PREC(v) - 1)/BITS_PER_MP_LIMB + 1; 
+  sign_quotient = (SIGN(u) == SIGN(v) ? 1 : -1); 
+  prec = PREC(r);
 
-  return exact;
+  if (!NOTZERO(u)) { SET_ZERO(r); return; }
+
+  if (!NOTZERO(v))
+    vsize = 1 / v->_mp_d[vsize - 1];    /* Gestion des infinis ? */
+  
+  if (!NOTZERO(v))
+    {
+      r->_mp_exp = 0;
+      MPN_ZERO(r->_mp_d, r->_mp_size); 
+      return;
+    }
+
+  up = u->_mp_d;
+  vp = v->_mp_d;
+
+#ifdef DEBUG
+      printf("Entering division : "); 
+      for(k = usize - 1; k >= 0; k--) { printf("%lu ", up[k]); }
+      printf(" by "); 
+      for(k = vsize - 1; k >= 0; k--) { printf("%lu ", vp[k]); }
+      printf(".\n"); 
+#endif
+
+  /* Compare the mantissas */
+  mult = mpn_cmp(up, vp, (usize > vsize ? vsize : usize)); 
+  if (mult == 0 && vsize > usize)
+    {
+      vn = vsize - usize; 
+      while (vn >= 0) if (vp[vn--]) { mult = 1; break; }
+      /* On peut diagnostiquer ici pour pas cher le cas u = v */
+    }
+  else { mult = (mult < 0 ? 1 : 0); }
+
+  rsize = (PREC(r) + 3)/BITS_PER_MP_LIMB + 1; 
+  rrsize = PREC(r)/BITS_PER_MP_LIMB + 1;
+  /* Three extra bits are needed in order to get the quotient with enough
+     precision ; take one extra bit for rrsize in order to solve more 
+     easily the problem of rounding to nearest. */
+
+  /* ATTENTION, USIZE DOIT RESTER > A VSIZE !!!!!!!! */
+
+  do
+    {
+      TMP_MARK (marker);
+
+      rexp = u->_mp_exp - v->_mp_exp;
+      
+      err = rsize*BITS_PER_MP_LIMB; 
+      if (rsize < vsize) { err-=2; } 
+      if (rsize < usize) { err--; }
+      if (err > rrsize * BITS_PER_MP_LIMB) 
+	{ err = rrsize * BITS_PER_MP_LIMB; }
+      
+      tp0 = (mp_ptr) TMP_ALLOC ((rsize+rrsize) * BYTES_PER_MP_LIMB);
+      /* fill by zero rrsize low limbs of t */
+      MPN_ZERO(tp0, rrsize); tp = tp0 + rrsize; 
+      tmp = (mp_ptr) TMP_ALLOC (rsize * BYTES_PER_MP_LIMB);
+      rp = (mp_ptr) TMP_ALLOC (rrsize * BYTES_PER_MP_LIMB); 
+
+      if (vsize >= rsize) { 
+	MPN_COPY (tmp, vp + vsize - rsize, rsize);
+      }
+      else { 
+	MPN_COPY (tmp + rsize - vsize, vp, vsize);
+	MPN_ZERO (tmp, rsize - vsize); 
+      }
+
+      if (usize >= rsize) { 
+	MPN_COPY (tp, up + usize - rsize, rsize);
+      }
+      else {
+	MPN_COPY (tp + rsize - usize, up, usize); 
+	MPN_ZERO (tp, rsize - usize); 
+      }
+
+      /* Do the real job */
+ 
+#ifdef DEBUG
+      printf("Dividing : "); 
+      for(k = rsize - 1; k >= 0; k--) { printf("%lu ", tp[k]); }
+      printf(" by "); 
+      for(k = rsize - 1; k >= 0; k--) { printf("%lu ", tmp[k]); }
+      printf(".\n"); 
+#endif
+
+      q_limb = mpn_divrem (rp, 0, tp0, rsize+rrsize, tmp, rsize);
+      tp = tp0; /* location of remainder */
+
+#ifdef DEBUG
+      printf("The result is : \n"); 
+      printf("Quotient : "); 
+      for(k = rrsize - 1; k >= 0; k--) { printf("%lu ", rp[k]); }
+      printf("Remainder : "); 
+      for(k = rsize - 1; k >= 0; k--) { printf("%lu ", tp[k]); }
+      printf("(q_limb = %lu)\n", q_limb); 
+#endif
+      
+      /* msb-normalize the result */
+      
+      if (q_limb)
+	{
+	  count_leading_zeros(k, q_limb); 
+	  mpn_rshift(rp, rp, rrsize, BITS_PER_MP_LIMB - k); 
+	  rp[rrsize - 1] |= (q_limb << k);  
+	  rexp += BITS_PER_MP_LIMB - k; 
+	}
+      else
+	{ 
+	  count_leading_zeros(k, rp[rrsize - 1]); 
+	  if (k) { mpn_lshift(rp, rp, rrsize, k); }
+	  rexp -= k; 
+	}
+      
+      can_round = (mpfr_can_round_raw(rp, rrsize, sign_quotient, err, 
+				     GMP_RNDN, rnd_mode, PREC(r))
+	|| (usize == rsize && vsize == rsize && 
+	    mpfr_can_round_raw(rp, rrsize, sign_quotient, err, 
+			       GMP_RNDZ, rnd_mode, PREC(r)))); 
+
+      /* If we used all the limbs of both the dividend and the divisor, 
+	 then we have the correct RNDZ rounding */
+
+      if (!can_round && (rsize < usize || rsize < vsize)) 
+	{ 
+#ifdef DEBUG
+	  printf("Increasing the precision.\n"); 
+#endif
+	  printf("#"); 
+	  TMP_FREE(marker); 
+	}
+    }
+  while (!can_round && (rsize < usize || rsize < vsize) 
+	 && (rsize++) && (rrsize++)); 
+
+  /* ON PEUT PROBABLEMENT SE DEBROUILLER DES QUE rsize >= vsize */
+  /* MAIS IL FAUT AJOUTER LE BOUT QUI MANQUE DE usize A rsize */
+    
+  if (can_round) 
+    {
+      cc = mpfr_round_raw(rp, rp, err, (sign_quotient == -1 ? 1 : 0),
+			  PREC(r), rnd_mode);  
+      rrsize = (PREC(r) - 1)/BITS_PER_MP_LIMB + 1; 
+    }
+  else
+    /* Use the remainder to find out the correct rounding */
+    /* Note that at this point the division has been done */
+    /* EXACTLY. */
+    if ((rnd_mode == GMP_RNDD && sign_quotient == -1) 
+	|| (rnd_mode == GMP_RNDU && sign_quotient == 1)
+	|| (rnd_mode == GMP_RNDN))
+      {	  
+	/* We cannot round, so that the last bits of the quotient
+	   have to be zero; just look if the remainder is nonzero */
+	k = rsize - 1; 
+	while (k >= 0) { if (tp[k]) break; k--; }
+	if (k >= 0) 
+	  cc = mpn_add_1(rp, rp, rrsize, (mp_limb_t)1 << (BITS_PER_MP_LIMB - 
+					       (PREC(r) & 
+						(BITS_PER_MP_LIMB - 1))));
+	else
+	  if (rnd_mode == GMP_RNDN) /* even rounding */
+	    {
+	      rw = (PREC(r) + 1) & (BITS_PER_MP_LIMB - 1);
+	      if (rw) { rw = BITS_PER_MP_LIMB - rw; nw = 0; } else nw = 1; 
+	      if ((rw ? (rp[nw] >> (rw + 1)) & 1 : 
+		   (rp[nw] >> (BITS_PER_MP_LIMB - 1)) & 1))
+		{
+		  cc = mpn_add_1(rp + nw, rp + nw, rrsize, 
+				 ((mp_limb_t)1) << rw); 
+		}
+	    }
+	/* cas 0111111 */
+      }
+
+  if (sign_quotient != SIGN(r)) { CHANGE_SIGN(r); } 
+  r->_mp_exp = rexp;
+  
+  if (cc) {
+    mpn_rshift(rp, rp, rrsize, 1);
+    rp[rrsize-1] |= (mp_limb_t) 1 << (BITS_PER_MP_LIMB-1);
+    r->_mp_exp++; 
+  }
+    
+  rsize = rrsize; 
+  rrsize = (PREC(r) - 1)/BITS_PER_MP_LIMB + 1;  
+  MPN_COPY(r->_mp_d, rp + rsize - rrsize, rrsize); 
+  MANT(r) [0] &= ~(((mp_limb_t)1 << (BITS_PER_MP_LIMB - 
+		    (PREC(r) & (BITS_PER_MP_LIMB - 1)))) - 1) ; 
+  
+  TMP_FREE (marker);
 }
