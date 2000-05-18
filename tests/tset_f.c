a8dda9f409ff5de26b8f0a89011359c3fb48a09a
Paul Zimmermann <Paul.Zimmermann@inria.fr>
2000-05-18 12:57:08 +0000

    added calls to mpf[r]_clear, and rewrote to avoid memory leaks
    [From SVN r520 (trunk)]

--- tests/tset_f.c
+++ tests/tset_f.c
@@ -32,20 +32,21 @@ MA 02111-1307, USA. */
 int
 main()
 {
-  mpfr_t x; mpf_t y; mpf_t z; unsigned long k, pr; double f;
+  mpfr_t x; mpf_t y, z; unsigned long k, pr; double f;
   
   mpfr_init2(x, 100);
   mpf_init(y); 
+  mpf_init(z);
 
   mpf_set_d(y, 0.0);
   mpfr_set_f(x, y, GMP_RNDN);
 
-  srandom(time(NULL)); 
+  srandom((int)time(NULL));
   mpf_random2(y, 10, 0); 
   mpfr_set_f(x, y, rand() & 3);
 
   /* bug found by Jean-Pierre Merlet on February 3, 2000 */
-  mpfr_set_prec(x, 256); mpf_init2(y, 256);
+  mpfr_set_prec(x, 256); mpf_set_prec(y, 256);
   mpfr_set_machine_rnd_mode(GMP_RNDD);
   mpf_set_str(y, "2033.033", 10);
   mpfr_set_f(x, y, GMP_RNDN);
@@ -60,15 +61,18 @@ main()
     fprintf(stderr, "mpfr_set_f failed for y=-2033.033\n"); exit(1);
   }
 
-  mpf_clear(y); mpfr_clear(x); 
+  mpfr_clear(x);
 
   for (k = 1; k <= 100000; k++)
     {
-      pr = 1 + (rand()&255); 
-      mpf_init2(z, pr);
+      pr = 1 + (rand()&255);
+      mpf_set_prec(z, pr);
       mpf_random2(z, z->_mp_prec, 0);
       mpfr_init2(x, pr);
       mpfr_set_f(x, z, 0);
+      mpfr_clear(x);
     }
+  mpf_clear(y);
+  mpf_clear(z);
   return(0);
 }
