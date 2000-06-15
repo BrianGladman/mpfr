b122707a09f008acf854f3e919038e71ddcc9822
Guillaume Hanrot <Guillaume.Hanrot@ens-lyon.fr>
2000-06-15 10:15:58 +0000

    Added div_2exp to the (very rough) test.
    [From SVN r609 (trunk)]

--- tests/tmul_2exp.c
+++ tests/tmul_2exp.c
@@ -31,19 +31,30 @@ MA 02111-1307, USA. */
 int
 main(argc,argv) int argc; char *argv[];
 {
-  double x, z; mpfr_t w;
+  double x, z; mpfr_t w; unsigned long k; 
 
   mpfr_init2(w, 53); 
 
-  srand48(time(NULL)); 
-  x = drand48(); 
-  mpfr_set_d(w, x, 0);
-  mpfr_mul_2exp(w, w, 10, GMP_RNDZ); 
-  if (x != (z = mpfr_get_d(w)/1024))
-    {
-      fprintf(stderr, "%f != %f\n", x, z); 
-      return (-1); 
-    }
+  for (k = 0; k < 100000; k++) {
+    srand48(time(NULL)); 
+    x = drand48(); 
+    mpfr_set_d(w, x, 0);
+    mpfr_mul_2exp(w, w, 10, GMP_RNDZ); 
+    if (x != (z = mpfr_get_d(w)/1024))
+      {
+	fprintf(stderr, "%f != %f\n", x, z); 
+	return (-1); 
+      }
+    
+    mpfr_set_d(w, x, 0);
+    mpfr_div_2exp(w, w, 10, GMP_RNDZ); 
+    if (x != (z = mpfr_get_d(w)*1024))
+      {
+	fprintf(stderr, "%f != %f\n", x, z); 
+	return (-1); 
+      }
+  }
+
   return (0); 
 }
 
