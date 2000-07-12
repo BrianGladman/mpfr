b491b7ba275d70d18d49bb77c5ea5d0a2c8d6b2c
Paul Zimmermann <Paul.Zimmermann@inria.fr>
2000-07-12 12:50:50 +0000

    replaced 0 by GMP_RNDN
    [From SVN r679 (trunk)]

--- tests/tround.c
+++ tests/tround.c
@@ -31,10 +31,10 @@ int main()
    /* checks that rounds to nearest sets the last
      bit to zero in case of equal distance */
    mpfr_init2(x, 2);
-   mpfr_set_d(x, 5.0, 0);
+   mpfr_set_d(x, 5.0, GMP_RNDN);
    if (mpfr_get_d(x) != 4.0) { printf("Error in tround: got %1.1f instead of 4.0\n",mpfr_get_d(x)); }
 
-   mpfr_set_d(x, 0.00098539467465030839, 0);
+   mpfr_set_d(x, 0.00098539467465030839, GMP_RNDN);
 
    mpfr_set_d(x, 9.84891017624509146344e-01, GMP_RNDU); 
    if (mpfr_get_d(x) != 1.0) { printf("Error in tround: got %f instead of 1.0\n",mpfr_get_d(x)); exit(1); }
