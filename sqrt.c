df7540d5f47fef096c22f7a77e1dd22474aa8d1b
Paul Zimmermann <Paul.Zimmermann@inria.fr>
2000-09-29 12:29:24 +0000

    now uses new code mpn_sqrtrem_new
    [From SVN r763 (trunk)]

--- sqrt.c
+++ sqrt.c
@@ -131,7 +131,7 @@ mpfr_sqrt (r, u, rnd_mode)
       printf(".\n"); 
 #endif
 
-      q_limb = kara_sqrtrem (rp, remp, tmp, rsize);
+      q_limb = mpn_sqrtrem_new (rp, remp, tmp, rsize);
 
 #ifdef DEBUG
       printf("The result is : \n"); 
