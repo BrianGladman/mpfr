7ba6930fe6da48bed981c0155ce9373684c97cdf
Paul Zimmermann <Paul.Zimmermann@inria.fr>
2001-03-08 11:21:31 +0000

    added one test
    [From SVN r1017 (trunk)]

--- tests/tcan_round.c
+++ tests/tcan_round.c
@@ -31,17 +31,30 @@ main (void)
 
    /* checks that rounds to nearest sets the last
      bit to zero in case of equal distance */
-   mpfr_init2(x, 59);
-   mpfr_set_str_raw(x, "-0.10010001010111000011110010111010111110000000111101100111111E663"); 
-   if (mpfr_can_round(x, 54, GMP_RNDZ, GMP_RNDZ, 53) != 0) {
-     fprintf(stderr, "Error in mpfr_can_round\n"); exit(1);
-   }
+   mpfr_init2 (x, 59);
+   mpfr_set_str_raw (x, "-0.10010001010111000011110010111010111110000000111101100111111E663"); 
+   if (mpfr_can_round (x, 54, GMP_RNDZ, GMP_RNDZ, 53) != 0)
+     {
+       fprintf (stderr, "Error (1) in mpfr_can_round\n");
+       exit (1);
+     }
 
-   mpfr_set_str_raw(x, "-Inf"); 
-   if (mpfr_can_round(x, 2000, GMP_RNDZ, GMP_RNDZ, 2000) != 0) { 
-     fprintf(stderr, "Error in mpfr_can_round\n"); exit(1); 
-   }
-   mpfr_clear(x);
+   mpfr_set_str_raw (x, "-Inf"); 
+   if (mpfr_can_round (x, 2000, GMP_RNDZ, GMP_RNDZ, 2000) != 0)
+     {
+       fprintf (stderr, "Error (2) in mpfr_can_round\n");
+       exit (1);
+     }
+
+   mpfr_set_prec (x, 64);
+   mpfr_set_str_raw (x, "0.1011001000011110000110000110001111101011000010001110011000000000");
+   if (mpfr_can_round (x, 65, GMP_RNDN, GMP_RNDN, 54))
+     {
+       fprintf (stderr, "Error (3) in mpfr_can_round\n");
+       exit (1);
+     }
+
+   mpfr_clear (x);
 
    return 0;
 }
