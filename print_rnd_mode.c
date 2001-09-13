c60fb52819c02417f0d71c70c3e78bfb95825f20
Vincent Lefèvre <vincent@vinc17.net>
2001-09-13 12:32:44 +0000

    #include added/removed.
    [From SVN r1202 (trunk)]

--- print_rnd_mode.c
+++ print_rnd_mode.c
@@ -20,9 +20,10 @@ the Free Software Foundation, Inc., 59 Temple Place - Suite 330, Boston,
 MA 02111-1307, USA. */
 
 #include <stdio.h>
+#include <stdlib.h>
 #include "gmp.h"
-#include "mpfr.h"
 #include "gmp-impl.h"
+#include "mpfr.h"
 
 char *
 #if __STDC__
