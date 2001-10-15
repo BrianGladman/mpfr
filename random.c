f46457c1990541b9438fee71108d08415df98c76
Paul Zimmermann <Paul.Zimmermann@inria.fr>
2001-10-15 14:05:10 +0000

    sign was not set
    [From SVN r1252 (trunk)]

--- random.c
+++ random.c
@@ -44,14 +44,18 @@ mpfr_random (x)
 
   mpn_random (xp, xn);
 
-  if (xp[xn - 1] == 0) xp[xn - 1] = 1; 
+  if (xp[xn - 1] == 0)
+    xp[xn - 1] = 1;
   /* since count_leading_zeros doesn't like zeroes */
 
   count_leading_zeros (cnt, xp[xn - 1]); 
-  if (cnt) mpn_lshift (xp, xp, xn, cnt); 
+  if (cnt)
+    mpn_lshift (xp, xp, xn, cnt); 
   MPFR_EXP(x) = -cnt; 
+  if (MPFR_SIGN(x) < 0)
+    MPFR_CHANGE_SIGN(x);
 
-  cnt = xn*BITS_PER_MP_LIMB - prec; 
+  cnt = xn * BITS_PER_MP_LIMB - prec; 
   /* cnt is the number of non significant bits in the low limb */
   xp[0] &= ~((((mp_limb_t) 1) << cnt) - 1);
 }
