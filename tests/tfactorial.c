7a238c37785480f294fdd2f0f47743f6942fe336
Paul Zimmermann <Paul.Zimmermann@inria.fr>
2001-10-25 13:48:28 +0000

    changed stupid test (was computing only 200!)
    [From SVN r1359 (trunk)]

--- tests/tfactorial.c
+++ tests/tfactorial.c
@@ -32,72 +32,81 @@ MA 02111-1307, USA. */
 int
 main (int argc, char *argv[])
 {
-  unsigned int prec, err, yprec, n, p0 = 1, p1 = 100, N = 100, k;
+  unsigned int prec, err, yprec, n, k, zeros;
   mp_rnd_t rnd;
   mpfr_t x, y, z, t;
+  int inexact;
 
   mpfr_init (x);
   mpfr_init (y);
   mpfr_init (z);
   mpfr_init (t);
 
-  k=0;
-  TEST_FUNCTION(y,k,GMP_RNDN);
+  mpfr_factorial (y, 0, GMP_RNDN);
  
-  if(mpfr_cmp_ui(y,1)!=0){ printf("EVALUATION OF FUNCTION IN 0 DO NOT RETUN 1"); exit(1);}
-
-
-
-  /* tsinh prec - perform one random computation with precision prec */
-  if (argc >= 2)
+  if (mpfr_cmp_ui (y, 1))
     {
-      p0 = p1 = atoi (argv[1]);
-      N = 1;
+      printf ("mpfr_factorial(0) does not give 1\n");
+      exit (1);
     }
 
-  for (prec = p0; prec <= p1; prec++)
+  for (prec = 1; prec <= 100; prec++)
     {
       mpfr_set_prec (x, prec);
       mpfr_set_prec (z, prec);
       mpfr_set_prec (t, prec);
       yprec = prec + 10;
+      mpfr_set_prec (y, yprec);
 
-      /*if(prec % 100 == 1)printf("la precision est : %i\n",prec); */
-
-      for (n=0; n<N; n++)
-	{ 
-	 
-	  k=(unsigned int)random(); 
-	 
-	  rnd = random () % 4;
-	  mpfr_set_prec (y, yprec);
-
-	  TEST_FUNCTION (y,200, rnd);
-
-	  err = (rnd == GMP_RNDN) ? yprec + 1 : yprec;
-	  if (mpfr_can_round (y, err, rnd, rnd, prec))
-	    {
-	      mpfr_set (t, y, rnd);
-	      TEST_FUNCTION (z,200, rnd);
-	      if (mpfr_cmp (t, z))
-		{
-		  printf ("results differ for x=");
-		  mpfr_out_str (stdout, 2, prec, x, GMP_RNDN);
-		  printf (" prec=%u rnd_mode=%s\n", prec,
-			  mpfr_print_rnd_mode (rnd));
-		  printf ("   got ");
-		  mpfr_out_str (stdout, 2, prec, z, GMP_RNDN);
-		  putchar ('\n');
-		  printf ("   expected ");
-		  mpfr_out_str (stdout, 2, prec, t, GMP_RNDN);
-		  putchar ('\n');
-		  printf ("   approximation was ");
-		  mpfr_print_raw (y);
-		  putchar ('\n');
-		  exit (1);
-		}
-	    }
-	}
+      for (n=0; n<100; n++)
+	for (rnd=0; rnd<4; rnd++)
+	  { 
+	    inexact = mpfr_factorial (y, n, rnd);
+	    err = (rnd == GMP_RNDN) ? yprec + 1 : yprec;
+	    if (mpfr_can_round (y, err, rnd, rnd, prec))
+	      {
+		mpfr_set (t, y, rnd);
+		inexact = mpfr_factorial (z, n, rnd);
+		/* fact(n) ends with floor(n/2)+floor(n/4)+... zeros */
+		for (k=n/2, zeros=0; k; k >>= 1)
+		  zeros += k;
+		if (MPFR_EXP(y) <= prec + zeros) /* result should be exact */
+		  {
+		    if (inexact)
+		      {
+			fprintf (stderr, "Wrong inexact flag: expected exact\n");
+			exit (1);
+		      }
+		  }
+		else /* result is inexact */
+		  {
+		    if (!inexact)
+		      {
+			fprintf (stderr, "Wrong inexact flag: expected inexact\n");
+			printf ("n=%u prec=%u\n", n, prec);
+			mpfr_print_raw(z); putchar('\n');
+			exit (1);
+		      }
+		  }
+		if (mpfr_cmp (t, z))
+		  {
+		    printf ("results differ for x=");
+		    mpfr_out_str (stdout, 2, prec, x, GMP_RNDN);
+		    printf (" prec=%u rnd_mode=%s\n", prec,
+			    mpfr_print_rnd_mode (rnd));
+		    printf ("   got ");
+		    mpfr_out_str (stdout, 2, prec, z, GMP_RNDN);
+		    putchar ('\n');
+		    printf ("   expected ");
+		    mpfr_out_str (stdout, 2, prec, t, GMP_RNDN);
+		    putchar ('\n');
+		    printf ("   approximation was ");
+		    mpfr_print_raw (y);
+		    putchar ('\n');
+		    exit (1);
+		  }
+	      }
+	  }
     }
 
   mpfr_clear (x);
