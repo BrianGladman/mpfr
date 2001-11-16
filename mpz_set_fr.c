f89391e5b77679345b9f9f603508b6298ac790c6
Paul Zimmermann <Paul.Zimmermann@inria.fr>
2001-11-16 10:05:01 +0000

    added stdio.h
    [From SVN r1502 (trunk)]

--- mpz_set_fr.c
+++ mpz_set_fr.c
@@ -19,6 +19,7 @@ along with the MPFR Library; see the file COPYING.LIB.  If not, write to
 the Free Software Foundation, Inc., 59 Temple Place - Suite 330, Boston,
 MA 02111-1307, USA. */
 
+#include <stdio.h>
 #include "gmp.h"
 #include "mpfr.h"
 #include "mpfr-impl.h"
@@ -28,28 +29,29 @@ MA 02111-1307, USA. */
 /* puts the mantissa of f into z, and returns 'exp' such that f = z * 2^exp
  */
 mp_exp_t
-#if __STDC__
 mpz_set_fr (mpz_ptr z, mpfr_srcptr f)
-#else
-mpz_set_fr (z, f) 
-     mpz_ptr z;
-     mpfr_srcptr f;
-#endif
 {
   int fn, sh;
 
-  /* TODO: semantique de INF, NAN dans ce contexte ; lever une FPE ? */
+  if (MPFR_IS_NAN(f) || MPFR_IS_INF(f))
+    {
+      fprintf (stderr, "Error in mpz_set_fr: input is NaN or Inf\n");
+      exit (1);
+    }
 
-  fn = 1 + (MPFR_PREC(f)-1)/BITS_PER_MP_LIMB;
+  fn = 1 + (MPFR_PREC(f) - 1) / BITS_PER_MP_LIMB;
 
   /* check whether allocated space for z is enough */
-  if (ALLOC(z) < fn) MPZ_REALLOC(z,fn);
+  if (ALLOC(z) < fn)
+    MPZ_REALLOC(z, fn);
 
-  sh = fn*BITS_PER_MP_LIMB - MPFR_PREC(f);
-  if (sh) mpn_rshift(PTR(z), MPFR_MANT(f), fn, sh);
-  else MPN_COPY(PTR(z), MPFR_MANT(f), fn);
+  sh = fn * BITS_PER_MP_LIMB - MPFR_PREC(f);
+  if (sh)
+    mpn_rshift (PTR(z), MPFR_MANT(f), fn, sh);
+  else
+    MPN_COPY (PTR(z), MPFR_MANT(f), fn);
 
   SIZ(z) = fn;
 
-  return MPFR_EXP(f)-MPFR_PREC(f);
+  return MPFR_EXP(f) - MPFR_PREC(f);
 }
