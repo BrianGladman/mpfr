52b4e59424ea8f41507f08a890598b8125e3ed10
Paul Zimmermann <Paul.Zimmermann@inria.fr>
2001-11-16 12:32:48 +0000

    changed Library GPL to Lesser GPL
    [From SVN r1536 (trunk)]

--- tests/thyperbolic.c
+++ tests/thyperbolic.c
@@ -5,15 +5,15 @@ Copyright (C) 2001 Free Software Foundation, Inc.
 This file is part of the MPFR Library.
  
 The MPFR Library is free software; you can redistribute it and/or modify
-it under the terms of the GNU Library General Public License as published
+it under the terms of the GNU Lesser General Public License as published
 by
-the Free Software Foundation; either version 2 of the License, or (at youroption) any later version.
+the Free Software Foundation; either version 2.1 of the License, or (at youroption) any later version.
  
 The MPFR Library is distributed in the hope that it will be useful, but
-WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITYor FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Library General Public
+WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITYor FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public
 License for more details.
  
-You should have received a copy of the GNU Library General Public License
+You should have received a copy of the GNU Lesser General Public License
 along with the MPFR Library; see the file COPYING.LIB.  If not, write to
 the Free Software Foundation, Inc., 59 Temple Place - Suite 330, Boston,
 MA 02111-1307, USA. */
