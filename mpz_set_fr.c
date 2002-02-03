4715d3a4f372f730ea7526079b49d03eab0e22eb
Vincent Lefèvre <vincent@vinc17.net>
2002-02-03 02:59:44 +0000

    For the value 0, return __mpfr_emin instead of 0.
    [From SVN r1679 (trunk)]

--- mpz_set_fr.c
+++ mpz_set_fr.c
@@ -26,7 +26,15 @@ MA 02111-1307, USA. */
 #include "mpfr-impl.h"
 
 /* puts the mantissa of f into z, and returns 'exp' such that f = z * 2^exp
+ *
+ * 0 doesn't have an exponent, therefore the returned exponent in this case
+ * isn't really important. We choose to return __mpfr_emin because
+ *   1) it is in the exponent range [__mpfr_emin,__mpfr_emax],
+ *   2) the smaller a number is (in absolute value), the smaller its
+ *      exponent is. In other words, the f -> exp function is monotonous
+ *      on nonnegative numbers.
  */
+
 mp_exp_t
 mpz_set_fr (mpz_ptr z, mpfr_srcptr f)
 {
@@ -38,7 +46,7 @@ mpz_set_fr (mpz_ptr z, mpfr_srcptr f)
   if (MPFR_IS_ZERO(f))
     {
       mpz_set_ui (z, 0);
-      return 0;
+      return __mpfr_emin;
     }
 
   fn = 1 + (MPFR_PREC(f) - 1) / BITS_PER_MP_LIMB;
