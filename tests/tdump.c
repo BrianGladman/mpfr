1c403d6b7671926dc4d096c2ba25bef587a0c4d4
Paul Zimmermann <Paul.Zimmermann@inria.fr>
2002-03-26 09:40:54 +0000

    added 2002 in copyright line
    [From SVN r1779 (trunk)]

--- tests/tdump.c
+++ tests/tdump.c
@@ -1,6 +1,6 @@
 /* Test file for mpfr_dump.
 
-Copyright 2000, 2001 Free Software Foundation, Inc.
+Copyright 2000, 2001, 2002 Free Software Foundation, Inc.
 
 This file is part of the MPFR Library.
 
