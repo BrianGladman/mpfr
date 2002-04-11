21cb5842f2fd8bf45a268bee8fa80b7232011e4d
Vincent Lefèvre <vincent@vinc17.net>
2002-04-11 13:00:43 +0000

    #include ordering changed for Windows + Cygwin 32.
    [From SVN r1856 (trunk)]

--- get_d.c
+++ get_d.c
@@ -21,6 +21,9 @@ the Free Software Foundation, Inc., 59 Temple Place - Suite 330, Boston,
 MA 02111-1307, USA. */
 
 #include <float.h>
+#ifndef NO_MATH_DEFS
+#include <math.h>
+#endif
 #include "gmp.h"
 #include "gmp-impl.h"
 #include "longlong.h"
