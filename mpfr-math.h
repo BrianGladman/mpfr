9406876f73d9da9fe815e26a83206f78ed600103
Kevin Ryde <user42@zip.com.au>
2002-08-22 00:47:22 +0000

    (_MPFR_NAN_BYTES, _MPFR_INFP_BYTES, _MPFR_INFM_BYTES): Use HAVE_DOUBLE_IEEE_LITTLE_ENDIAN etc to select endianness, not a big block of #ifdefs.
    [From SVN r2014 (trunk)]

--- mpfr-math.h
+++ mpfr-math.h
@@ -19,43 +19,16 @@ along with the MPFR Library; see the file COPYING.LIB.  If not, write to
 the Free Software Foundation, Inc., 59 Temple Place - Suite 330, Boston,
 MA 02111-1307, USA. */
 
-#if defined (_LITTLE_ENDIAN) || defined (__LITTLE_ENDIAN__)		\
- || defined (__alpha)							\
- || (defined (__arm__) && (defined (__ARMWEL__) || defined (__linux__)))\
- || defined (__clipper__)						\
- || defined (__cris)							\
- || defined (__i386__)							\
- || defined (__i860__)							\
- || defined (__i960__)							\
- || defined (__ia64)							\
- || defined (MIPSEL) || defined (_MIPSEL)				\
- || defined (__ns32000__)						\
- || defined (__WINNT) || defined (_WIN32)
+#if HAVE_DOUBLE_IEEE_LITTLE_ENDIAN || HAVE_DOUBLE_IEEE_LITTLE_SWAPPED
 #define _MPFR_NAN_BYTES  { 0, 0, 0xc0, 0x7f }
 #define _MPFR_INFP_BYTES { 0, 0, 0x80, 0x7f }
 #define _MPFR_INFM_BYTES { 0, 0, 0x80, 0xff }
-#else
-#if defined (_BIG_ENDIAN) || defined (__BIG_ENDIAN__)			\
- || defined (__mc68000__) || defined (__mc68020__) || defined (__m68k__)\
- || defined (mc68020)                                                   \
- || defined (__a29k__) || defined (_AM29K)				\
- || defined (__arm__)							\
- || (defined (__convex__) && defined (_IEEE_FLOAT_))			\
- || defined (_CRAYMPP) || defined (_CRAYIEEE)				\
- || defined (__i370__) || defined (__mvs__)				\
- || defined (__m88000__)						\
- || defined (MIPSEB) || defined (_MIPSEB)				\
- || defined (__hppa) || defined (__hppa__)				\
- || defined (__pyr__)							\
- || defined (__ibm032__)						\
- || defined (_IBMR2) || defined (_ARCH_PPC)				\
- || defined (__sh__)							\
- || defined (__sparc) || defined (sparc) || defined (__sparc__)		\
- || defined (__we32k__)
+#endif
+
+#if HAVE_DOUBLE_IEEE_BIG_ENDIAN
 #define _MPFR_NAN_BYTES  { 0x7f, 0xc0, 0, 0 }
 #define _MPFR_INFP_BYTES { 0x7f, 0x80, 0, 0 }
 #define _MPFR_INFM_BYTES { 0xff, 0x80, 0, 0 }
-#endif
 #endif
 
 #ifdef NAN
