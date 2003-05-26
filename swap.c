cc5fafcf21df9a558bae5bd6c2cbc40e355a7242
Vincent Lefèvre <vincent@vinc17.net>
2003-05-26 13:20:54 +0000

    Function mpfr_swap rewritten using memcpy.
    [From SVN r2306 (trunk)]

--- swap.c
+++ swap.c
@@ -19,36 +19,24 @@ along with the MPFR Library; see the file COPYING.LIB.  If not, write to
 the Free Software Foundation, Inc., 59 Temple Place - Suite 330, Boston,
 MA 02111-1307, USA. */
 
+#include <string.h>
 #include "gmp.h"
-#include "gmp-impl.h"
 #include "mpfr.h"
-#include "mpfr-impl.h"
+
+/*
+ * We now use memcpy instead of copying the structure field by field.
+ * Thus we avoid copying values that may have trap representations,
+ * and if we change the structure in the future, this will still be
+ * valid. Also, there should not be any noticeable speed change in
+ * practice.
+ */
 
 void
 mpfr_swap (mpfr_ptr u, mpfr_ptr v)
 {
-  mp_ptr up, vp;
-  mp_size_t usize, vsize;
-  mp_prec_t uprec, vprec;
-  mp_exp_t  uexp, vexp;
+  mpfr_t tmp;
 
-  uprec = MPFR_PREC(u);
-  vprec = MPFR_PREC(v);
-  MPFR_PREC(v) = uprec;
-  MPFR_PREC(u) = vprec;
-
-  usize = MPFR_SIZE(u);
-  vsize = MPFR_SIZE(v);
-  MPFR_SIZE(v) = usize;
-  MPFR_SIZE(u) = vsize;
-
-  uexp = MPFR_GET_EXP (u);
-  vexp = MPFR_GET_EXP (v);
-  MPFR_SET_EXP (v, uexp);
-  MPFR_SET_EXP (u, vexp);
-
-  up = MPFR_MANT(u);
-  vp = MPFR_MANT(v);
-  MPFR_MANT(v) = up;
-  MPFR_MANT(u) = vp;
+  memcpy (tmp, u, sizeof (mpfr_t));
+  memcpy (u, v, sizeof (mpfr_t));
+  memcpy (v, tmp, sizeof (mpfr_t));
 }
