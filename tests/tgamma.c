7b441bacb782f40b890ca7d3cf8dfb7f95cb23ed
Paul Zimmermann <Paul.Zimmermann@inria.fr>
2003-06-10 08:43:05 +0000

    made test faster
    [From SVN r2321 (trunk)]

--- tests/tgamma.c
+++ tests/tgamma.c
@@ -69,7 +69,7 @@ main (void)
       mpfr_set_prec (z, prec);
       yprec = prec + 10;
 
-      for (n = 0; n < 10; n++)
+      for (n = 0; n < 4; n++)
 	{
 	  mpfr_random (x);
 	  rnd = random () % 4;
