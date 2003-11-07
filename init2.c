074341ed3e35b94d30fac6b3c561e08d3607a13a
Vincent Lefèvre <vincent@vinc17.net>
2003-11-07 13:25:18 +0000

    Initialize the exp field.
    [From SVN r2538 (trunk)]

--- init2.c
+++ init2.c
@@ -1,6 +1,6 @@
 /* mpfr_init2 -- initialize a floating-point number with given precision
 
-Copyright 2001, 2002 Free Software Foundation, Inc.
+Copyright 2001, 2002, 2003 Free Software Foundation, Inc.
 
 This file is part of the MPFR Library.
 
@@ -38,8 +38,10 @@ mpfr_init2 (mpfr_ptr x, mpfr_prec_t p)
 
   xsize = (mp_size_t) ((p - 1) / BITS_PER_MP_LIMB) + 1;
   tmp   = (mp_ptr) (*__gmp_allocate_func)(MPFR_ALLOC_SIZE(xsize));
-  
+
   MPFR_PREC(x) = p;
+  MPFR_EXP (x) = MPFR_EXP_INVALID; /* make sure that the exp field has a
+                                      valid value in the C point of view */
   MPFR_SET_POS(x);   /* Set a sign */
   MPFR_SET_MANT_PTR(x, tmp);
   MPFR_SET_ALLOC_SIZE(x, xsize);
