cd9fdd998860a5554de43d0bdf199fdc9650eb38
Patrick Pélissier <patrick.pelissier@gmail.com>
2004-02-17 12:11:49 +0000

    Add 2004 in copyright line.
    [From SVN r2750 (trunk)]

--- round_raw_generic.c
+++ round_raw_generic.c
@@ -1,6 +1,6 @@
 /* mpfr_round_raw_generic -- Generic rounding function
 
-Copyright 1999, 2000, 2001, 2002, 2003 Free Software Foundation, Inc.
+Copyright 1999, 2000, 2001, 2002, 2003, 2004 Free Software Foundation, Inc.
 
 This file is part of the MPFR Library.
 
