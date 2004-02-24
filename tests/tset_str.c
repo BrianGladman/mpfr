50df1d139e9185c3a2f97855b2a8daffc47b36fa
Patrick Pélissier <patrick.pelissier@gmail.com>
2004-02-24 09:12:03 +0000

    Improve test coverage.
    [From SVN r2797 (trunk)]

--- tests/tset_str.c
+++ tests/tset_str.c
@@ -480,6 +480,12 @@ main (int argc, char *argv[])
   MPFR_ASSERTN(mpfr_cmp_ui (x, 1) == 0 && ret == -1);
   ret = mpfr_set_str (x, "1p10toto", 16, GMP_RNDN);
   MPFR_ASSERTN(mpfr_cmp_ui (x, 1) == 0 && ret == -1);
+  ret = mpfr_set_str (x, "", 16, GMP_RNDN);
+  MPFR_ASSERTN(mpfr_cmp_ui (x, 1) == 0 && ret == -1);
+  ret = mpfr_set_str (x, "+", 16, GMP_RNDN);
+  MPFR_ASSERTN(mpfr_cmp_ui (x, 1) == 0 && ret == -1);
+  ret = mpfr_set_str (x, "-", 16, GMP_RNDN);
+  MPFR_ASSERTN(mpfr_cmp_ui (x, 1) == 0 && ret == -1);
   ret = mpfr_set_str (x, "this_is_an_invalid_number_in_base_36", 36, GMP_RNDN);
   MPFR_ASSERTN(mpfr_cmp_ui (x, 1) == 0 && ret == -1);
   mpfr_set_prec (x, 135);
