01ef1a4ed969d08f443457c175ea5f6f4e30dc5f
Patrick Pélissier <patrick.pelissier@gmail.com>
2005-01-04 15:04:18 +0000

    new version (completely rewritten) by Paul Zimmermann.
    [From SVN r3175 (trunk)]

--- const_pi.c
+++ const_pi.c
@@ -1,6 +1,6 @@
 /* mpfr_const_pi -- compute Pi
 
-Copyright 1999, 2000, 2001, 2002, 2003, 2004 Free Software Foundation.
+Copyright 1999, 2000, 2001, 2002, 2003, 2004, 2005 Free Software Foundation.
 
 This file is part of the MPFR Library.
 
@@ -19,208 +19,139 @@ along with the MPFR Library; see the file COPYING.LIB.  If not, write to
 the Free Software Foundation, Inc., 59 Temple Place - Suite 330, Boston,
 MA 02111-1307, USA. */
 
-#define MPFR_NEED_LONGLONG_H
 #include "mpfr-impl.h"
 
-static int mpfr_aux_pi (mpfr_ptr, mpz_srcptr, long, int);
-static int mpfr_pi_machin3 (mpfr_ptr, mp_rnd_t);
+/* Time: mpfr_const_pi2 is this new version.
+   norroy% time ./pi2 1000000
+   mpfr_const_pi took 14110ms
+   mpfr_const_pi2 took 4730ms
 
-#define A
-#define A1 1
-#define A2 2
-#undef B
-#define C
-#define C1 3
-#define C2 2
-#define GENERIC mpfr_aux_pi
-#define R_IS_RATIONAL
-#define NO_FACTORIAL
-#include "generic.c"
+   norroy% time ./pi2 2000000
+   mpfr_const_pi took 39390ms
+   mpfr_const_pi2 took 13140ms
 
-
-static int
-mpfr_pi_machin3 (mpfr_ptr mylog, mp_rnd_t rnd_mode)
-{
-  mp_prec_t prec, logn, prec_x;
-  mp_prec_t prec_i_want;
-  mpfr_t tmp1, tmp2, result, tmp3, tmp4, tmp5, tmp6;
-  mpz_t cst;
-  int inex = 0;  /* here, 0 means not set */
-
-  prec_i_want = MPFR_PREC (mylog);
-  logn = MPFR_INT_CEIL_LOG2 (prec_i_want);
-  prec_x = prec_i_want + logn;
-  mpz_init (cst);
-  while (!inex)
-    {
-      prec = MPFR_INT_CEIL_LOG2 (prec_x);
-
-      mpfr_init2(tmp1, prec_x);
-      mpfr_init2(tmp2, prec_x);
-      mpfr_init2(tmp3, prec_x);
-      mpfr_init2(tmp4, prec_x);
-      mpfr_init2(tmp5, prec_x);
-      mpfr_init2(tmp6, prec_x);
-      mpfr_init2(result, prec_x);
-      mpz_set_si(cst, -1);
-
-      mpfr_aux_pi(tmp1, cst, 268L*268L, prec - 4);
-      mpfr_div_ui(tmp1, tmp1, 268, GMP_RNDD);
-      mpfr_mul_ui(tmp1, tmp1, 61, GMP_RNDD);
-
-      mpfr_aux_pi(tmp2, cst, 343L*343L, prec - 4);
-      mpfr_div_ui(tmp2, tmp2, 343, GMP_RNDD);
-      mpfr_mul_ui(tmp2, tmp2, 122, GMP_RNDD);
-
-      mpfr_aux_pi(tmp3, cst, 557L*557L, prec - 4);
-      mpfr_div_ui(tmp3, tmp3, 557, GMP_RNDD);
-      mpfr_mul_ui(tmp3, tmp3, 115, GMP_RNDD);
-
-      mpfr_aux_pi(tmp4, cst, 1068L*1068L, prec - 4);
-      mpfr_div_ui(tmp4, tmp4, 1068, GMP_RNDD);
-      mpfr_mul_ui(tmp4, tmp4, 32, GMP_RNDD);
-
-      mpfr_aux_pi(tmp5, cst, 3458L*3458L, prec - 4);
-      mpfr_div_ui(tmp5, tmp5, 3458, GMP_RNDD);
-      mpfr_mul_ui(tmp5, tmp5, 83, GMP_RNDD);
-
-      mpfr_aux_pi(tmp6, cst, 27493L*27493L, prec - 4);
-      mpfr_div_ui(tmp6, tmp6, 27493, GMP_RNDD);
-      mpfr_mul_ui(tmp6, tmp6, 44, GMP_RNDD);
-
-      mpfr_add(result, tmp1, tmp2, GMP_RNDD);
-      mpfr_add(result, result, tmp3, GMP_RNDD);
-      mpfr_sub(result, result, tmp4, GMP_RNDD);
-      mpfr_add(result, result, tmp5, GMP_RNDD);
-      mpfr_add(result, result, tmp6, GMP_RNDD);
-      mpfr_mul_2ui(result, result, 2, GMP_RNDD);
-      mpfr_clear(tmp1);
-      mpfr_clear(tmp2);
-      mpfr_clear(tmp3);
-      mpfr_clear(tmp4);
-      mpfr_clear(tmp5);
-      mpfr_clear(tmp6);
-      if (mpfr_can_round (result, prec_x - 5, GMP_RNDD, GMP_RNDZ,
-                          prec_i_want + (rnd_mode == GMP_RNDN)))
-        {
-          inex = mpfr_set (mylog, result, rnd_mode);
-          MPFR_ASSERTN (inex != 0);
-        }
-      else
-        {
-          prec_x += logn;
-        }
-      mpfr_clear (result);
-    }
-  mpz_clear (cst);
-  return inex;
-}
-
-/*
-Set x to the value of Pi to precision MPFR_PREC(x) rounded to direction rnd_mode.
-Use the formula giving the binary representation of Pi found by Simon Plouffe
-David Bailey and Peter Borwein.
-
-                   infinity    4         2         1         1
-                    -----   ------- - ------- - ------- - -------
-                     \      8 n + 1   8 n + 4   8 n + 5   8 n + 6
-              Pi =    )     -------------------------------------
-                     /                         n
-                    -----                    16
-                    n = 0
-
-i.e. Pi*16^N = S(N) + R(N) where
-S(N) = sum(16^(N-n)*(4/(8*n+1)-2/(8*n+4)-1/(8*n+5)-1/(8*n+6)), n=0..N-1)
-R(N) = sum((4/(8*n+1)-2/(8*n+4)-1/(8*n+5)-1/(8*n+6))/16^(n-N), n=N..infinity)
-
-Let f(n) = 4/(8*n+1)-2/(8*n+4)-1/(8*n+5)-1/(8*n+6), we can show easily that
-f(n) < 15/(64*n^2), so R(N) < sum(15/(64*n^2)/16^(n-N), n=N..infinity)
-                            < 15/64/N^2*sum(1/16^(n-N), n=N..infinity)
-			    = 1/4/N^2
-
-Now let S'(N) = sum(floor(16^(N-n)*(120*n^2+151*n+47),
-  (512*n^4+1024*n^3+712*n^2+194*n+15)), n=0..N-1)
-
-S(N)-S'(N) <= sum(1, n=0..N-1) = N
-
-so Pi*16^N-S'(N) <= N+1 (as 1/4/N^2 < 1)
-*/
+   norroy% time ./pi2 5000000
+   mpfr_const_pi took 252120ms
+   mpfr_const_pi2 took 51780ms */
 
 int
 (mpfr_const_pi) (mpfr_ptr x, mp_rnd_t rnd_mode)
 {
-  mp_prec_t N, oldN, n;
-  mpfr_prec_t prec;
-  mpz_t pi, num, den, d3, d2, tmp;
-  int inex;
-  MPFR_SAVE_EXPO_DECL (expo);
+  mpfr_t a, A, B, D, S;
+  mp_prec_t px, p, cancel, k, kmax;
+  int inex, ok;
 
-  MPFR_SAVE_EXPO_MARK (expo);
+  px = MPFR_PREC (x);
 
-  prec = MPFR_PREC(x);
+  /* we need 9*2^kmax - 4 >= px+2*kmax+8 */
+  for (kmax = 2; ((px + 2 * kmax + 12) / 9) >> kmax; kmax ++);
 
-  if (prec < 20000)
-    {
-      /* need to recompute */
-      N=1;
-      do {
-	oldN = N;
-	N = (prec+3)/4 + MPFR_INT_CEIL_LOG2 (N + 1);
-      } while (N != oldN);
-      mpz_init(pi);
-      mpz_init(num);
-      mpz_init(den);
-      mpz_init(d3);
-      mpz_init(d2);
-      mpz_init(tmp);
-      mpz_set_ui(pi, 0);
-      mpz_set_ui(num, 16); /* num(-1) */
-      mpz_set_ui(den, 21); /* den(-1) */
-      mpz_set_si(d3, -2454);
-      mpz_set_ui(d2, 14736);
-      /* invariants: num=120*n^2+151*n+47,
-         den=512*n^4+1024*n^3+712*n^2+194*n+15
-         d3 = 2048*n^3+400*n-6, d2 = 6144*n^2-6144*n+2448
-      */
-      for (n=0; n<N; n++)
-        {
-          /* num(n)-num(n-1) = 240*n+31 */
-          mpz_add_ui(num, num, 240*n+31); /* no overflow up to MPFR_PREC=71M */
-          /* d2(n) - d2(n-1) = 12288*(n-1) */
-          if (n>0)
-            mpz_add_ui(d2, d2, 12288*(n-1));
-          else
-            mpz_sub_ui(d2, d2, 12288);
-          /* d3(n) - d3(n-1) = d2 */
-          mpz_add(d3, d3, d2);
-          /* den(n)-den(n-1) = 2048*n^3 + 400n - 6 = d3 */
-          mpz_add(den, den, d3);
-          mpz_mul_2exp(tmp, num, 4*(N-n));
-          mpz_fdiv_q(tmp, tmp, den);
-          mpz_add(pi, pi, tmp);
-        }
-      inex = mpfr_set_z (x, pi, rnd_mode);
-#ifdef WANT_ASSERT
+  p = px + 2 * kmax + 14; /* guarantees no recomputation for px <= 10000 */
+
+  do {
+    p += kmax;
+    
+    mpfr_init2 (a, p);
+    mpfr_init2 (A, p);
+    mpfr_init2 (B, p);
+    mpfr_init2 (D, p);
+    mpfr_init2 (S, p);
+    
+    mpfr_set_ui (a, 1, GMP_RNDN);          /* a = 1 */
+    mpfr_set_ui (A, 1, GMP_RNDN);          /* A = a^2 = 1 */
+    mpfr_set_ui_2exp (B, 1, -1, GMP_RNDN); /* B = b^2 = 1/2 */
+    mpfr_set_ui_2exp (D, 1, -2, GMP_RNDN); /* D = 1/4 */
+    
+#define b B
+#define ap a
+#define Ap A
+#define Bp B
+    for (k = 0, cancel = 0; ; k++)
       {
-	mpfr_t y;
-	mpfr_init2 (y, mpfr_get_prec(x));
-	mpz_add_ui (pi, pi, N+1);
-	mpfr_set_z (y, pi, rnd_mode);
-	MPFR_ASSERTN (mpfr_cmp (x, y) == 0);
-	mpfr_clear (y);
+	mpfr_add (S, A, B, GMP_RNDN);
+	mpfr_div_2exp (S, S, 2, GMP_RNDN); /* exact */
+	mpfr_sqrt (b, B, GMP_RNDN);
+	mpfr_add (ap, a, b, GMP_RNDN);
+	mpfr_div_2exp (ap, ap, 1, GMP_RNDN); /* exact */
+	mpfr_mul (Ap, ap, ap, GMP_RNDN);
+	mpfr_sub (Bp, Ap, S, GMP_RNDN);
+	mpfr_mul_2exp (Bp, Bp, 1, GMP_RNDN);
+	mpfr_sub (S, Ap, Bp, GMP_RNDN);
+	cancel = mpfr_cmp_ui (S, 0) ? -mpfr_get_exp(S) : p;
+	/* MPFR_ASSERTN (cancel >= px || cancel >= 9 * (1 << k) - 4); */
+	mpfr_mul_2exp (S, S, k, GMP_RNDN);
+	mpfr_sub (D, D, S, GMP_RNDN);
+	/* stop when |A_k - B_k| <= 2^(k-p) i.e. cancel >= p-k */
+	if (cancel + k >= p)
+	  break;
       }
-#endif
-      MPFR_SET_EXP (x, MPFR_GET_EXP(x) - 4*N);
-      mpz_clear(pi);
-      mpz_clear(num);
-      mpz_clear(den);
-      mpz_clear(d3);
-      mpz_clear(d2);
-      mpz_clear(tmp);
-    }
-  else
-    inex = mpfr_pi_machin3 (x, rnd_mode);
+#undef b
+#undef ap
+#undef Ap
+#undef Bp
 
-  MPFR_SAVE_EXPO_FREE (expo);
-  return mpfr_check_range(x, inex, rnd_mode);
+      mpfr_div (A, B, D, GMP_RNDN);
+
+      /* MPFR_ASSERTN(p >= 2 * k + 8); */
+      if ((ok = mpfr_can_round (A, p - 2 * k - 8, GMP_RNDN, GMP_RNDZ,
+                                px + (rnd_mode == GMP_RNDN))))
+        inex = mpfr_set (x, A, rnd_mode);
+      
+      mpfr_clear (a);
+      mpfr_clear (A);
+      mpfr_clear (B);
+      mpfr_clear (D);
+      mpfr_clear (S);
+  } while (ok == 0);
+
+  return inex;
 }
+
+/*
+ int
+main (int argc, char *argv[])
+{
+  int n = atoi (argv[1]);
+  int k = (argc > 2) ? atoi (argv[2]) : 1;
+  int i;
+  mpfr_t x, y;
+  int st;
+  FILE *fp;
+
+  mpfr_init2 (x, n);
+  mpfr_init2 (y, n);
+
+#if 1
+  st = cputime ();
+  for (i = 0; i < k; i++)
+    (mpfr_const_pi) (x, GMP_RNDN);
+  printf ("mpfr_const_pi took %dms\n", cputime () - st);
+#endif
+
+  st = cputime ();
+  for (i = 0; i < k; i++)
+    mpfr_const_pi2 (y, GMP_RNDN);
+  printf ("mpfr_const_pi2 took %dms\n", cputime () - st);
+
+  fp = fopen ("pi", "w");
+  st = cputime ();
+  fprintf (fp, "pi2:=");
+  mpfr_out_str (fp, 10, 0, y, GMP_RNDZ);
+  fprintf (fp, ":\n");
+  printf ("output took %dms\n", cputime () - st);
+  fclose (fp);
+  
+#if 1
+  if (mpfr_cmp (x, y))
+    {
+      fprintf (stderr, "results differ\n");
+      exit (1);
+    }
+#endif
+
+  mpfr_clear (x);
+  mpfr_clear (y);
+  
+  return 0;
+}
+*/
