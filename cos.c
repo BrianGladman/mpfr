f42f325c74ab30d53bed43afde7aa2307214b95b
Vincent Lefèvre <vincent@vinc17.net>
2005-01-28 10:58:01 +0000

    Updated copyright line.
    [From SVN r3232 (trunk)]

--- cos.c
+++ cos.c
@@ -1,6 +1,6 @@
 /* mpfr_cos -- cosine of a floating-point number
 
-Copyright 2001, 2002, 2003, 2004 Free Software Foundation.
+Copyright 2001, 2002, 2003, 2004, 2005 Free Software Foundation.
 
 This file is part of the MPFR Library.
 
