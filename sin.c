c1fa402d17ecd39e46837667e936b9ac331e3118
Vincent Lefèvre <vincent@vinc17.net>
2005-01-28 14:20:09 +0000

    Updated copyright line.
    [From SVN r3242 (trunk)]

--- sin.c
+++ sin.c
@@ -1,6 +1,6 @@
 /* mpfr_sin -- sine of a floating-point number
 
-Copyright 2001, 2002, 2003, 2004 Free Software Foundation, Inc.
+Copyright 2001, 2002, 2003, 2004, 2005 Free Software Foundation, Inc.
 
 This file is part of the MPFR Library.
 
