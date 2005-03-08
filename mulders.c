4b996bc78c48108751a3a25a5e7d015087b48824
Patrick Pélissier <patrick.pelissier@gmail.com>
2005-03-08 14:14:49 +0000

    Add an assert. Cosmetic change.
    [From SVN r3369 (trunk)]

--- mulders.c
+++ mulders.c
@@ -1,4 +1,4 @@
-/* Mulder's MulHigh function
+/* Mulder's MulHigh function (short product)
    
 Copyright 2005 Free Software Foundation.
 
@@ -19,7 +19,6 @@ along with the MPFR Library; see the file COPYING.LIB.  If not, write to
 the Free Software Foundation, Inc., 59 Temple Place - Suite 330, Boston,
 MA 02111-1307, USA. */
 
-
 #define MPFR_NEED_LONGLONG_H
 #include "mpfr-impl.h"
 
@@ -31,8 +30,7 @@ static short mulhigh_ktab[] = {MPFR_MULHIGH_TAB};
 #define MPFR_MULHIGH_TAB_SIZE (sizeof(mulhigh_ktab) / sizeof(mulhigh_ktab[0]))
 #endif
 
-
-/* Put in  rp[n-1..2n-1] an approximation of the n+1 high limbs
+/* Put in  rp[n..2n-1] an approximation of the n high limbs
    of {mp, n} * {np, n}. 
    The error is at worst of ln(n) for rp[n] and rp[n-1] is totally wrong */
 static void
@@ -50,7 +48,8 @@ void
 mpfr_mulhigh_n (mp_ptr rp, mp_srcptr np, mp_srcptr mp, mp_size_t n)
 {
   mp_size_t k;
-
+  
+  MPFR_ASSERTD (MPFR_MULHIGH_TAB_SIZE > 4);
   k = MPFR_LIKELY (n < MPFR_MULHIGH_TAB_SIZE) ? mulhigh_ktab[n] : 2*n/3;
   if (k < 0)
     mpn_mul_basecase (rp, np, n, mp, n);
@@ -69,65 +68,3 @@ mpfr_mulhigh_n (mp_ptr rp, mp_srcptr np, mp_srcptr mp, mp_size_t n)
       mpn_add_1 (rp + n + l, rp + n + l, k, cy); /* propagate carry */
     }
 }
-
-#if 0
-int mpfr_mul () 
-{
-#define MPFR_MUL_BASECASE_THREEHOLD 5
-
-  /* multiplies two mantissa in temporary allocated space */
-  b1 = MPFR_LIKELY (bn >= cn)
-    ? mpn_mul (tmp, MPFR_MANT (b), bn, MPFR_MANT (c), cn)
-    : mpn_mul (tmp, MPFR_MANT (c), cn, MPFR_MANT (b), bn);
-
-
-  /* TO REPLACE WITH -->; */
-  
-  if (MPFR_UNLIKELY (bn < cn)) 
-    {
-      mpfr_ptr  tmp = b;
-      mp_size_t tn  = bn;
-      b = c;
-      c = tmp;
-      bn = cn;
-      cn = tn;
-    }
-  if (MPFR_UNLIKELY (bn > MPFR_MUL_BASECASE_THREEHOLD))
-    {
-      mp_size_t cancel;
-      mp_prec_t prec_cn;
-
-      prec_cn = cn*BITS_PER_MP_LIMB-MPFR_INT_CEIL_LOG2 (cn);
-      /* prec_cn is the expected precision of mulhigh */
-
-      MPFR_ASSERTD (bn >= cn);
-      /* FIXME: Find best guard bits to add */
-      if (MPFR_UNLIKELY (MPFR_PREC (a) > prec_cn - 4))
-	/* MulHigh can't produce a roundable result.
-	   Do the full multiply instead. */
-	goto full_multiply;
-      cancel = 0;
-      if (MPFR_UNLIKELY (MPFR_PREC (a) < prec_cn - 4 -  BITS_PER_MP_LIMB))
-	{
-	  /* MulHigh will computes too much bits */
-	  cancel = (prec_cn - 4 - MPFR_PREC (a)) / BITS_PER_MP_LIMB;
-	  MPFR_ASSERTD (cancel >= 1);
-	}
-      mpfr_mulhigh_n (tmp+2*cancel, MPFR_MANT (b) + cancel, 
-		      MPFR_MANT (c) + cancel, cn-cancel);
-      /* FIXME: tn or k? */
-      if (MPFR_LIKELY (mpfr_can_round_raw (tmp, k, sign, prec_cn,
-					   GMP_RNDN, GMP_RNDZ,
-					   MPFR_PREC(a)+(rnd_mode==GMP_RNDN))))
-	b1 = tmp[2*bn-1];
-      else
-	goto full_multiply;
-      }
-    }
-  else
-    {
-    full_multiply:
-      b1 = mpn_mul (tmp, MPFR_MANT (b), bn, MPFR_MANT (c), cn);      
-    }
-
-#endif
