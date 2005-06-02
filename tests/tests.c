2337a4eba66b4c1ac19f2764b7c8063437611c2c
Patrick Pélissier <patrick.pelissier@gmail.com>
2005-06-02 16:12:06 +0000

    Change the address of the FSF.
    [From SVN r3606 (trunk)]

--- tests/tests.c
+++ tests/tests.c
@@ -16,8 +16,8 @@ License for more details.
 
 You should have received a copy of the GNU Lesser General Public License
 along with the MPFR Library; see the file COPYING.LIB.  If not, write to
-the Free Software Foundation, Inc., 59 Temple Place - Suite 330, Boston,
-MA 02111-1307, USA. */
+the Free Software Foundation, Inc., 51 Franklin Place, Fifth Floor, Boston,
+MA 02110-1301, USA. */
 
 #ifdef HAVE_CONFIG_H
 # if HAVE_CONFIG_H
