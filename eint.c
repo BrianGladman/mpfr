b8bc6e9ebbd25dbe80372ac4146452fcb2503b25
Patrick Pélissier <patrick.pelissier@gmail.com>
2005-06-08 12:35:31 +0000

    Replace MPFR_EXP to MPFR_GET_EXP Use GROUP function Use MPFR_MPZ_SIZEINBASE2 Factorize the mpfr_mul_2exp chain.
    [From SVN r3640 (trunk)]

--- eint.c
+++ eint.c
@@ -34,25 +34,25 @@ MA 02110-1301, USA. */
 static mp_exp_t
 mpfr_eint_aux (mpfr_t y, mpfr_srcptr x)
 {
-  mp_prec_t w = MPFR_PREC(y), w_minus_e;
-  unsigned long k;
-  mpz_t m, s, t, u;
-  mp_exp_t e;
   mpfr_t eps; /* dynamic (absolute) error bound on t */
   mpfr_t erru, errs;
+  mpz_t m, s, t, u;
+  mp_exp_t e, sizeinbase;
+  mp_prec_t w = MPFR_PREC(y), w_minus_e;
+  unsigned long k;
+  MPFR_GROUP_DECL (group);
 
   mpz_init (s); /* initializes to 0 */
   mpz_init (t);
   mpz_init (u);
   mpz_init (m);
-  mpfr_init2 (eps, 31);
-  mpfr_init2 (erru, 31);
-  mpfr_init2 (errs, 31);
+  MPFR_GROUP_INIT_3 (group, 31, eps, erru, errs);
   e = mpfr_get_z_exp (m, x); /* x = m * 2^e */
-  if (mpz_sizeinbase (m, 2) > w)
+  MPFR_ASSERTD (mpz_sizeinbase (m, 2) == MPFR_PREC (x));
+  if (MPFR_PREC (x) > w)
     {
-      e += mpz_sizeinbase (m, 2) - w;
-      mpz_tdiv_q_2exp (m, m, mpz_sizeinbase (m, 2) - w);
+      e += MPFR_PREC (x) - w;
+      mpz_tdiv_q_2exp (m, m, MPFR_PREC (x) - w);
     }
   /* remove trailing zeroes from m: this will speed up much cases where
      x is a small integer divided by a power of 2 */
@@ -75,12 +75,17 @@ mpfr_eint_aux (mpfr_t y, mpfr_srcptr x)
                   = 1 + (eps[k-1]*2^(w-1) + t[k-1])*2^(1-w)*m*2^e/k */
       mpfr_mul_2exp (eps, eps, w - 1, GMP_RNDU);
       mpfr_add_z (eps, eps, t, GMP_RNDU);
+#if 0
       mpfr_div_2exp (eps, eps, w - 1, GMP_RNDU);
       mpfr_mul_2exp (eps, eps, mpz_sizeinbase (m, 2), GMP_RNDU);
       if (e < 0)
         mpfr_div_2exp (eps, eps, -e, GMP_RNDU);
       else
         mpfr_mul_2exp (eps, eps, e, GMP_RNDU);
+#else
+      MPFR_MPZ_SIZEINBASE2 (sizeinbase, m);
+      mpfr_mul_2si (eps, eps, sizeinbase - (w - 1) + e, GMP_RNDU);
+#endif
       mpfr_div_ui (eps, eps, k, GMP_RNDU);
       mpfr_add_ui (eps, eps, 1, GMP_RNDU);
       mpz_mul (t, t, m);
@@ -97,7 +102,11 @@ mpfr_eint_aux (mpfr_t y, mpfr_srcptr x)
       /* and that on s is the sum of all errors on u */
       mpfr_add (errs, errs, erru, GMP_RNDU);
       /* we are done when t is smaller than errs */
-      if (mpz_sizeinbase (t, 2) < MPFR_EXP(errs))
+      if (mpz_sgn (t) == 0)
+        sizeinbase = 0;
+      else
+        MPFR_MPZ_SIZEINBASE2 (sizeinbase, t);
+      if (sizeinbase < MPFR_GET_EXP (errs))
         break;
     }
   /* the truncation error is bounded by (|t|+eps)/k*(|x|/k + |x|^2/k^2 + ...)
@@ -108,7 +117,7 @@ mpfr_eint_aux (mpfr_t y, mpfr_srcptr x)
   mpfr_abs (erru, x, GMP_RNDU); /* |x| */
   mpfr_mul (eps, eps, erru, GMP_RNDU);
   mpfr_ui_sub (erru, k, erru, GMP_RNDD);
-  if (MPFR_SIGN(erru) < 0)
+  if (MPFR_IS_NEG (erru))
     {
       /* the truncated series does not converge, return fail */
       e = w;
@@ -123,11 +132,9 @@ mpfr_eint_aux (mpfr_t y, mpfr_srcptr x)
          in terms of ulp(y). Since ulp(y) = 2^(EXP(y)-PREC(y)), we must
          divide the error by 2^(EXP(y)-PREC(y)), but since we divided also
          y by 2^w = 2^PREC(y), we must simply divide by 2^EXP(y). */
-      e = MPFR_EXP(errs) - MPFR_EXP(y);
+      e = MPFR_GET_EXP (errs) - MPFR_GET_EXP (y);
     }
-  mpfr_clear (eps);
-  mpfr_clear (erru);
-  mpfr_clear (errs);
+  MPFR_GROUP_CLEAR (group);
   mpz_clear (s);
   mpz_clear (t);
   mpz_clear (u);
@@ -186,7 +193,7 @@ mpfr_eint (mpfr_ptr y, mpfr_srcptr x, mp_rnd_t rnd)
 
   /* eint() has a root 0.37250741078136663446..., so if x is near,
      already take more bits */
-  if (MPFR_EXP(x) == -1) /* 1/4 <= x < 1/2 */
+  if (MPFR_GET_EXP(x) == -1) /* 1/4 <= x < 1/2 */
     {
       double d;
       d = mpfr_get_d (x, GMP_RNDN) - 0.37250741078136663;
@@ -198,20 +205,21 @@ mpfr_eint (mpfr_ptr y, mpfr_srcptr x, mp_rnd_t rnd)
   for (;;)                               /* Infinite loop */
     {
       err = mpfr_eint_aux (tmp, x); /* error <= 2^err ulp(tmp) */
-      te = MPFR_EXP(tmp);
+      te = MPFR_GET_EXP(tmp);
       mpfr_const_euler (ump, GMP_RNDN); /* 0.577 -> EXP(ump)=0 */
       mpfr_add (tmp, tmp, ump, GMP_RNDN);
       /* error <= 1/2 + 1/2*2^(EXP(ump)-EXP(tmp)) + 2^(te-EXP(tmp)+err)
                <= 1/2 + 2^(MAX(EXP(ump), te+err+1) - EXP(tmp))
                <= 2^(MAX(0, 1 + MAX(EXP(ump), te+err+1) - EXP(tmp))) */
-      err = MAX(1, te + err + 2) - MPFR_EXP(tmp);
+      err = MAX(1, te + err + 2) - MPFR_GET_EXP(tmp);
       err = MAX(0, err);
-      te = MPFR_EXP(tmp);
+      te = MPFR_GET_EXP(tmp);
       mpfr_log (ump, x, GMP_RNDN);
       mpfr_add (tmp, tmp, ump, GMP_RNDN);
       /* same formula as above, except now EXP(ump) is not 0 */
-      err = MAX(0, 1 + MAX(MPFR_EXP(ump), te + err + 1) - MPFR_EXP(tmp));
-      err = MPFR_PREC(tmp) - err;
+      err = MAX(0, 1 + MAX (MPFR_GET_EXP (ump), te + err + 1)
+                - MPFR_GET_EXP (tmp));
+      err = MPFR_PREC (tmp) - err;
       if (MPFR_LIKELY (MPFR_CAN_ROUND (tmp, err, MPFR_PREC (y), rnd)))
 	break;
       MPFR_ZIV_NEXT (loop, prec);        /* Increase used precision */
