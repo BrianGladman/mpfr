34969c4dae722ffab9c49e18c8d512327482826d
Patrick Pélissier <patrick.pelissier@gmail.com>
2005-06-14 11:17:55 +0000

    Add Fast case for u==1 Add Fast case for u a power of 2 (Call mul/div_2si).
    [From SVN r3650 (trunk)]

--- div_ui.c
+++ div_ui.c
@@ -64,13 +64,20 @@ mpfr_div_ui (mpfr_ptr y, mpfr_srcptr x, unsigned long int u, mp_rnd_t rnd_mode)
             }
         }
     }
-  else if (MPFR_UNLIKELY (u == 0))
+  else if (MPFR_UNLIKELY (u <= 1))
     {
-      /* x/0 is Inf since x != 0*/
-      MPFR_SET_INF (y);
-      MPFR_SET_SAME_SIGN (y, x);
-      MPFR_RET (0);
+      if (u < 1)
+        {
+          /* x/0 is Inf since x != 0*/
+          MPFR_SET_INF (y);
+          MPFR_SET_SAME_SIGN (y, x);
+          MPFR_RET (0);
+        }
+      else /* y = x/1 = x */
+        return mpfr_set (y, x, rnd_mode);
     }
+  else if (MPFR_UNLIKELY (IS_POW2 (u)))
+    return mpfr_div_2si (y, x, MPFR_INT_CEIL_LOG2 (u)-1, rnd_mode);
 
   MPFR_CLEAR_FLAGS (y);
 
