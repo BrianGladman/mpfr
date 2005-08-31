989763c6c0207e6bf2fdc5a95500c3c01ddea95b
Vincent Lefèvre <vincent@vinc17.net>
2005-08-31 12:21:14 +0000

    Updated version to 2.3.0.
    [From SVN r3750 (trunk)]

--- mpfr.h
+++ mpfr.h
@@ -24,9 +24,9 @@ MA 02110-1301, USA. */
 
 /* Define MPFR version number */
 #define MPFR_VERSION_MAJOR 2
-#define MPFR_VERSION_MINOR 2
+#define MPFR_VERSION_MINOR 3
 #define MPFR_VERSION_PATCHLEVEL 0
-#define MPFR_VERSION_STRING "2.2.0"
+#define MPFR_VERSION_STRING "2.3.0"
 
 /* Macros dealing with MPFR VERSION */
 #define MPFR_VERSION_NUM(a,b,c) (((a) << 16L) | ((b) << 8) | (c))
