f97066ec4d40f6b291dc0b20390457d371f24a0b
Vincent Lefèvre <vincent@vinc17.net>
2005-09-06 11:50:09 +0000

    mpfr_set_str_binary was buggy and inefficient. Let's use mpfr_strtofr.
    [From SVN r3783 (trunk)]

--- set_str_raw.c
+++ set_str_raw.c
@@ -1,6 +1,6 @@
 /* mpfr_set_str_binary -- set a floating-point number from a binary string
 
-Copyright 1999, 2000, 2001, 2002, 2003, 2004 Free Software Foundation, Inc.
+Copyright 1999, 2000, 2001, 2002, 2003, 2004, 2005 Free Software Foundation, Inc.
 
 This file is part of the MPFR Library.
 
@@ -19,11 +19,6 @@ along with the MPFR Library; see the file COPYING.LIB.  If not, write to
 the Free Software Foundation, Inc., 51 Franklin Place, Fifth Floor, Boston,
 MA 02110-1301, USA. */
 
-#include <stdlib.h>
-#include <string.h>
-#include <errno.h>
-
-#define MPFR_NEED_LONGLONG_H
 #include "mpfr-impl.h"
 
 /* Currently the number should be of the form +/- xxxx.xxxxxxEyy, with
@@ -33,23 +28,8 @@ MA 02110-1301, USA. */
 void
 mpfr_set_str_binary (mpfr_ptr x, const char *str)
 {
-  char *str2, *str0, negative = 0;
-  unsigned long j, l, k = 0, xsize, cnt, alloc;
-  mp_limb_t *xp;
-  long expn = 0, e;
-  char *endstr2;
-
-  xp = MPFR_MANT(x);
-  xsize = MPFR_LIMB_SIZE(x);
-  alloc = strlen(str) + 1;
-
-  if (*str == '-')
-    {
-      negative = 1;
-      str++;
-    }
-  else if (*str == '+')
-    str++;
+  int has_sign;
+  int res;
 
   if (*str == 'N')
     {
@@ -58,94 +38,17 @@ mpfr_set_str_binary (mpfr_ptr x, const char *str)
       return;
     }
 
-  if (*str == 'I')
+  has_sign = *str == '-' || *str == '+';
+  if (str[has_sign] == 'I')
     {
       MPFR_SET_INF(x);
-      if (negative)
+      if (*str == '-')
         MPFR_SET_NEG(x);
       else
         MPFR_SET_POS(x);
-      /*if (MPFR_IS_STRICTNEG(x) != negative)
-        MPFR_CHANGE_SIGN(x);*/
       return;
     }
 
-  MPFR_CLEAR_FLAGS(x);
-
-  str0 = str2 = (char *) (*__gmp_allocate_func) (alloc);
-
-  while (*str == '0')
-    str++;
-
-  while (*str == '0' || *str == '1')
-    {
-      *(str2++) = *(str++);
-      k++;
-    }
-
-  if (*str == '.')
-    {
-      str++;
-      while (*str == '0' || *str == '1')
-        {
-          *(str2++) = *(str++);
-        }
-    }
-
-  if (*str == 'e' || *str == 'E')
-    {
-      char *end;
-
-      e = strtol (++str, &end, 10); /* signed exponent after 'e' or 'E' */
-      MPFR_ASSERTN (*end == '\0' && errno != ERANGE);
-      expn = k + e;
-      MPFR_ASSERTN (expn >= e);
-    }
-  else
-    expn = k;
-
-  endstr2 = str2;
-  l = endstr2 - str0; /* length of mantissa */
-  if (l == 0)
-    { /* input is zero */
-      MPFR_SET_ZERO(x);
-    }
-  else
-    {
-      l = (l - 1) / BITS_PER_MP_LIMB + 1;
-      str2 = str0;
-
-      MPFR_ASSERTN (l <= xsize);
-
-      /* str2[0]..endstr2[-1] contains the mantissa */
-      for (k = 1; k <= l; k++)
-        {
-          j = 0;
-          xp[xsize - k] = 0;
-          while ((str2 < endstr2) && (j < BITS_PER_MP_LIMB))
-            {
-              xp[xsize - k] = (xp[xsize - k] << 1) + (*str2 - '0');
-              str2++; j++;
-            }
-          xp[xsize - k] <<= (BITS_PER_MP_LIMB - j);
-        }
-
-      for (; k <= xsize; k++)
-        xp[xsize - k] = 0;
-
-      count_leading_zeros(cnt, xp[xsize - 1]);
-      if (cnt)
-        mpn_lshift(xp, xp, xsize, cnt);
-
-      MPFR_SET_EXP (x, expn - cnt);
-      if (negative)
-        MPFR_SET_NEG(x);
-      else
-        MPFR_SET_POS(x);
-    }
-
-  (*__gmp_free_func) (str0, alloc);
-
-  /* May change to take into account : syntax errors, overflow in exponent,
-     string truncated because of size of x */
+  res = mpfr_strtofr (x, str, 0, 2, GMP_RNDZ);
+  MPFR_ASSERTN (res == 0);
 }
