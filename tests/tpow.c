9a0e4bf183e33ce1b96813325237e052b3f7eb12
Vincent Lefèvre <vincent@vinc17.net>
2005-10-06 10:03:56 +0000

    One of the tests was using the fabs function, but -lm isn't given when generating the executable, making the linking fail under HP-UX. Fixed by not using this fabs function.
    [From SVN r3882 (trunk)]

--- tests/tpow.c
+++ tests/tpow.c
@@ -509,6 +509,7 @@ particular_cases (void)
   for (i = 0; i < 11; i++)
     for (j = 0; j < 11; j++)
       {
+        double d;
         int p;
         static int q[11][11] = {
           /*          NaN +inf -inf  +0   -0   +1   -1   +2   -2  +0.5 -0.5 */
@@ -527,7 +528,7 @@ particular_cases (void)
         test_pow (r, t[i], t[j], GMP_RNDN);
         p = mpfr_nan_p (r) ? 0 : mpfr_inf_p (r) ? 1 :
           mpfr_cmp_ui (r, 0) == 0 ? 2 :
-          (int) (fabs (mpfr_get_d (r, GMP_RNDN)) * 128.0);
+          (d = mpfr_get_d (r, GMP_RNDN), (int) (ABS(d) * 128.0));
         if (p != 0 && MPFR_SIGN(r) < 0)
           p = -p;
         if (p != q[i][j])
