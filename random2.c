54ecf60dce0adc76b08ee490bbe56836a4cc9d23
Paul Zimmermann <Paul.Zimmermann@inria.fr>
2006-03-14 15:33:31 +0000

    added missing include file
    [From SVN r4036 (trunk)]

--- random2.c
+++ random2.c
@@ -21,6 +21,8 @@ along with the MPFR Library; see the file COPYING.LIB.  If not, write to
 the Free Software Foundation, Inc., 51 Franklin Place, Fifth Floor, Boston,
 MA 02110-1301, USA. */
 
+#include <stdio.h>  /* for NULL */
+
 #define MPFR_NEED_LONGLONG_H
 #include "mpfr-impl.h"
 
