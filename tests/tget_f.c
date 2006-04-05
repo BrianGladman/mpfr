5ea3fe16e8377299e389896eb1e001b162804dbe
Vincent Lefèvre <vincent@vinc17.net>
2006-04-05 16:32:23 +0000

    tget_f.c: test that there is no lost of accuracy when converting a mpfr_t number into a mpf_t number (test with various precisions and exponents).
    [From SVN r4063 (trunk)]

--- tests/tget_f.c
+++ tests/tget_f.c
@@ -26,6 +26,70 @@ MA 02110-1301, USA. */
 
 #include "mpfr-test.h"
 
+/* Test that there is no lost of accuracy when converting a mpfr_t number
+   into a mpf_t number (test with various precisions and exponents). */
+static void
+prec_test (void)
+{
+  int px, py;
+
+  for (py = 3; py <= 136; py++)
+    {
+      mpfr_t y1, y2, y3;
+
+      mpfr_init2 (y1, py);
+      mpfr_init2 (y2, py);
+      mpfr_init2 (y3, py);
+
+      for (px = 32; px <= 160; px += 32)
+        {
+          mpf_t x1, x2, x3;
+          int e;
+
+          mpf_init (x1);
+          mpf_init (x2);
+          mpf_init (x3);
+          mpfr_set_ui_2exp (y1, 1, py - 1, GMP_RNDN);
+          mpfr_get_f (x1, y1, GMP_RNDN);  /* exact (power of 2) */
+          mpf_set (x2, x1);
+          mpfr_set (y2, y1, GMP_RNDN);
+
+          for (e = py - 2; e >= 0; e--)
+            {
+              int inex;
+              mpf_div_2exp (x2, x2, 1);
+              mpf_add (x1, x1, x2);
+              mpfr_div_2exp (y2, y2, 1, GMP_RNDN);
+              inex = mpfr_add (y1, y1, y2, GMP_RNDN);
+              MPFR_ASSERTN (inex == 0);
+              mpfr_set_f (y3, x1, GMP_RNDN);
+              if (! mpfr_equal_p (y1, y3))
+                break;
+              mpfr_get_f (x3, y3, GMP_RNDN);
+              if (mpf_cmp (x1, x3) != 0)
+                {
+                  printf ("Error in prec_test (px = %d, py = %d, e = %d)\n",
+                          px, py, e);
+                  printf ("x1 = ");
+                  mpf_out_str (stdout, 16, 0, x1);
+                  printf ("\nx2 = ");
+                  mpf_out_str (stdout, 16, 0, x1);
+                  printf ("\n");
+                  exit (1);
+                }
+            }
+
+          mpf_clear (x1);
+          mpf_clear (x2);
+          mpf_clear (x3);
+        }
+
+      mpfr_clear (y1);
+      mpfr_clear (y2);
+      mpfr_clear (y3);
+    }
+}
+
 int
 main (void)
 {
@@ -175,6 +239,8 @@ main (void)
   mpfr_clear (y);
   mpfr_clear (z);
   mpf_clear (x);
+
+  prec_test ();
 
   tests_end_mpfr ();
   return 0;
