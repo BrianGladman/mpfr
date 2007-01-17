827b8821000b700b24aea0ecf833498183028fc6
Paul Zimmermann <Paul.Zimmermann@inria.fr>
2007-01-17 19:38:49 +0000

    added documentation for mpfr_dim (was missing)
    [From SVN r4335 (trunk)]

--- dim.c
+++ dim.c
@@ -28,15 +28,12 @@ MA 02110-1301, USA. */
 */
 
 int
-mpfr_dim (mpfr_ptr z, mpfr_srcptr x ,mpfr_srcptr y , mp_rnd_t rnd_mode)
+mpfr_dim (mpfr_ptr z, mpfr_srcptr x, mpfr_srcptr y, mp_rnd_t rnd_mode)
 {
-  if (MPFR_ARE_SINGULAR(x,y))
+  if (MPFR_IS_NAN(x) || MPFR_IS_NAN(y))
     {
-      if (MPFR_IS_NAN(x) || MPFR_IS_NAN(y))
-        {
-          MPFR_SET_NAN(z);
-          MPFR_RET_NAN;
-        }
+      MPFR_SET_NAN(z);
+      MPFR_RET_NAN;
     }
 
   if (mpfr_cmp (x,y) > 0)
