8fbb8d213e1c3cf484d6f487ffad83f0db9db97c
Vincent Lefèvre <vincent@vinc17.net>
2007-05-04 00:29:47 +0000

    tremquo.c: fixed tests (= -> ==).
    [From SVN r4438 (trunk)]

--- tests/tremquo.c
+++ tests/tremquo.c
@@ -69,12 +69,12 @@ main (int argc, char *argv[])
   mpfr_set_ui (y, 1, GMP_RNDN);
   mpfr_remquo (r, q, x, y, GMP_RNDN);
   MPFR_ASSERTN (mpfr_nan_p (r));
-  
+
   mpfr_set_inf (x, 1); /* +Inf */
   mpfr_set_ui (y, 0, GMP_RNDN);
   mpfr_remquo (r, q, x, y, GMP_RNDN);
   MPFR_ASSERTN (mpfr_nan_p (r));
-  
+
   mpfr_set_inf (x, 1); /* +Inf */
   mpfr_set_inf (y, 1);
   mpfr_remquo (r, q, x, y, GMP_RNDN);
@@ -85,14 +85,14 @@ main (int argc, char *argv[])
   mpfr_remquo (r, q, x, y, GMP_RNDN);
   MPFR_ASSERTN (mpfr_cmp_ui (r, 0) == 0 && MPFR_IS_POS (r));
   MPFR_ASSERTN (q[0] == (long) 0);
-  
+
   mpfr_set_ui (x, 0, GMP_RNDN);
   mpfr_neg (x, x, GMP_RNDN); /* -0 */
   mpfr_set_inf (y, 1);
   mpfr_remquo (r, q, x, y, GMP_RNDN);
   MPFR_ASSERTN (mpfr_cmp_ui (r, 0) == 0 && MPFR_IS_NEG (r));
   MPFR_ASSERTN (q[0] == (long) 0);
-  
+
   mpfr_set_ui (x, 17, GMP_RNDN);
   mpfr_set_inf (y, 1);
   mpfr_remquo (r, q, x, y, GMP_RNDN);
@@ -103,7 +103,7 @@ main (int argc, char *argv[])
   mpfr_set_ui (y, 0, GMP_RNDN);
   mpfr_remquo (r, q, x, y, GMP_RNDN);
   MPFR_ASSERTN (mpfr_nan_p (r));
-  
+
   mpfr_set_ui (x, 0, GMP_RNDN);
   mpfr_set_ui (y, 17, GMP_RNDN);
   mpfr_remquo (r, q, x, y, GMP_RNDN);
@@ -125,7 +125,7 @@ main (int argc, char *argv[])
   mpfr_remquo (r, q, x, y, GMP_RNDN);
   MPFR_ASSERTN (mpfr_cmp_ui (r, 8) == 0);
   MPFR_ASSERTN (q[0] == (long) 2);
-  
+
   mpfr_set_si (x, -42, GMP_RNDN);
   mpfr_set_ui (y, 17, GMP_RNDN);
   mpfr_remquo (r, q, x, y, GMP_RNDN);
@@ -140,7 +140,7 @@ main (int argc, char *argv[])
   mpfr_remquo (r, q, x, y, GMP_RNDN);
   MPFR_ASSERTN (mpfr_cmp_ui_2exp (r, 1, -94) == 0);
   MPFR_ASSERTN (q[0] == (long) 2);
-  
+
   mpfr_set_prec (x, 50);
   mpfr_set_prec (y, 100);
   mpfr_set_ui (x, 42, GMP_RNDN);
@@ -149,7 +149,7 @@ main (int argc, char *argv[])
   mpfr_remquo (r, q, x, y, GMP_RNDN);
   MPFR_ASSERTN (mpfr_cmp_ui_2exp (r, 1, -44) == 0);
   MPFR_ASSERTN (q[0] == (long) 2);
-  
+
   mpfr_set_prec (x, 100);
   mpfr_set_prec (y, 50);
   mpfr_set_ui (x, 42, GMP_RNDN);
@@ -199,7 +199,7 @@ main (int argc, char *argv[])
   mpfr_remquo (r, q, x, y, GMP_RNDN);
   /* x/y = 1.5, quotient should be 2 (even rule), remainder should be -1 */
   MPFR_ASSERTN (mpfr_cmp_si (r, -1) == 0);
-  MPFR_ASSERTN (q[0] = 2L);
+  MPFR_ASSERTN (q[0] == 2L);
   mpfr_set_ui (x, 3, GMP_RNDN);
   mpfr_nextabove (x); /* 3 + 2^(-63) */
   mpfr_set_ui (y, 2, GMP_RNDN);
@@ -207,7 +207,7 @@ main (int argc, char *argv[])
   /* x/y = 1.5 + 2^(-64), quo should be 2, r should be -1 + 2^(-63) */
   MPFR_ASSERTN (mpfr_add_ui (r, r, 1, GMP_RNDN) == 0);
   MPFR_ASSERTN (mpfr_cmp_ui_2exp (r, 1, -63) == 0);
-  MPFR_ASSERTN (q[0] = 2L);
+  MPFR_ASSERTN (q[0] == 2L);
   mpfr_set_ui (x, 3, GMP_RNDN);
   mpfr_set_ui (y, 2, GMP_RNDN);
   mpfr_nextabove (y); /* 2 + 2^(-63) */
@@ -215,7 +215,7 @@ main (int argc, char *argv[])
   /* x/y = 1.5 - eps, quo should be 1, r should be 1 - 2^(-63) */
   MPFR_ASSERTN (mpfr_sub_ui (r, r, 1, GMP_RNDN) == 0);
   MPFR_ASSERTN (mpfr_cmp_si_2exp (r, -1, -63) == 0);
-  MPFR_ASSERTN (q[0] = 1L);
+  MPFR_ASSERTN (q[0] == 1L);
 
   /* bug founds by Kaveh Ghazi, 3 May 2007 */
   mpfr_set_ui (x, 2, GMP_RNDN);
