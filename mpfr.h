8c6a802990f9c6c293b41e5fe7a12459914cc4dd
Vincent Lefèvre <vincent@vinc17.net>
2007-07-01 23:01:10 +0000

    Added mpfr_fms based on mpfr_fma.
    [From SVN r4602 (trunk)]

--- mpfr.h
+++ mpfr.h
@@ -558,6 +558,8 @@ __MPFR_DECLSPEC int mpfr_cmp_f _MPFR_PROTO ((mpfr_srcptr, mpf_srcptr));
 
 __MPFR_DECLSPEC int mpfr_fma _MPFR_PROTO ((mpfr_ptr, mpfr_srcptr, mpfr_srcptr,
                                            mpfr_srcptr, mpfr_rnd_t));
+__MPFR_DECLSPEC int mpfr_fms _MPFR_PROTO ((mpfr_ptr, mpfr_srcptr, mpfr_srcptr,
+                                           mpfr_srcptr, mpfr_rnd_t));
 __MPFR_DECLSPEC int mpfr_sum _MPFR_PROTO ((mpfr_ptr, mpfr_ptr *__gmp_const,
                                            unsigned long, mpfr_rnd_t));
 
