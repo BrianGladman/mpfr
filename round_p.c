e0661d6b1ff2ba6e98bb3e30dca9fcd59527c216
Vincent Lefèvre <vincent@vinc17.net>
2007-12-19 14:49:34 +0000

    round_p.c: in comment, "round to zero" -> "round toward zero".
    [From SVN r5120 (trunk)]

--- round_p.c
+++ round_p.c
@@ -51,7 +51,8 @@ mpfr_round_p (mp_limb_t *bp, mp_size_t bn, mp_exp_t err0, mp_prec_t prec)
 /*
  * Assuming {bp, bn} is an approximation of a non-singular number
  * with error at most equal to 2^(EXP(b)-err) (`err' bits of b are known)
- * of direction unknown, check if we can round b to zero with precision prec.
+ * of direction unknown, check if we can round b toward zero with
+ * precision prec.
  */
 int
 mpfr_round_p (mp_limb_t *bp, mp_size_t bn, mp_exp_t err0, mp_prec_t prec)
