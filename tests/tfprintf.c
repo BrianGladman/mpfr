87465706331a15b0479d422804cda2bdecbf0b38
Philippe Théveny <philippe.theveny@laposte.net>
2008-02-28 16:11:33 +0000

    add mixed format tests (with different types and different sizes)
    [From SVN r5330 (trunk)]

--- tests/tfprintf.c
+++ tests/tfprintf.c
@@ -19,19 +19,19 @@ the Free Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston,
 MA 02110-1301, USA. */
 
 #ifdef HAVE_STDARG
-
 #include <stdarg.h>
 
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
+#include <limits.h>
+#include <float.h>
+#include <stddef.h>
 
-#ifdef HAVE_LOCALE_H
-#include <locale.h>
+#ifdef HAVE_STDINT_H
+#include <stdint.h>
 #endif
 
 #include <gmp.h>
-
 #include "mpfr-test.h"
 
 #if MPFR_VERSION >= MPFR_VERSION_NUM(2,4,0)
@@ -50,7 +50,24 @@ check (FILE *fout, char *fmt, mpfr_t x)
     }
 }
 
-static int
+static void
+check_vfprintf (FILE *fout, char *fmt, ...)
+{
+  va_list ap;
+
+  va_start (ap, fmt);
+  if (mpfr_vfprintf (fout, fmt, ap) == -1)
+    {
+      mpfr_printf ("Error in mpfr_vfprintf(fout, \"%s\", ...)\n", fmt);
+
+      va_end (ap);
+      exit (1);
+    }
+
+  va_end (ap);
+}
+
+static void
 check_special (FILE *fout)
 {
   mpfr_t x;
@@ -63,6 +80,11 @@ check_special (FILE *fout)
   check (fout, "%Re", x);
   check (fout, "%Rf", x);
   check (fout, "%Rg", x);
+  check_vfprintf (fout, "%Ra", x);
+  check_vfprintf (fout, "%Rb", x);
+  check_vfprintf (fout, "%Re", x);
+  check_vfprintf (fout, "%Rf", x);
+  check_vfprintf (fout, "%Rg", x);
 
   mpfr_set_inf (x, -1);
   check (fout, "%Ra", x);
@@ -70,6 +92,11 @@ check_special (FILE *fout)
   check (fout, "%Re", x);
   check (fout, "%Rf", x);
   check (fout, "%Rg", x);
+  check_vfprintf (fout, "%Ra", x);
+  check_vfprintf (fout, "%Rb", x);
+  check_vfprintf (fout, "%Re", x);
+  check_vfprintf (fout, "%Rf", x);
+  check_vfprintf (fout, "%Rg", x);
 
   mpfr_set_nan (x);
   check (fout, "%Ra", x);
@@ -77,12 +104,90 @@ check_special (FILE *fout)
   check (fout, "%Re", x);
   check (fout, "%Rf", x);
   check (fout, "%Rg", x);
+  check_vfprintf (fout, "%Ra", x);
+  check_vfprintf (fout, "%Rb", x);
+  check_vfprintf (fout, "%Re", x);
+  check_vfprintf (fout, "%Rf", x);
+  check_vfprintf (fout, "%Rg", x);
 
   mpfr_clear (x);
-  return 0;
 }
 
-static int
+static void
+check_mixed (FILE *fout)
+{
+  char ch = CHAR_MAX;
+  signed char sch = SCHAR_MIN;
+  unsigned char uch = UCHAR_MAX;
+  short sh = SHRT_MIN;
+  unsigned short ush = USHRT_MAX;
+  int i = INT_MIN;
+  unsigned int ui = UINT_MAX;
+  long lo = LONG_MIN;
+  unsigned long ulo = ULONG_MAX;
+  float f = FLT_MAX;
+  double d = DBL_MAX;
+  long double ld = LDBL_MAX;
+
+  ptrdiff_t p = PTRDIFF_MAX;
+  size_t si = SIZE_MAX;
+
+#ifdef HAVE_LONG_LONG
+  long long llo = 2 * LONG_MIN;
+  unsigned long long ullo = 2 * ULONG_MAX;
+#endif
+
+#ifdef HAVE_STDINT_H
+  intmax_t im = INTMAX_MIN;
+  uintmax_t uim = UINTMAX_MAX;
+#endif
+
+  mpz_t mpz;
+  mpq_t mpq;
+  mpf_t mpf;
+  mp_rnd_t rnd = GMP_RNDN;
+
+  mpfr_t mpfr;
+  mpfr_prec_t prec;
+
+  mpz_init (mpz);
+  mpz_set_ui (mpz, ulo);
+  mpq_init (mpq);
+  mpq_set_si (mpq, lo, ulo);
+  mpf_init (mpf);
+  mpf_set_q (mpf, mpq);
+  mpfr_init (mpfr);
+  mpfr_set_f (mpfr, mpf, GMP_RNDN);
+  prec = mpfr_get_prec (mpfr);
+
+  check_vfprintf (fout, "a. %Ra, b. %hhd, c. %i, d. %lx\n", mpfr, sch, i,
+                  ulo);
+  check_vfprintf (fout, "a. %hhu, b. %Rb, c. %u, d. %li\n", uch, mpfr, ui,
+                  lo);
+  check_vfprintf (fout, "a. %hx, b. %*f, c. %Re\n", sh, 3, f, mpfr);
+  check_vfprintf (fout, "a. %hi, b. %e, c. %Rf\n", ush, d, mpfr);
+  check_vfprintf (fout, "a. %Pu, b. %c, c. %Lf, d. %Zi\n", prec, ch, ld, mpz);
+  check_vfprintf (fout, "%% a. %p, b. %RNA, c. %td, d. %Qx\n", &i, mpfr, p,
+                  mpq);
+  check_vfprintf (fout, "a. %R*A, b. %Fe, c. %i\n", rnd, mpfr, mpf, si);
+
+#ifdef HAVE_LONG_LONG
+  check_vfprintf (fout, "a. %Re, b. %llx\n", mpfr, ullo);
+  check_vfprintf (fout, "a. %lli, b. %Rf\n", llo, mpfr);
+#endif
+
+#ifdef HAVE_STDINT_H
+  check_vfprintf (fout, "a. %*RA, b. %ji\n", 10, mpfr, im);
+  check_vfprintf (fout, "a. %.*Re, b. %jx\n", 10, mpfr, uim);
+#endif
+
+  mpfr_clear (mpfr);
+  mpf_clear (mpf);
+  mpq_clear (mpq);
+  mpz_clear (mpz);
+}
+
+static void
 check_random (FILE *fout, int nb_tests)
 {
   int i;
@@ -171,7 +276,6 @@ check_random (FILE *fout, int nb_tests)
   mpfr_set_emax (old_emax);
 
   mpfr_clear (x);
-  return 0;
 }
 
 int
@@ -213,6 +317,7 @@ main (int argc, char *argv[])
         }
     }
 
+  check_mixed (fout);
   check_random (fout, N);
 
   fclose (fout);
