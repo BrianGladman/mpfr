be53fe74b1424d599f8130398f9a083322e15c35
Vincent Lefèvre <vincent@vinc17.net>
2009-03-21 00:52:31 +0000

    mpfr.texi: corrected mpfr_strtofr documentation ("NAN()" is accepted). tests/tstrtofr.c: added a test for "NAN()". Note: the code follows the ISO C99 standard for strtod, as explained in the mpfr_strtofr documentation, so that the code was correct. [merged changeset 6131 from the 2.4 branch and changed GMP_RNDN into MPFR_RNDN]
    [From SVN r6132 (trunk)]

--- tests/tstrtofr.c
+++ tests/tstrtofr.c
@@ -107,6 +107,13 @@ check_special (void)
       printf ("Error for setting NAN (6)\n");
       exit (1);
     }
+  mpfr_set_ui (x, 0, MPFR_RNDN); /* make sure that x is modified */
+  res = mpfr_strtofr (x, "NAN()foo", &s, 10, MPFR_RNDN);
+  if (res != 0 || !mpfr_nan_p (x) || strcmp(s, "foo") )
+    {
+      printf ("Error for setting NAN (7)\n");
+      exit (1);
+    }
 
   /* Check INF */
   res = mpfr_strtofr (x, "INFINITY", &s, 8, MPFR_RNDN);
