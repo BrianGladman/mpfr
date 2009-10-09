6bd8dd651deb4ba51e02e771f0796581a8ffaec7
Vincent Lefèvre <vincent@vinc17.net>
2009-10-09 07:29:54 +0000

    tests/tsin_cos.c: mp_rnd_t -> mpfr_rnd_t.
    [From SVN r6510 (trunk)]

--- tests/tsin_cos.c
+++ tests/tsin_cos.c
@@ -420,7 +420,7 @@ bug20091008 (void)
   mpfr_t x, y, z, yref, zref;
   mp_prec_t p = 1000;
   int inex, inexref;
-  mp_rnd_t r = MPFR_RNDN;
+  mpfr_rnd_t r = MPFR_RNDN;
 
   mpfr_init2 (x, p);
   mpfr_init2 (y, p);
