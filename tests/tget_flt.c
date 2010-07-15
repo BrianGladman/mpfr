2b583bd9e1c6cdb8cefbf64c0ad0a3ebe10f53fd
Paul Zimmermann <Paul.Zimmermann@inria.fr>
2010-07-15 12:49:09 +0000

    [tget_flt.c] print more information in case a test fails
    [From SVN r7003 (trunk)]

--- tests/tget_flt.c
+++ tests/tget_flt.c
@@ -57,7 +57,9 @@ main (void)
   mpfr_set_flt (x, f, MPFR_RNDN);
   if (mpfr_inf_p (x) == 0 || mpfr_sgn (x) < 0)
     {
-      printf ("Error for mpfr_set_flt(mpfr_get_flt(+Inf))n");
+      printf ("Error for mpfr_set_flt(mpfr_get_flt(+Inf)):\n");
+      printf ("expected -Inf\n");
+      printf ("got "); mpfr_dump (x);
       exit (1);
     }
 
