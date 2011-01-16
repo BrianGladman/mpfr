c6a71e74dad3a715302460e16d11285765cc8dda
Vincent Lefèvre <vincent@vinc17.net>
2011-01-16 14:00:34 +0000

    [src/yn.c] Added support for the division-by-zero exception.
    [From SVN r7379 (trunk)]

--- src/yn.c
+++ src/yn.c
@@ -180,10 +180,11 @@ mpfr_yn (mpfr_ptr res, long n, mpfr_srcptr z, mpfr_rnd_t r)
               when z goes to zero */
         {
           MPFR_SET_INF(res);
-          if (n >= 0 || (n & 1) == 0)
+          if (n >= 0 || ((unsigned long) n & 1) == 0)
             MPFR_SET_NEG(res);
           else
             MPFR_SET_POS(res);
+          mpfr_set_divby0 ();
           MPFR_RET(0);
         }
     }
