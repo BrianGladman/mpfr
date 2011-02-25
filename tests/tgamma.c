d74fd9257bf477f3c1c098ce5bbaf4577e77d117
Vincent Lefèvre <vincent@vinc17.net>
2011-02-25 16:20:41 +0000

    [tests/tgamma.c] Added tests in maximum exponent range.
    [From SVN r7498 (trunk)]

--- tests/tgamma.c
+++ tests/tgamma.c
@@ -578,6 +578,34 @@ exprange (void)
   mpfr_set_ui (x, 0, MPFR_RNDN);
   mpfr_nextabove (x);
   mpfr_set_inf (y, 1);
+  inex1 = 1;
+  flags1 = MPFR_FLAGS_INEXACT | MPFR_FLAGS_OVERFLOW;
+  mpfr_clear_flags ();
+  inex2 = mpfr_gamma (z, x, MPFR_RNDU);
+  flags2 = __gmpfr_flags;
+  MPFR_ASSERTN (mpfr_inexflag_p ());
+  if (inex1 != inex2 || flags1 != flags2 || ! mpfr_equal_p (y, z))
+    {
+      printf ("Error in exprange (test5)\n");
+      printf ("Expected inex1 = %d, flags1 = %u, ", SIGN (inex1), flags1);
+      mpfr_dump (y);
+      printf ("Got      inex2 = %d, flags2 = %u, ", SIGN (inex2), flags2);
+      mpfr_dump (z);
+      exit (1);
+    }
+  mpfr_clear_flags ();
+  inex2 = mpfr_gamma (z, x, MPFR_RNDN);
+  flags2 = __gmpfr_flags;
+  MPFR_ASSERTN (mpfr_inexflag_p ());
+  if (inex1 != inex2 || flags1 != flags2 || ! mpfr_equal_p (y, z))
+    {
+      printf ("Error in exprange (test6)\n");
+      printf ("Expected inex1 = %d, flags1 = %u, ", SIGN (inex1), flags1);
+      mpfr_dump (y);
+      printf ("Got      inex2 = %d, flags2 = %u, ", SIGN (inex2), flags2);
+      mpfr_dump (z);
+      exit (1);
+    }
   mpfr_nextbelow (y);
   inex1 = -1;
   flags1 = MPFR_FLAGS_INEXACT;
@@ -587,7 +615,7 @@ exprange (void)
   MPFR_ASSERTN (mpfr_inexflag_p ());
   if (inex1 != inex2 || flags1 != flags2 || ! mpfr_equal_p (y, z))
     {
-      printf ("Error in exprange (test5)\n");
+      printf ("Error in exprange (test7)\n");
       printf ("Expected inex1 = %d, flags1 = %u, ", SIGN (inex1), flags1);
       mpfr_dump (y);
       printf ("Got      inex2 = %d, flags2 = %u, ", SIGN (inex2), flags2);
