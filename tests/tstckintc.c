88a69c00e14f15684ce9816df521744ad72d4eb1
Olivier Demengeon <olivier.demengeon@inria.fr>
2011-03-01 15:47:36 +0000

    Extend coverage test of mpfr_custom_get_kind to 100%
    [From SVN r7501 (trunk)]

--- tests/tstckintc.c
+++ tests/tstckintc.c
@@ -165,6 +165,53 @@ test2 (void)
   stack = org;
 }
 
+static void
+test_nan_inf_zero (void)
+{
+  mpfr_ptr val;
+  int sign;
+  int kind;
+  char *org = stack;
+
+  val = new_mpfr (MPFR_PREC_MIN);
+
+  mpfr_set_nan (val);
+  kind = (mpfr_custom_get_kind) (val);
+  if (kind != MPFR_NAN_KIND)
+    {
+      printf ("mpfr_custom_get_kind error : ");
+      mpfr_dump (val);
+      printf (" is kind %d instead of %d\n", kind, MPFR_NAN_KIND);
+      exit (1);
+    }
+
+  sign = 1;
+  mpfr_set_inf (val, sign);
+  kind = (mpfr_custom_get_kind) (val);
+  if ((ABS (kind) != MPFR_INF_KIND) || (SIGN (kind) != SIGN (sign)))
+    {
+      printf ("mpfr_custom_get_kind error : ");
+      mpfr_dump (val);
+      printf (" is kind %d instead of %d\n", kind, MPFR_INF_KIND);
+      printf (" have sign %d instead of %d\n", SIGN (kind), SIGN (sign));
+      exit (1);
+    }
+
+  sign = -1;
+  mpfr_set_zero (val, sign);
+  kind = (mpfr_custom_get_kind) (val);
+  if ((ABS (kind) != MPFR_ZERO_KIND) || (SIGN (kind) != SIGN (sign)))
+    {
+      printf ("mpfr_custom_get_kind error : ");
+      mpfr_dump (val);
+      printf (" is kind %d instead of %d\n", kind, MPFR_ZERO_KIND);
+      printf (" have sign %d instead of %d\n", SIGN (kind), SIGN (sign));
+      exit (1);
+    }
+
+  stack = org;
+}
+
 int
 main (void)
 {
@@ -174,6 +221,7 @@ main (void)
     {
       test1 ();
       test2 ();
+      test_nan_inf_zero ();
     }
   tests_end_mpfr ();
   return 0;
