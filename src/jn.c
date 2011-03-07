2decbf86bfc148eeafcc98e5c660a090bbd3dd3e
Vincent Lefèvre <vincent@vinc17.net>
2011-03-07 10:57:44 +0000

    [src/jn.c] Detect incorrect error analysis (that may have been the goal of the assertion corrected in r7534, but this wasn't documented). Added a FIXME comment.
    [From SVN r7536 (trunk)]

--- src/jn.c
+++ src/jn.c
@@ -196,8 +196,10 @@ mpfr_jn (mpfr_ptr res, long n, mpfr_srcptr z, mpfr_rnd_t r)
       MPFR_GROUP_REPREC_3 (g, prec, y, s, t);
       mpfr_pow_ui (t, z, absn, MPFR_RNDN); /* z^|n| */
       mpfr_mul (y, z, z, MPFR_RNDN);       /* z^2 */
+      mpfr_clear_erangeflag ();
       zz = mpfr_get_ui (y, MPFR_RNDU);
-      MPFR_ASSERTN (zz <= ULONG_MAX);
+      /* FIXME: The error analysis is incorrect in case of range error. */
+      MPFR_ASSERTN (! mpfr_erangeflag_p ());
       mpfr_div_2ui (y, y, 2, MPFR_RNDN);   /* z^2/4 */
       mpfr_fac_ui (s, absn, MPFR_RNDN);    /* |n|! */
       mpfr_div (t, t, s, MPFR_RNDN);
