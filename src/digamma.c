a4ecc409906b66eb1cbefc2469ad18c9c043b6ef
Paul Zimmermann <Paul.Zimmermann@inria.fr>
2011-03-18 12:30:22 +0000

    [digamma.c] fixed compiler warning (ISO C90 forbids mixed declarations and code)
    [From SVN r7587 (trunk)]

--- src/digamma.c
+++ src/digamma.c
@@ -279,12 +279,12 @@ int
 mpfr_digamma (mpfr_ptr y, mpfr_srcptr x, mpfr_rnd_t rnd_mode)
 {
   int inex;
+  MPFR_SAVE_EXPO_DECL (expo);
 
   MPFR_LOG_FUNC
     (("x[%Pu]=%.*Rg rnd=%d", mpfr_get_prec(x), mpfr_log_prec, x, rnd_mode),
      ("y[%Pu]=%.*Rg inexact=%d", mpfr_get_prec(y), mpfr_log_prec, y, inex));
 
-  MPFR_SAVE_EXPO_DECL (expo);
 
   if (MPFR_UNLIKELY(MPFR_IS_SINGULAR(x)))
     {
