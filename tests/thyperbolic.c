51d0ecce1756e5c0accbf82bc631f42917910646
Vincent Lefèvre <vincent@vinc17.net>
2011-05-18 14:26:30 +0000

    [tests/thyperbolic.c] Fixed copyright notice.
    [From SVN r7704 (trunk)]

--- tests/thyperbolic.c
+++ tests/thyperbolic.c
@@ -6,12 +6,13 @@ Contributed by the Arenaire and Caramel projects, INRIA.
 This file is part of the GNU MPFR Library.
 
 The GNU MPFR Library is free software; you can redistribute it and/or modify
-it under the terms of the GNU Lesser General Public License as published
-by
-the Free Software Foundation; either version 3 of the License, or (at youroption) any later version.
+it under the terms of the GNU Lesser General Public License as published by
+the Free Software Foundation; either version 3 of the License, or (at your
+option) any later version.
 
 The GNU MPFR Library is distributed in the hope that it will be useful, but
-WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITYor FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public
+WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public
 License for more details.
 
 You should have received a copy of the GNU Lesser General Public License
