6528082779ccf15ee11633a90ec28324f326dc20
Vincent Lefèvre <vincent@vinc17.net>
2011-09-09 11:37:01 +0000

    [src/mp_clz_tab.c] Updated the copyright notice.
    [From SVN r7815 (trunk)]

--- src/mp_clz_tab.c
+++ src/mp_clz_tab.c
@@ -1,25 +1,25 @@
 /* __clz_tab -- support for longlong.h
 
    THE CONTENTS OF THIS FILE ARE FOR INTERNAL USE AND MAY CHANGE
-   INCOMPATIBLY OR DISAPPEAR IN A FUTURE GNU MP RELEASE.
+   INCOMPATIBLY OR DISAPPEAR IN A FUTURE GNU MPFR RELEASE.
 
 Copyright 1991, 1993, 1994, 1996, 1997, 2000, 2001, 2004, 2006, 2007, 2008, 2009, 2010, 2011 Free Software Foundation, Inc.
 
-This file is part of the GNU MP Library.
-Adapted to be used by the GNU MPFR library.
+This file is part of the GNU MPFR Library.
+It has been copied and adapted from the GNU MP Library.
 
-The GNU MP Library is free software; you can redistribute it and/or modify
+The GNU MPFR Library is free software; you can redistribute it and/or modify
 it under the terms of the GNU Lesser General Public License as published by
 the Free Software Foundation; either version 3 of the License, or (at your
 option) any later version.
 
-The GNU MP Library is distributed in the hope that it will be useful, but
+The GNU MPFR Library is distributed in the hope that it will be useful, but
 WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
 or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public
 License for more details.
 
 You should have received a copy of the GNU Lesser General Public License
-along with the GNU MP Library; see the file COPYING.LESSER.  If not, see
+along with the GNU MPFR Library; see the file COPYING.LESSER.  If not, see
 http://www.gnu.org/licenses/ or write to the Free Software Foundation, Inc.,
 51 Franklin St, Fifth Floor, Boston, MA 02110-1301, USA. */
 
