79a24baa97059d5e0697385f69fcee8c096b1a1f
Paul Zimmermann <Paul.Zimmermann@inria.fr>
2011-09-20 08:06:47 +0000

    [cfarm.sh] updated for MPFR 3.1.0-rc1 (with GMP 5.0.2)
    [From SVN r7873 (trunk)]

--- tools/cfarm.sh
+++ tools/cfarm.sh
@@ -2,59 +2,61 @@
 # 1) update the GMP version if needed
 # 2) update the MPFR release candidate
 # 3) ssh gcc100 < cfarm.sh
-/bin/rm -fr gmp-5.0.1*
-wget ftp://ftp.gmplib.org/pub/gmp-5.0.1/gmp-5.0.1.tar.bz2
-tar jxf gmp-5.0.1.tar.bz2
-cd gmp-5.0.1
+/bin/rm -fr gmp-5.0.2*
+wget http://mirror.ibcp.fr/pub/gnu/gmp/gmp-5.0.2.tar.bz2
+tar jxf gmp-5.0.2.tar.bz2
+cd gmp-5.0.2
 ./configure --prefix=$HOME --disable-shared
 make
 make install
 cd $HOME
-/bin/rm -fr mpfr-3.0.1*
-wget http://www.mpfr.org/mpfr-3.0.1/mpfr-3.0.1-rc1.tar.bz2
-tar jxf mpfr-3.0.1-rc1.tar.bz2
-cd mpfr-3.0.1-rc1
+/bin/rm -fr mpfr-3.1.0*
+wget http://www.mpfr.org/mpfr-3.1.0/mpfr-3.1.0-rc1.tar.bz2
+tar jxf mpfr-3.1.0-rc1.tar.bz2
+cd mpfr-3.1.0-rc1
 ./configure --with-gmp=$HOME --prefix=$HOME --disable-shared
 make
 make check
 
-# results with mpfr-3.0.1-rc1.tar.bz2
-# gcc10 All 156 tests passed
-# gcc11 All 156 tests passed
-# gcc12 All 156 tests passed
-# gcc13 All 156 tests passed
-# gcc14 All 156 tests passed
-# gcc15 All 156 tests passed
-# gcc16 All 156 tests passed
-# gcc17 All 156 tests passed
-# gcc20 All 156 tests passed
-# gcc33 All 156 tests passed
-# gcc34 All 156 tests passed
-# gcc35 Connection timed out
-# gcc36 Connection timed out
-# gcc37 All 156 tests passed
-# gcc38 Connection timed out
+# results with mpfr-3.1.0-rc1.tar.bz2
+# gcc10 All 160 tests passed (1 test was not run)
+# gcc11 All 160 tests passed (1 test was not run)
+# gcc12 All 160 tests passed (1 test was not run)
+# gcc13 All 160 tests passed (1 test was not run)
+# gcc14 All 160 tests passed (1 test was not run)
+# gcc15 All 160 tests passed (1 test was not run)
+# gcc16 All 160 tests passed (1 test was not run)
+# gcc17 All 160 tests passed (1 test was not run)
+# gcc20 All 160 tests passed (1 test was not run)
+# gcc33 Connection refused
+# gcc34 Connection refused
+# gcc35 Connection refused
+# gcc36 Connection refused
+# gcc37 Connection refused
+# gcc38 All 160 tests passed (1 test was not run)
 # gcc40 Connection timed out
-# gcc42 All 156 tests passed
-# gcc43 Connection timed out
-# gcc45 All 156 tests passed
-# gcc46 All 156 tests passed
-# gcc47 All 156 tests passed
-# gcc50 Connection timed out
-# gcc51 All 156 tests passed
-# gcc52 All 156 tests passed
+# gcc41 Connection refused
+# gcc42 Connection timed out
+# gcc43 Connection refused
+# gcc45 Connection refused
+# gcc46 All 160 tests passed (1 test was not run)
+# gcc47 All 160 tests passed (1 test was not run)
+# gcc48 All 160 tests passed (1 test was not run)
+# gcc50 Connection refused
+# gcc51 Connection refused
+# gcc52 Connection timed out
 # gcc53 Connection timed out
-# gcc54 All 156 tests passed
+# gcc54 1 of 160 tests failed FAIL: tagm ok with --disable-thread-safe or -O1
 # gcc55 Connection timed out
-# gcc56 Connection timed out
-# gcc57 Connection timed out
-# gcc60 All 156 tests passed
-# gcc61 All 156 tests passed (GMP configured with ABI=1.0)
-# gcc62 All 156 tests passed
-# gcc63 All 156 tests passed
-# gcc64 All 156 tests passed
-# gcc70 All 156 tests passed
-# gcc100 Connection closed by remote host
-# gcc101 Connection closed by remote host
+# gcc56 Connection refused
+# gcc57 Connection refused
+# gcc60 All 160 tests passed (1 test was not run)
+# gcc61 All 160 tests passed (1 test was not run) ABI=1.0 GCC 4.4.1
+# gcc62 1 of 160 tests failed FAIL: tagm
+# gcc63 1 of 160 tests failed FAIL: tagm
+# gcc64 All 160 tests passed (1 test was not run)
+# gcc70 All 160 tests passed (1 test was not run)
+# gcc100 Connection timed out
+# gcc101 Connection timed out
 # gcc200 Connection timed out
 # gcc201 Connection timed out
