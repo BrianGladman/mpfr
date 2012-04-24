ba5e3a73d744881e674c0782afa573a1af7e16c0
Vincent Lefèvre <vincent@vinc17.net>
2012-04-24 15:40:59 +0000

    [src/mpfr-impl.h] Added MPFR_FLAGS_DIVBY0 (missing from r8143).
    [From SVN r8153 (branches/flags)]

--- src/mpfr-impl.h
+++ src/mpfr-impl.h
@@ -341,6 +341,8 @@ __MPFR_DECLSPEC extern const mpfr_t __gmpfr_four;
   do __gmpfr_flags &= MPFR_FLAGS_ALL ^ MPFR_FLAGS_INEXACT; while (0)
 #define MPFR_CLEAR_ERANGEFLAG() \
   do __gmpfr_flags &= MPFR_FLAGS_ALL ^ MPFR_FLAGS_ERANGE; while (0)
+#define MPFR_CLEAR_DIVBY0() \
+  do __gmpfr_flags &= MPFR_FLAGS_ALL ^ MPFR_FLAGS_DIVBY0; while (0)
 #define MPFR_SET_ERANGEFLAG() \
   do __gmpfr_flags |= MPFR_FLAGS_ERANGE; while (0)
 
