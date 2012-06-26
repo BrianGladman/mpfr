478d533b0ac3ac7fa777d63772c19feb83e1b672
Vincent Lefèvre <vincent@vinc17.net>
2012-06-26 17:26:00 +0000

    Forgot to add some files in r8266 (tests that are expected to fail).
    [From SVN r8267 (trunk)]

---
+++ tests/tabort_defalloc2.c
@@ -0,0 +1,41 @@
+/* tabort_defalloc2 -- Test for abort due to reaching out of memory
+
+Copyright 2012 Free Software Foundation, Inc.
+Contributed by the AriC and Caramel projects, INRIA.
+
+This file is part of the GNU MPFR Library.
+
+The GNU MPFR Library is free software; you can redistribute it and/or modify
+it under the terms of the GNU Lesser General Public License as published by
+the Free Software Foundation; either version 3 of the License, or (at your
+option) any later version.
+
+The GNU MPFR Library is distributed in the hope that it will be useful, but
+WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public
+License for more details.
+
+You should have received a copy of the GNU Lesser General Public License
+along with the GNU MPFR Library; see the file COPYING.LESSER.  If not, see
+http://www.gnu.org/licenses/ or write to the Free Software Foundation, Inc.,
+51 Franklin St, Fifth Floor, Boston, MA 02110-1301, USA. */
+
+#define MPFR_NEED_LONGLONG_H
+#include "mpfr-test.h"
+
+int
+main (int argc, char **argv)
+{
+  char *ptr;
+
+  tests_start_mpfr ();
+  tests_expect_abort ();
+
+  printf ("[tabort_defalloc2] Check for good handling of abort"
+          " in memory function.\n");
+  ptr = (*__gmp_allocate_func) (128);
+  ptr = (*__gmp_reallocate_func) (ptr, 128, (size_t) -1);
+
+  tests_end_mpfr ();
+  return -1; /* Should not be executed */
+}
