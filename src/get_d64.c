5a64789523291aca1d35add22768e983c812e5e2
Vincent Lefèvre <vincent@vinc17.net>
2012-10-17 22:56:04 +0000

    [src/get_d64.c] Added a const (thanks to Patrick Pélissier).
    [From SVN r8424 (trunk)]

--- src/get_d64.c
+++ src/get_d64.c
@@ -41,7 +41,7 @@ http://www.gnu.org/licenses/ or write to the Free Software Foundation, Inc.,
 #endif
 
 #ifdef DPD_FORMAT
-static int T[1000] = {
+static const int T[1000] = {
   0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 32,
   33, 34, 35, 36, 37, 38, 39, 40, 41, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57,
   64, 65, 66, 67, 68, 69, 70, 71, 72, 73, 80, 81, 82, 83, 84, 85, 86, 87, 88,
