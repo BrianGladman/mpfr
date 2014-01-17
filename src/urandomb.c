e6bc6780e7fdf4d5e157dee61bad63522b1c195e
Paul Zimmermann <Paul.Zimmermann@inria.fr>
2014-01-17 15:51:49 +0000

    mpn_copyd (rp, sp, n) should be used when rp >= sp
    [From SVN r8763 (trunk)]

--- src/urandomb.c
+++ src/urandomb.c
@@ -99,7 +99,7 @@ mpfr_urandomb (mpfr_ptr rop, gmp_randstate_t rstate)
       if (cnt != 0)
         mpn_lshift (rp + k, rp, nlimbs, cnt);
       else if (k != 0)
-        mpn_copyi (rp + k, rp, nlimbs);
+        mpn_copyd (rp + k, rp, nlimbs);
       if (k != 0)
         MPN_ZERO (rp, k);
     }
