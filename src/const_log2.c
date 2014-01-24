5edf0aca4d64acc22ea9d48e64f52efa2f675d1a
Vincent Lefèvre <vincent@vinc17.net>
2014-01-24 02:39:30 +0000

    [src/const_log2.c] Optimization by using GROUP allocation (patch by Patrick Pélissier).
    [From SVN r8889 (trunk)]

--- src/const_log2.c
+++ src/const_log2.c
@@ -119,14 +119,13 @@ mpfr_const_log2_internal (mpfr_ptr x, mpfr_rnd_t rnd_mode)
   int inexact;
   int ok = 1; /* ensures that the 1st try will give correct rounding */
   unsigned long lgN, i;
-  MPFR_ZIV_DECL (loop);
+  MPFR_GROUP_DECL(group);
+  MPFR_TMP_DECL(marker);
+  MPFR_ZIV_DECL(loop);
 
   MPFR_LOG_FUNC (
     ("rnd_mode=%d", rnd_mode),
     ("x[%Pu]=%.*Rg inex=%d", mpfr_get_prec(x), mpfr_log_prec, x, inexact));
-
-  mpfr_init2 (t, MPFR_PREC_MIN);
-  mpfr_init2 (q, MPFR_PREC_MIN);
 
   if (n < 1253)
     w = n + 10; /* ensures correct rounding for the four rounding modes,
@@ -145,6 +144,9 @@ mpfr_const_log2_internal (mpfr_ptr x, mpfr_rnd_t rnd_mode)
       ok = 0;
     }
 
+  MPFR_TMP_MARK(marker);
+  MPFR_GROUP_INIT_2(group, w, t, q);
+
   MPFR_ZIV_INIT (loop, w);
   for (;;)
     {
@@ -156,7 +158,7 @@ mpfr_const_log2_internal (mpfr_ptr x, mpfr_rnd_t rnd_mode)
       MPFR_ASSERTD(w >= 3 && N >= 2);
 
       lgN = MPFR_INT_CEIL_LOG2 (N) + 1;
-      T  = (mpz_t *) (*__gmp_allocate_func) (3 * lgN * sizeof (mpz_t));
+      T  = (mpz_t *) MPFR_TMP_ALLOC (3 * lgN * sizeof (mpz_t));
       P  = T + lgN;
       Q  = T + 2*lgN;
       for (i = 0; i < lgN; i++)
@@ -168,9 +170,6 @@ mpfr_const_log2_internal (mpfr_ptr x, mpfr_rnd_t rnd_mode)
 
       S (T, P, Q, 0, N, 0);
 
-      mpfr_set_prec (t, w);
-      mpfr_set_prec (q, w);
-
       mpfr_set_z (t, T[0], MPFR_RNDN);
       mpfr_set_z (q, Q[0], MPFR_RNDN);
       mpfr_div (t, t, q, MPFR_RNDN);
@@ -181,20 +180,20 @@ mpfr_const_log2_internal (mpfr_ptr x, mpfr_rnd_t rnd_mode)
           mpz_clear (P[i]);
           mpz_clear (Q[i]);
         }
-      (*__gmp_free_func) (T, 3 * lgN * sizeof (mpz_t));
 
       if (MPFR_LIKELY (ok != 0
                        || mpfr_can_round (t, w - 2, MPFR_RNDN, rnd_mode, n)))
         break;
 
       MPFR_ZIV_NEXT (loop, w);
+      MPFR_GROUP_REPREC_2(group, w, t, q);
     }
   MPFR_ZIV_FREE (loop);
 
   inexact = mpfr_set (x, t, rnd_mode);
 
-  mpfr_clear (t);
-  mpfr_clear (q);
+  MPFR_GROUP_CLEAR(group);
+  MPFR_TMP_FREE(marker);
 
   return inexact;
 }
