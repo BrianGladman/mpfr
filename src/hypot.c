df31099503ce336e4434ce9d1735d4c13ff49879
Vincent Lefèvre <vincent@vinc17.net>
2014-02-12 02:01:25 +0000

    [src/hypot.c] Reverted changeset r8950 (MPFR_SET_EXP is OK).
    [From SVN r8951 (branches/set_exp)]

--- src/hypot.c
+++ src/hypot.c
@@ -110,7 +110,7 @@ mpfr_hypot (mpfr_ptr z, mpfr_srcptr x, mpfr_srcptr y, mpfr_rnd_t rnd_mode)
             }
           else
             {
-              MPFR_EXP (z) = Ex;
+              MPFR_SET_EXP (z, Ex);
               MPFR_SET_SIGN (z, 1);
               MPFR_RNDRAW_GEN (inexact, z, MPFR_MANT (x), N, rnd_mode, 1,
                                goto addoneulp,
