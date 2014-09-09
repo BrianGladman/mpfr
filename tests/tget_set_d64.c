9626b19da88f53ac85934b5f6c29c0bb56bbc9a7
Vincent Lefèvre <vincent@vinc17.net>
2014-09-09 00:17:47 +0000

    [tests] Corrections for builds with "config.h".
    [From SVN r9204 (trunk)]

--- tests/tget_set_d64.c
+++ tests/tget_set_d64.c
@@ -20,6 +20,10 @@ along with the GNU MPFR Library; see the file COPYING.LESSER.  If not, see
 http://www.gnu.org/licenses/ or write to the Free Software Foundation, Inc.,
 51 Franklin St, Fifth Floor, Boston, MA 02110-1301, USA. */
 
+#ifdef HAVE_CONFIG_H
+# include "config.h"       /* for a build within gmp */
+#endif
+
 #ifdef MPFR_WANT_DECIMAL_FLOATS
 
 #include "mpfr-test.h"
