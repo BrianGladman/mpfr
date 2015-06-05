6522c08dcbf392707d5b6d75523716c27a33f8ca
Vincent Lefèvre <vincent@vinc17.net>
2015-06-05 00:13:04 +0000

    [tests/tpow_all.c] Better flags output in error message.
    [From SVN r9509 (trunk)]

--- tests/tpow_all.c
+++ tests/tpow_all.c
@@ -107,16 +107,19 @@ cmpres (int spx, const void *px, const char *sy, mpfr_rnd_t rnd,
   printf ("Expected ");
   if (z1 == NULL)
     {
-      printf ("pure FP value, flags = %u\n", flags1);
+      printf ("pure FP value, flags =");
+      flags_out (flags1);
     }
   else
     {
       mpfr_out_str (stdout, 16, 0, z1, MPFR_RNDN);
-      printf (", inex = %d, flags = %u\n", SIGN (inex1), flags1);
+      printf (", inex = %d,\n         flags =", SIGN (inex1));
+      flags_out (flags1);
     }
   printf ("Got      ");
   mpfr_out_str (stdout, 16, 0, z2, MPFR_RNDN);
-  printf (", inex = %d, flags = %u\n", SIGN (inex2), flags2);
+  printf (", inex = %d,\n         flags =", SIGN (inex2));
+  flags_out (flags2);
   if (all_cmpres_errors != 0)
     all_cmpres_errors = -1;
   else
