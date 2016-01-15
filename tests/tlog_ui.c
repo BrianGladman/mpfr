eee9a0d3eaed6df2dfd28bc4658f5339a574a343
Vincent Lefèvre <vincent@vinc17.net>
2016-01-15 11:14:08 +0000

    [tests/tlog_ui.c] Added tests for n around LONG_MAX.
    [From SVN r9813 (trunk)]

--- tests/tlog_ui.c
+++ tests/tlog_ui.c
@@ -99,12 +99,14 @@ main (int argc, char *argv[])
       yprec = prec + 20;
       mpfr_set_prec (y, yprec);
 
-      for (m = 2; m < 70; m++)
+      for (m = 2; m < 100; m++)
         RND_LOOP (rnd)
           {
             /* Start with n = 2 to 49 (mpfr_can_round would fail for n < 2),
-               then ULONG_MAX down to ULONG_MAX-19. */
-            n = m < 50 ? m : ULONG_MAX - (m - 50);
+               then around LONG_MAX, then ULONG_MAX down to ULONG_MAX-19. */
+            n = (m < 50 ? m :
+                 m < 80 ? (unsigned long) LONG_MAX + m - 65 :
+                 ULONG_MAX - (m - 80));
             inex = mpfr_set_ui (v, n, MPFR_RNDN);
             MPFR_ASSERTN (inex == 0);
             mpfr_log (y, v, MPFR_RNDN);
