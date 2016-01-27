119f430beae2b6cb4189072938e344145a6022c2
Paul Zimmermann <Paul.Zimmermann@inria.fr>
2016-01-27 12:03:22 +0000

    also print the rounded value
    [From SVN r9872 (trunk)]

--- examples/can_round.c
+++ examples/can_round.c
@@ -28,28 +28,40 @@ http://www.gnu.org/licenses/ or write to the Free Software Foundation, Inc.,
 int
 main (void)
 {
-  mpfr_t x;
+  mpfr_t x, y;
+  mpfr_prec_t px = 53, py = 50;
   mpfr_rnd_t r1, r2;
   int ok;
 
-  /* Given an approximation of Pi to 53 bits computed with rounding mode r1,
+  /* Given an approximation of Pi to px bits computed with rounding mode r1,
      we call mpfr_can_round() to see if we can deduced the correct rounding
-     of Pi to 50 bits with rounding mode r2.
+     of Pi to py bits with rounding mode r2.
      The error is at most 1 ulp for directed rounding, and 1/2 ulp for rounding
      to nearest. This translates into err = prec(x) for directed rounding,
      and err = prec(x) + 1 for rounding to nearest. */
-  mpfr_init2 (x, 53);
+  mpfr_init2 (x, px);
+  mpfr_init2 (y, py);
   for (r1 = 0; r1 < 4; r1++)
     {
       mpfr_const_pi (x, r1);
-      mpfr_printf ("rnd1=%s approx=%Rb\n", mpfr_print_rnd_mode (r1), x);
+      printf ("r1=%s approx=", mpfr_print_rnd_mode (r1));
+      mpfr_out_str (stdout, 2, 0, x, MPFR_RNDN);
+      printf ("\n");
       for (r2 = 0; r2 < 4; r2++)
         {
           ok = mpfr_can_round (x, mpfr_get_prec (x) + (r1 == MPFR_RNDN),
-                               r1, r2, 50);
-          printf ("rnd2=%s can_round=%d\n", mpfr_print_rnd_mode (r2), ok);
+                               r1, r2, py);
+          printf ("r2=%s ok=%d", mpfr_print_rnd_mode (r2), ok);
+          if (ok)
+            {
+              mpfr_set (y, x, r2);
+              printf ("   ");
+              mpfr_out_str (stdout, 2, 0, y, MPFR_RNDN);
+            }
+          printf ("\n");
         }
     }
   mpfr_clear (x);
+  mpfr_clear (y);
   return 0;
 }
