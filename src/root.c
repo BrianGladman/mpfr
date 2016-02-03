29d1336dfb731da6692c228559bf30a379760373
Vincent Lefèvre <vincent@vinc17.net>
2016-02-03 11:21:18 +0000

    [src/root.c] Added a FIXME comment.
    [From SVN r9923 (trunk)]

--- src/root.c
+++ src/root.c
@@ -136,6 +136,8 @@ mpfr_root (mpfr_ptr y, mpfr_srcptr x, unsigned long k, mpfr_rnd_t rnd_mode)
   r = e % (mpfr_exp_t) k;
   if (r < 0)
     r += k; /* now r = e (mod k) with 0 <= e < r */
+  /* FIXME: The above comment is incorrect: e can be negative.
+     When corrected, add corresponding assertions. */
   /* x = (m*2^r) * 2^(e-r) where e-r is a multiple of k */
 
   MPFR_MPZ_SIZEINBASE2 (size_m, m);
