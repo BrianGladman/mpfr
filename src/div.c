BrianGladman/mpfr

commit 3ad5bec1db96c21557d3ee84a758966232c93f51

now mpfr_round_p() always requires normalized input
git-svn-id: https://scm.gforge.inria.fr/anonscm/svn/mpfr/trunk@9902 280ebfd0-de03-0410-8827-d642c229c3f4

src/div.c
@@ -460,22 +460,18 @@ mpfr_div (mpfr_ptr q, mpfr_srcptr u, mpfr_srcptr v, mpfr_rnd_t rnd_mode)
          cf algorithms.tex */
 
       p = n * GMP_NUMB_BITS - MPFR_INT_CEIL_LOG2 (2 * n + 2);
-      /* If qh is 1, then we need only PREC(q)-1 bits of {qp,n},
-         if rnd=RNDN, we need to be able to round with a directed rounding
-            and one more bit.
-         Note: if qh=1, the operand {qp, n} given to mpfr_round_p might not
-         be normalized: the most significant bit of qp[n-1] might be zero. */
+      /* If rnd=RNDN, we need to be able to round with a directed rounding
+         and one more bit. */
+      if (qh == 1)
+        {
+          mpn_rshift (qp, qp, n, 1);
+          qp[n - 1] ^= MPFR_LIMB_HIGHBIT;
+        }
       if (MPFR_LIKELY (mpfr_round_p (qp, n, p,
-                                 MPFR_PREC(q) + (rnd_mode == MPFR_RNDN) - qh)))
+                                     MPFR_PREC(q) + (rnd_mode == MPFR_RNDN))))
         {
           /* we can round correctly whatever the rounding mode */
-          if (qh == 0)
-            MPN_COPY (q0p, qp + 1, q0size);
-          else
-            {
-              mpn_rshift (q0p, qp + 1, q0size, 1);
-              q0p[q0size - 1] ^= MPFR_LIMB_HIGHBIT;
-            }
+          MPN_COPY (q0p, qp + 1, q0size);
           q0p[0] &= ~MPFR_LIMB_MASK(sh); /* put to zero low sh bits */
 
           if (rnd_mode == MPFR_RNDN) /* round to nearest */

src/round_p.c
@@ -30,22 +30,20 @@ mpfr_round_p (mp_limb_t *bp, mp_size_t bn, mpfr_exp_t err0, mpfr_prec_t prec)
 {
   int i1, i2;
 
+  MPFR_ASSERTN(bp[bn - 1] & MPFR_LIMB_HIGHBIT);
+
   i1 = mpfr_round_p_2 (bp, bn, err0, prec);
 
-  /* mpfr_round_p accepts non-normalized inputs, but not mpfr_can_round_raw.
-     So, compare with mpfr_can_round_raw only on normalized inputs. */
-  if (bp[bn - 1] & MPFR_LIMB_HIGHBIT)
+  /* compare with mpfr_can_round_raw */
+  i2 = mpfr_can_round_raw (bp, bn, MPFR_SIGN_POS, err0,
+                           MPFR_RNDN, MPFR_RNDZ, prec);
+  if (i1 != i2)
     {
-      i2 = mpfr_can_round_raw (bp, bn, MPFR_SIGN_POS, err0,
-                               MPFR_RNDN, MPFR_RNDZ, prec);
-      if (i1 != i2)
-        {
-          fprintf (stderr, "mpfr_round_p(%d) != mpfr_can_round(%d)!\n"
-                   "bn = %lu, err0 = %ld, prec = %lu\nbp = ", i1, i2,
-                   (unsigned long) bn, (long) err0, (unsigned long) prec);
-          gmp_fprintf (stderr, "%NX\n", bp, bn);
-          MPFR_ASSERTN (0);
-        }
+      fprintf (stderr, "mpfr_round_p(%d) != mpfr_can_round(%d)!\n"
+               "bn = %lu, err0 = %ld, prec = %lu\nbp = ", i1, i2,
+               (unsigned long) bn, (long) err0, (unsigned long) prec);
+      gmp_fprintf (stderr, "%NX\n", bp, bn);
+      MPFR_ASSERTN (0);
     }
 
   return i1;
@@ -58,12 +56,6 @@ mpfr_round_p (mp_limb_t *bp, mp_size_t bn, mpfr_exp_t err0, mpfr_prec_t prec)
  * with error at most equal to 2^(EXP(b)-err0) (`err0' bits of b are known)
  * of direction unknown, check if we can round b toward zero with
  * precision prec.
- *
- * Note: on some cases (for example in mpfr_div), we call mpfr_round_p() with
- * an implicit extra bit which would correspond to bp[bn], in which case
- * {bp, bn} is not necessarily normalized (the most significant bit of bp[bn-1]
- * might not be set). In that case, the meaning of EXP(b), err0 and prec is as
- * if {bp, bn} were normalized.
  */
 int
 mpfr_round_p (mp_limb_t *bp, mp_size_t bn, mpfr_exp_t err0, mpfr_prec_t prec)
@@ -73,8 +65,7 @@ mpfr_round_p (mp_limb_t *bp, mp_size_t bn, mpfr_exp_t err0, mpfr_prec_t prec)
   mp_limb_t tmp, mask;
   int s;
 
-  /* Note: {bp, bn} is not always normalized, for example when called from
-     mpfr_div. */
+  MPFR_ASSERTD(bp[bn - 1] & MPFR_LIMB_HIGHBIT);
 
   err = (mpfr_prec_t) bn * GMP_NUMB_BITS;
   if (MPFR_UNLIKELY (err0 <= 0 || (mpfr_uexp_t) err0 <= prec || prec >= err))

commit 06f33db73c6cf371ffb7afdbe2b6ae3bd6fa6da4

[src/div.c] Fixed bug introduced in r9902 and visible in the tests on 32-bit machines.
[From SVN r9932 (trunk)]

src/div.c
@@ -465,7 +465,7 @@ mpfr_div (mpfr_ptr q, mpfr_srcptr u, mpfr_srcptr v, mpfr_rnd_t rnd_mode)
       if (qh == 1)
         {
           mpn_rshift (qp, qp, n, 1);
-          qp[n - 1] ^= MPFR_LIMB_HIGHBIT;
+          qp[n - 1] |= MPFR_LIMB_HIGHBIT;
         }
       if (MPFR_LIKELY (mpfr_round_p (qp, n, p,
                                      MPFR_PREC(q) + (rnd_mode == MPFR_RNDN))))
@@ -479,15 +479,10 @@ mpfr_div (mpfr_ptr q, mpfr_srcptr u, mpfr_srcptr v, mpfr_rnd_t rnd_mode)
               /* we know we can round, thus we are never in the even rule case:
                  if the round bit is 0, we truncate
                  if the round bit is 1, we add 1 */
-              if (qh == 0)
-                {
-                  if (sh > 0)
-                    round_bit = (qp[1] >> (sh - 1)) & 1;
-                  else
-                    round_bit = qp[0] >> (GMP_NUMB_BITS - 1);
-                }
-              else /* qh = 1 */
-                round_bit = (qp[1] >> sh) & 1;
+              if (sh > 0)
+                round_bit = (qp[1] >> (sh - 1)) & 1;
+              else
+                round_bit = qp[0] >> (GMP_NUMB_BITS - 1);
               if (round_bit == 0)
                 {
                   inex = -1;