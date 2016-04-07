60bf66df461926c27d5e072599f23773d6b4a418
Vincent Lefèvre <vincent@vinc17.net>
2016-04-07 15:10:30 +0000

    [doc/mpfr.texi] Document mpfr_dump (which has always been there). [src/dump.c] Added a comment (with TODO).
    [From SVN r10253 (trunk)]

--- src/dump.c
+++ src/dump.c
@@ -22,6 +22,25 @@ http://www.gnu.org/licenses/ or write to the Free Software Foundation, Inc.,
 
 #include "mpfr-impl.h"
 
+/* mpfr_dump is mainly for debugging purpose. It outputs a MPFR number
+ * in some unspecified format, then a newline character. This function
+ * is part of the API, but the output format may change without breaking
+ * the ABI.
+ *
+ * TODO: Since this is for debugging, it should support invalid data,
+ * such as:
+ *   - out-of-range exponents (with some form of warning);
+ *   - numbers that are not normalized (MSB = 0);
+ *   - numbers with trailing bits that are not all zero's
+ *     (e.g. output in square brackets in such a case).
+ * The format may depend on MPFR_* environment variables. For instance,
+ * some environment variables could specify prefix and suffix strings
+ * to colorize parts of the output that correspond to invalid data.
+ *
+ * Moreover, get rid of mpfr_print_binary? It should be replaced
+ * either by mpfr_dump or by mpfr_out_str in the MPFR code.
+ */
+
 void
 mpfr_dump (mpfr_srcptr u)
 {
