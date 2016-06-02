594aa0bf0a1d5aba70426a12df6e0deab8789617
Paul Zimmermann <Paul.Zimmermann@inria.fr>
2016-06-02 16:20:26 +0000

    RNDF: fixed texp2
    [From SVN r10410 (branches/faithful)]

--- tests/texp2.c
+++ tests/texp2.c
@@ -185,7 +185,7 @@ overflowed_exp2_0 (void)
                     err = 1;
                   }
               }
-            else
+            else if (rnd != MPFR_RNDF)
               {
                 if (inex <= 0)
                   {
