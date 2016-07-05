0226f62abb439d60254ddc520d793301044066d2
Paul Zimmermann <Paul.Zimmermann@inria.fr>
2016-07-05 15:07:58 +0000

    [src/sub1sp.c] added comment
    [From SVN r10559 (trunk)]

--- src/sub1sp.c
+++ src/sub1sp.c
@@ -291,6 +291,9 @@ mpfr_sub1sp1 (mpfr_ptr a, mpfr_srcptr b, mpfr_srcptr c, mpfr_rnd_t rnd_mode,
       if (ap[0] == 0)
         {
           ap[0] = MPFR_LIMB_HIGHBIT;
+          /* Note: bx+1 cannot exceed __gmpfr_emax, since |a| <= |b|, thus
+             bx+1 is at most equal to the original exponent of b. */
+             
           MPFR_SET_EXP (a, bx + 1);
         }
       MPFR_RET(MPFR_SIGN(a));
