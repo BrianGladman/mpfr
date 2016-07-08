bad723b64cf200a2deca700ef5503cd582b67508
Vincent Lefèvre <vincent@vinc17.net>
2016-07-08 11:57:26 +0000

    [src/sub1sp.c] Clarified comment (it's better not to mention underflow before rounding because it is not considered at all in MPFR).
    [From SVN r10600 (trunk)]

--- src/sub1sp.c
+++ src/sub1sp.c
@@ -265,10 +265,9 @@ mpfr_sub1sp1 (mpfr_ptr a, mpfr_srcptr b, mpfr_srcptr c, mpfr_rnd_t rnd_mode,
 
   /* Warning: MPFR considers underflow *after* rounding with an unbounded
      exponent range. However since b and c have same precision p, they are
-     multiples of 2^(__gmpfr_emin-p), likewise for b-c. Thus if bx < emin,
-     the subtraction is exact, and it is not possible that rounding with an
-     unbounded exponent range increases the exponent to bx+1 (in which case we
-     would signal a false underflow by dealing with underflow before rounding). */
+     multiples of 2^(emin-p), likewise for b-c. Thus if bx < emin, the
+     subtraction (with an unbounded exponent range) is exact, so that bx is
+     also the exponent after rounding with an unbounded exponent range. */
   if (MPFR_UNLIKELY(bx < __gmpfr_emin))
     {
       /* for RNDN, mpfr_underflow always rounds away, thus for |a| <= 2^(emin-2)
