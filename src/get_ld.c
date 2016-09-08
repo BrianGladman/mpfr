589f08c1b73ca4660a1db6b05d92ffc758998f85
Vincent Lefèvre <vincent@vinc17.net>
2016-09-08 16:15:18 +0000

    [src/get_ld.c] Updated FIXME (mention subnormals).
    [From SVN r10828 (trunk)]

--- src/get_ld.c
+++ src/get_ld.c
@@ -38,7 +38,9 @@ mpfr_get_ld (mpfr_srcptr x, mpfr_rnd_t rnd_mode)
 
 /* FIXME: This code assumes that a 64-bit precision is available while
    the processor may be configured to round on 53 bits or less. This
-   yields a failure in tset_ld on NetBSD 5.1 (the test is correct). */
+   yields a failure in tset_ld on NetBSD 5.1 (the test is correct).
+   Warning! This also has an effect on subnormals: the last 11 bits
+   must still be 0. */
 
 /* special code for IEEE 754 little-endian extended format */
 long double
