47a9067403b2b60e0eab3c55e4203c6b5676a83a
Vincent Lefèvre <vincent@vinc17.net>
2016-09-21 11:12:23 +0000

    [src/mpfr-impl.h] In the MPFR_RET macro, avoid the use of an integer in a boolean context (even though that this is not wrong in C). Fix after a bug report from David Binderman. Explanations:   https://sympa.inria.fr/sympa/arc/mpfr/2016-09/msg00028.html
    [From SVN r10879 (trunk)]

--- src/mpfr-impl.h
+++ src/mpfr-impl.h
@@ -991,7 +991,7 @@ typedef uintmax_t mpfr_ueexp_t;
    following two macros, unless the flag comes from another function
    returning the ternary inexact value */
 #define MPFR_RET(I) return \
-  (I) ? ((__gmpfr_flags |= MPFR_FLAGS_INEXACT), (I)) : 0
+  (I) != 0 ? ((__gmpfr_flags |= MPFR_FLAGS_INEXACT), (I)) : 0
 #define MPFR_RET_NAN return (__gmpfr_flags |= MPFR_FLAGS_NAN), 0
 
 #define SIGN(I) ((I) < 0 ? -1 : (I) > 0)
