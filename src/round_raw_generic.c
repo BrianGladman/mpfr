5210e0e82fb147f340adc72a3ee80642d777bf25
Vincent Lefèvre <vincent@vinc17.net>
2017-01-09 10:52:17 +0000

    [src/round_raw_generic.c] Added MPFR_RNDF special support (seen as MPFR_RNDZ since this is faster, with *inexp = 0).
    [From SVN r11163 (branches/faithful)]

--- src/round_raw_generic.c
+++ src/round_raw_generic.c
@@ -68,7 +68,7 @@ mpfr_round_raw_generic(
 {
   mp_size_t xsize, nw;
   mp_limb_t himask, lomask, sb;
-  int rw;
+  int rw, new_use_inexp;
 #if flag == 0
   int carry;
 #endif
@@ -80,9 +80,22 @@ mpfr_round_raw_generic(
     MPFR_ASSERTD(inexp != ((int*) 0));
   MPFR_ASSERTD(neg == 0 || neg == 1);
 
-  if (flag && !use_inexp &&
-      (xprec <= yprec || MPFR_IS_LIKE_RNDZ (rnd_mode, neg)))
-    return 0;
+  if (rnd_mode == MPFR_RNDF)
+    {
+      if (use_inexp)
+        *inexp = 0;  /* make sure it has a valid value */
+      if (flag)
+        return 0;
+      rnd_mode = MPFR_RNDZ;  /* faster */
+      new_use_inexp = 0;
+    }
+  else
+    {
+      if (flag && !use_inexp &&
+          (xprec <= yprec || MPFR_IS_LIKE_RNDZ (rnd_mode, neg)))
+        return 0;
+      new_use_inexp = use_inexp;
+    }
 
   xsize = MPFR_PREC2LIMBS (xprec);
   nw = yprec / GMP_NUMB_BITS;
@@ -104,7 +117,7 @@ mpfr_round_raw_generic(
       return 0;
     }
 
-  if (use_inexp || !MPFR_IS_LIKE_RNDZ(rnd_mode, neg))
+  if (new_use_inexp || !MPFR_IS_LIKE_RNDZ(rnd_mode, neg))
     {
       mp_size_t k = xsize - nw - 1;
 
