1ca260e37dc1650577951c72325596a096ac2800
Vincent Lefèvre <vincent@vinc17.net>
2017-02-08 13:28:21 +0000

    [tests/tsub1sp.c] Fix: C++ compatibility.
    [From SVN r11282 (trunk)]

--- tests/tsub1sp.c
+++ tests/tsub1sp.c
@@ -167,7 +167,7 @@ compare_sub_sub1sp (void)
   mpfr_prec_t p;
   unsigned long d;
   int i, inex_ref, inex;
-  mpfr_rnd_t r;
+  int r;
 
   for (p = 1; p <= 3*GMP_NUMB_BITS; p++)
     {
@@ -194,14 +194,14 @@ compare_sub_sub1sp (void)
               {
                 /* increase the precision of b to ensure sub1sp is not used */
                 mpfr_prec_round (b, p + 1, MPFR_RNDN);
-                inex_ref = mpfr_sub (a_ref, b, c, r);
+                inex_ref = mpfr_sub (a_ref, b, c, (mpfr_rnd_t) r);
                 inex = mpfr_prec_round (b, p, MPFR_RNDN);
                 MPFR_ASSERTN(inex == 0);
-                inex = mpfr_sub1sp (a, b, c, r);
+                inex = mpfr_sub1sp (a, b, c, (mpfr_rnd_t) r);
                 if (inex != inex_ref)
                   {
                     printf ("mpfr_sub and mpfr_sub1sp differ for r=%s\n",
-                            mpfr_print_rnd_mode (r));
+                            mpfr_print_rnd_mode ((mpfr_rnd_t) r));
                     printf ("b="); mpfr_dump (b);
                     printf ("c="); mpfr_dump (c);
                     printf ("expected inex=%d and ", inex_ref);
