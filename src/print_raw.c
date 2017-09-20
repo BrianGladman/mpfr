10c5c92f399a1b62ac4564297e893f9c43a5f61f
Vincent Lefèvre <vincent@vinc17.net>
2017-09-20 13:57:58 +0000

    [src/print_raw.c] Changes in mpfr_print_mant_binary():   * Output a ']' when a '[' was output earlier.   * Correction: putchar() takes an int (or unsigned char), not a char.   * GNU coding style.
    [From SVN r11757 (trunk)]

--- src/print_raw.c
+++ src/print_raw.c
@@ -1,5 +1,5 @@
 /* mpfr_print_binary -- print the internal binary representation of a
-                     floating-point number
+   floating-point number (for the tests and debugging purpose)
 
 Copyright 1999-2017 Free Software Foundation, Inc.
 Contributed by the AriC and Caramba projects, INRIA.
@@ -75,25 +75,24 @@ mpfr_print_binary (mpfr_srcptr x)
 }
 
 void
-mpfr_print_mant_binary(const char *str, const mp_limb_t *p, mpfr_prec_t r)
+mpfr_print_mant_binary (const char *str, const mp_limb_t *p, mpfr_prec_t r)
 {
   int i;
   mpfr_prec_t count = 0;
-  char c;
   mp_size_t n = MPFR_PREC2LIMBS (r);
 
-  printf("%s ", str);
-  for(n-- ; n>=0 ; n--)
+  printf ("%s ", str);
+  for (n-- ; n >= 0 ; n--)
     {
-      for(i = GMP_NUMB_BITS-1 ; i >=0 ; i--)
+      for (i = GMP_NUMB_BITS - 1 ; i >=0 ; i--)
         {
-          c = (p[n] & (MPFR_LIMB_ONE << i)) ? '1' : '0';
-          putchar(c);
-          count++;
-          if (count == r)
-            putchar('[');
+          putchar ((p[n] & (MPFR_LIMB_ONE << i)) ? '1' : '0');
+          if (++count == r)
+            putchar ('[');
         }
-      putchar('.');
+      putchar ('.');
     }
-  putchar('\n');
+  if (count >= r)
+    putchar (']');
+  putchar ('\n');
 }
