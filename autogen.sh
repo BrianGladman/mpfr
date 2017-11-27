5f88e1a795291de6e6352b1b51c1f92eb924809f
Vincent Lefèvre <vincent@vinc17.net>
2017-11-27 10:31:33 +0000

    Updated autogen.sh so that doc/texinfo.tex isn't replaced.
    [From SVN r11841 (trunk)]

--- autogen.sh
+++ autogen.sh
@@ -1,23 +1,28 @@
 #!/bin/sh
 
-# "autoreconf -f" will clobber our INSTALL file with a generic one if we
-# don't move it out of the way.
+# "autoreconf -f" will clobber some of our files with generic ones if we
+# don't move them out of the way (see $files below).
 
 # EXIT and signals that correspond to SIGHUP, SIGINT, SIGQUIT and SIGTERM.
 signals="0 1 2 3 15"
 
+files="INSTALL doc/texinfo.tex"
+
 cleanup()
 {
   trap '' $signals
-  if [ -f INSTALL.$$.tmp ]; then
-    echo "$0: restoring the INSTALL file" >&2
-    mv -f INSTALL.$$.tmp INSTALL
-  fi
+  for i in $files
+  do
+    if [ -f "$i.$$.tmp" ]; then
+      echo "$0: restoring the $i file" >&2
+      mv -f "$i.$$.tmp" "$i"
+    fi
+  done
 }
 
-rm -f INSTALL.$$.tmp
+for i in $files; do rm -f -- "$i.$$.tmp"; done
 trap cleanup $signals
-mv -f INSTALL INSTALL.$$.tmp
+for i in $files; do mv -f -- "$i" "$i.$$.tmp"; done
 
 autoreconf -v -f -i --warnings=all,error
 status=$?
