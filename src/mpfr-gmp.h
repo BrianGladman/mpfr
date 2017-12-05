1a8c48e45895c2aa7ffcc3d20c6cfe8c9c29ee8e
Paul Zimmermann <Paul.Zimmermann@inria.fr>
2017-12-05 09:44:49 +0000

    fixed bug in replacement function for copyi and copyd (found with mini-gmp)
    [From SVN r11900 (trunk)]

--- src/mpfr-gmp.h
+++ src/mpfr-gmp.h
@@ -432,12 +432,13 @@ typedef struct {mp_limb_t inv32;} mpfr_pi1_t;
 #endif
 
 /* mpn_copyi and mpn_copyd are new exported functions in GMP 5.
-   Defining them to memmove works in overlap cases. */
+   Defining them to memmove works in overlap cases.
+   Warning: mini-gmp does not define GMP_VERSION (as of gmp-6.1.2). */
 #if !__MPFR_GMP(5,0,0)
 # undef  mpn_copyi
-# define mpn_copyi memmove
+# define mpn_copyi(d,s,n) memmove(d,s,n*sizeof(mp_limb_t))
 # undef  mpn_copyd
-# define mpn_copyd memmove
+# define mpn_copyd(d,s,n) memmove(d,s,n*sizeof(mp_limb_t))
 #endif
 
 /* The following macro is copied from GMP-6.1.1, file gmp-impl.h,
