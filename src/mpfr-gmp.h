9632770e5806e05dee1d0db35e44a7d9ce3ea045
Paul Zimmermann <Paul.Zimmermann@inria.fr>
2017-12-13 07:05:01 +0000

    [src/mpfr-gmp.h] redefine __clz_tab also with gmp-build
    [From SVN r11969 (trunk)]

--- src/mpfr-gmp.h
+++ src/mpfr-gmp.h
@@ -80,7 +80,10 @@ extern "C" {
 # error "Can't compute log2(GMP_NUMB_BITS)"
 #endif
 
-
+/* Remap names of internal mpn functions (for longlong.h).
+   Note: this should be made both with and without gmp build. */
+#undef  __clz_tab
+#define __clz_tab               mpfr_clz_tab
 
 /******************************************************
  ************* Define GMP Internal Interface  *********
@@ -230,10 +233,6 @@ typedef unsigned long int UDItype;
 typedef mp_limb_t UWtype;
 typedef unsigned int UHWtype;
 #define W_TYPE_SIZE GMP_NUMB_BITS
-
-/* Remap names of internal mpn functions (for longlong.h).  */
-#undef  __clz_tab
-#define __clz_tab               mpfr_clz_tab
 
 /* Use (4.0 * ...) instead of (2.0 * ...) to work around buggy compilers
    that don't convert ulong->double correctly (eg. SunOS 4 native cc).  */
