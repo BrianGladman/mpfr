2708a15311f26d3c2619805d02cd58d724545a4d
Vincent Lefèvre <vincent@vinc17.net>
2018-01-07 20:06:18 +0000

    [src/ai.c] Really fixed the bug.
    [From SVN r12074 (trunk)]

--- src/ai.c
+++ src/ai.c
@@ -576,7 +576,6 @@ mpfr_ai2 (mpfr_ptr y, mpfr_srcptr x, mpfr_rnd_t rnd)
     } /* End of ZIV loop */
 
   MPFR_ZIV_FREE (loop);
-  MPFR_SAVE_EXPO_FREE (expo);
 
   r = mpfr_set (y, result, rnd);
 
@@ -592,6 +591,7 @@ mpfr_ai2 (mpfr_ptr y, mpfr_srcptr x, mpfr_rnd_t rnd)
   mpfr_clear (temp1);
   mpfr_clear (temp2);
 
+  MPFR_SAVE_EXPO_FREE (expo);
   return mpfr_check_range (y, r, rnd);
 }
 
