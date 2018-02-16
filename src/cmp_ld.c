6f08973ccfdd52b3c12e3e0d8c6e1832597e4ef0
Paul Zimmermann <Paul.Zimmermann@inria.fr>
2018-02-16 10:43:49 +0000

    [src/cmp_ld.c] simplified code (and avoid useless test when long double is not 	       double-double)
    [From SVN r12263 (trunk)]

--- src/cmp_ld.c
+++ src/cmp_ld.c
@@ -33,16 +33,14 @@ mpfr_cmp_ld (mpfr_srcptr b, long double d)
 
   MPFR_SAVE_EXPO_MARK (expo);
 
+#if !HAVE_LDOUBLE_MAYBE_DOUBLE_DOUBLE
   mpfr_init2 (tmp, MPFR_LDBL_MANT_DIG);
+#else
+  /* since the smallest value is 2^(-1074) and the largest is < 2^1024,
+     every double-double is exactly representable with 1024 + 1074 bits */
+  mpfr_init2 (tmp, 1024 + 1074);
+#endif
   res = mpfr_set_ld (tmp, d, MPFR_RNDN);
-  if (res != 0) /* can happen when "long double" is double-double */
-    {
-      /* since the smallest value is 2^(-1074) and the largest is
-         < 2^1024, every double-double is exactly representable with
-         1024 + 1074 bits */
-      mpfr_set_prec (tmp, 1024 + 1074);
-      res = mpfr_set_ld (tmp, d, MPFR_RNDN);
-    }
   MPFR_ASSERTD (res == 0);
 
   MPFR_CLEAR_FLAGS ();
