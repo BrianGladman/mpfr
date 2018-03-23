0db6d5fe544e81eb7df03e6051356f81f13590e5
Vincent Lefèvre <vincent@vinc17.net>
2018-03-23 10:06:33 +0000

    [tests] Removed some useless #include's, in particular all the unconditional #include <math.h> occurrences. Note: There is an incompatibility between glibc 2.27 and the math.h provided by ICC 15 (at least), and this solves a compilation failure in the build of the tests. Moreover, <math.h> is not required by freestanding implementations, so that it may be better to avoid it if possible.
    [From SVN r12488 (trunk)]

--- tests/tpow.c
+++ tests/tpow.c
@@ -20,9 +20,6 @@ along with the GNU MPFR Library; see the file COPYING.LESSER.  If not, see
 http://www.gnu.org/licenses/ or write to the Free Software Foundation, Inc.,
 51 Franklin St, Fifth Floor, Boston, MA 02110-1301, USA. */
 
-#include <float.h>
-#include <math.h>
-
 #define _MPFR_NO_DEPRECATED_ROOT
 #include "mpfr-test.h"
 
