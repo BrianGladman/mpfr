e921ef7f3546afe535962f0382396efbc27d6b2c
Vincent Lefèvre <vincent@vinc17.net>
2018-04-03 10:48:34 +0000

    [tools/check_mparam.c] C89 compatibility. Return an error code (useful for scripts).
    [From SVN r12530 (trunk)]

--- tools/check_mparam.c
+++ tools/check_mparam.c
@@ -23,17 +23,30 @@ static short sqrhigh_ktab[] = {MPFR_SQRHIGH_TAB};
 static short divhigh_ktab[] = {MPFR_DIVHIGH_TAB};
 #define MPFR_DIVHIGH_TAB_SIZE (numberof_const (divhigh_ktab))
 
-int main()
+int main (void)
 {
-  for (int n = 0; n < MPFR_MULHIGH_TAB_SIZE; n++)
+  int err = 0, n;
+
+  for (n = 0; n < MPFR_MULHIGH_TAB_SIZE; n++)
     if (mulhigh_ktab[n] >= n)
-      printf ("Error, mulhigh_ktab[%d] = %d\n", n, mulhigh_ktab[n]);
+      {
+        printf ("Error, mulhigh_ktab[%d] = %d\n", n, mulhigh_ktab[n]);
+        err = 1;
+      }
 
-  for (int n = 0; n < MPFR_SQRHIGH_TAB_SIZE; n++)
+  for (n = 0; n < MPFR_SQRHIGH_TAB_SIZE; n++)
     if (sqrhigh_ktab[n] >= n)
-      printf ("Error, sqrhigh_ktab[%d] = %d\n", n, sqrhigh_ktab[n]);
+      {
+        printf ("Error, sqrhigh_ktab[%d] = %d\n", n, sqrhigh_ktab[n]);
+        err = 1;
+      }
 
-  for (int n = 2; n < MPFR_DIVHIGH_TAB_SIZE; n++)
+  for (n = 2; n < MPFR_DIVHIGH_TAB_SIZE; n++)
     if (divhigh_ktab[n] >= n-1)
-      printf ("Error, divhigh_ktab[%d] = %d\n", n, divhigh_ktab[n]);
+      {
+        printf ("Error, divhigh_ktab[%d] = %d\n", n, divhigh_ktab[n]);
+        err = 1;
+      }
+
+  return err;
 }
