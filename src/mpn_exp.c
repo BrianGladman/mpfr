a58af2cc38cdf9b3d3e889fe024d64eaf5dc3679
Vincent Lefèvre <vincent@vinc17.net>
2018-05-25 14:40:40 +0000

    [src/mpn_exp.c] Code cleanup and protection against integer overflow.
    [From SVN r12714 (trunk)]

--- src/mpn_exp.c
+++ src/mpn_exp.c
@@ -24,7 +24,7 @@ http://www.gnu.org/licenses/ or write to the Free Software Foundation, Inc.,
 #define MPFR_NEED_LONGLONG_H
 #include "mpfr-impl.h"
 
-/* this function computes an approximation of b^e in {a, n}, with exponent
+/* this function computes an approximation to b^e in {a, n}, with exponent
    stored in exp_r. The computed value is rounded toward zero (truncated).
    It returns an integer f such that the final error is bounded by 2^f ulps,
    that is:
@@ -42,17 +42,17 @@ mpfr_mpn_exp (mp_limb_t *a, mpfr_exp_t *exp_r, int b, mpfr_exp_t e, size_t n)
   mp_limb_t *c, B;
   mpfr_exp_t f, h;
   int i;
-  unsigned long t; /* number of bits in e */
-  unsigned long bits;
-  size_t n1;
-  unsigned int error;           /* (number - 1) of loop a^2b inexact */
+  int t;                         /* number of bits in e */
+  size_t bits, n1;
+  unsigned int error;            /* (number - 1) of loops a^2b inexact */
                                  /* error == t means no error */
   int err_s_a2 = 0;
   int err_s_ab = 0;              /* number of error when shift A^2, AB */
   MPFR_TMP_DECL(marker);
 
-  MPFR_ASSERTN(e > 0);
-  MPFR_ASSERTN((2 <= b) && (b <= 62));
+  MPFR_ASSERTN (n > 0 && n <= ((size_t) -1) / GMP_NUMB_BITS);
+  MPFR_ASSERTN (e > 0 && e <= MPFR_LIMB_MAX);
+  MPFR_ASSERTN (2 <= b && b <= 62);
 
   MPFR_TMP_MARK(marker);
 
@@ -79,13 +79,13 @@ mpfr_mpn_exp (mp_limb_t *a, mpfr_exp_t *exp_r, int b, mpfr_exp_t e, size_t n)
 
   t = GMP_NUMB_BITS - t; /* number of bits of exponent e */
 
-  error = t; /* error <= GMP_NUMB_BITS */
+  error = t;
+  MPFR_ASSERTD (error >= 0 && error <= GMP_NUMB_BITS);
 
   MPN_ZERO (c, 2 * n);
 
   for (i = t - 2; i >= 0; i--)
     {
-
       /* determine precision needed */
       bits = n * GMP_NUMB_BITS - mpn_scan1 (a, 0);
       n1 = (n * GMP_NUMB_BITS - bits) / GMP_NUMB_BITS;
@@ -98,19 +98,19 @@ mpfr_mpn_exp (mp_limb_t *a, mpfr_exp_t *exp_r, int b, mpfr_exp_t e, size_t n)
       /* set {c+n, 2n1-n} to 0 : {c, n} = {a, n}^2*K^n */
 
       /* check overflow on f */
-      if (MPFR_UNLIKELY(f < MPFR_EXP_MIN/2 || f > MPFR_EXP_MAX/2))
+      if (MPFR_UNLIKELY (f < MPFR_EXP_MIN / 2 || f > MPFR_EXP_MAX / 2))
         {
         overflow:
           MPFR_TMP_FREE(marker);
           return -2;
         }
-      /* FIXME: Could f = 2*f + n * GMP_NUMB_BITS be used? */
-      f = 2*f;
+      /* FIXME: Could f = 2 * f + n * GMP_NUMB_BITS be used? */
+      f = 2 * f;
       MPFR_SADD_OVERFLOW (f, f, n * GMP_NUMB_BITS,
                           mpfr_exp_t, mpfr_uexp_t,
                           MPFR_EXP_MIN, MPFR_EXP_MAX,
                           goto overflow, goto overflow);
-      if ((c[2*n - 1] & MPFR_LIMB_HIGHBIT) == 0)
+      if (MPFR_LIMB_MSB (c[2*n - 1]) == 0)
         {
           /* shift A by one bit to the left */
           mpn_lshift (a, c + n, n, 1);
@@ -122,11 +122,12 @@ mpfr_mpn_exp (mp_limb_t *a, mpfr_exp_t *exp_r, int b, mpfr_exp_t e, size_t n)
       else
         MPN_COPY (a, c + n, n);
 
-      if ((error == t) && (2 * n1 <= n) &&
-          (mpn_scan1 (c + 2 * n1, 0) < (n - 2 * n1) * GMP_NUMB_BITS))
+      if (error == t && 2 * n1 <= n &&
+          mpn_scan1 (c + 2 * n1, 0) < (n - 2 * n1) * GMP_NUMB_BITS)
         error = i;
 
-      if (e & ((mpfr_exp_t) 1 << i))
+      /* Let's recall that e > 0 and fits in a limb. */
+      if (e & (MPFR_LIMB_ONE << i))
         {
           /* multiply A by B */
           c[2 * n - 1] = mpn_mul_1 (c + n - 1, a, n, B);
@@ -143,10 +144,14 @@ mpfr_mpn_exp (mp_limb_t *a, mpfr_exp_t *exp_r, int b, mpfr_exp_t e, size_t n)
               if (error != t)
                 err_s_ab ++;
             }
-          if ((error == t) && (c[n - 1] != 0))
+          if (error == t && c[n - 1] != 0)
             error = i;
         }
     }
+
+  MPFR_ASSERTD (error >= 0 && error <= GMP_NUMB_BITS);
+  MPFR_ASSERTD (err_s_a2 >= 0 && err_s_a2 < GMP_NUMB_BITS);
+  MPFR_ASSERTD (err_s_ab >= 0 && err_s_ab < GMP_NUMB_BITS);
 
   MPFR_TMP_FREE(marker);
 
@@ -162,15 +167,6 @@ mpfr_mpn_exp (mp_limb_t *a, mpfr_exp_t *exp_r, int b, mpfr_exp_t e, size_t n)
          at most 2^(p+ceil((j+1)/2)+l+1)*ulp(res).
          This is bounded by 2^(5/2*t-1/2) where t is the number of bits of e.
       */
-      error = error + err_s_ab + err_s_a2 / 2 + 3; /* <= 5t/2-1/2 */
-#if 0
-      if ((error - 1) >= ((n * GMP_NUMB_BITS - 1) / 2))
-        error = n * GMP_NUMB_BITS; /* result is completely wrong:
-                                         this is very unlikely since error is
-                                         at most 5/2*log_2(e), and
-                                         n * GMP_NUMB_BITS is at least
-                                         3*log_2(e) */
-#endif
-      return error;
+      return error + err_s_ab + err_s_a2 / 2 + 3; /* <= 5t/2-1/2 */
     }
 }
