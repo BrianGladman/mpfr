bab9cee6d6b3462a4aa9c501a0c83cf1dc3736b8
Vincent Lefèvre <vincent@vinc17.net>
2018-06-22 02:25:39 +0000

    [src/set_d128.c] Added a FIXME (stack-buffer-overflow).
    [From SVN r12813 (trunk)]

--- src/set_d128.c
+++ src/set_d128.c
@@ -103,6 +103,9 @@ static unsigned int T[1024] = {
 #endif
 
 #if _MPFR_IEEE_FLOATS && (GMP_NUMB_BITS == 32 || GMP_NUMB_BITS == 64)
+/* FIXME: stack-buffer-overflow with:
+     CC=gcc-snapshot CFLAGS="-m32 -O3 -march=native -fsanitize=address"
+*/
 /* Convert d to a decimal string (one-to-one correspondence, no rounding).
    The string s needs to have at least 44 characters (including the final \0):
    * 1 for the sign '-'
