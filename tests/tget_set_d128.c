2462683ebfc059137f8e2513245639287aef5ae9
Vincent Lefèvre <vincent@vinc17.net>
2018-06-23 00:20:00 +0000

    [tests/tget_set_d128.c] Replaced d != d by DOUBLE_ISNAN (d).
    [From SVN r12851 (trunk)]

--- tests/tget_set_d128.c
+++ tests/tget_set_d128.c
@@ -36,7 +36,7 @@ http://www.gnu.org/licenses/ or write to the Free Software Foundation, Inc.,
 static void
 print_decimal128 (_Decimal128 d)
 {
-  if (d != d)
+  if (DOUBLE_ISNAN (d))
     printf ("NaN");
   else if (d > DEC128_MAX)
     printf ("Inf");
