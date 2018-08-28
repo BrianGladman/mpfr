2516ee72f3e49358718de649c88e2f8c4bf63812
Paul Zimmermann <Paul.Zimmermann@inria.fr>
2018-08-28 14:02:06 +0000

    [src/exp_2.c] removed a static assertion (duplicate with MPFR_LONG_WITHIN_LIMB)
    [From SVN r13057 (trunk)]

--- src/exp_2.c
+++ src/exp_2.c
@@ -119,7 +119,6 @@ mpfr_exp_2 (mpfr_ptr y, mpfr_srcptr x, mpfr_rnd_t rnd_mode)
       {
         mp_limb_t a;
         mpfr_exp_t exp;
-        MPFR_STAT_STATIC_ASSERT (MPFR_LIMB_MAX >= ULONG_MAX);
         /* Read the long directly (faster than using mpfr_get_si
            since it fits, it is not singular, it can't be zero
            and there is no conversion to do) */
