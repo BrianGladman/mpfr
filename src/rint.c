288365ad5be3edf5cbc25ce2a5cfa9fce5ed7c3c
Paul Zimmermann <Paul.Zimmermann@inria.fr>
2018-09-06 07:56:44 +0000

    [src/rint.c] fixed bug with 16-bit limb              (found by trint with GMP_CHECK_RANDOMIZE=1536206745598891)
    [From SVN r13150 (trunk)]

--- src/rint.c
+++ src/rint.c
@@ -191,7 +191,7 @@ mpfr_rint (mpfr_ptr r, mpfr_srcptr u, mpfr_rnd_t rnd_mode)
             }
           if (uflags == 0)
             { /* u is an integer; determine if it is representable in r */
-              if (sh != 0 && rp[0] << (GMP_NUMB_BITS - sh) != 0)
+              if (sh != 0 && MPFR_LIMB_LSHIFT(rp[0], GMP_NUMB_BITS - sh) != 0)
                 uflags = 1;  /* u is not representable in r */
               else
                 {
@@ -288,7 +288,7 @@ mpfr_rint (mpfr_ptr r, mpfr_srcptr u, mpfr_rnd_t rnd_mode)
         MPFR_RET(0);
 
       MPFR_ASSERTD (rnd_away >= 0);  /* rounding direction is defined */
-      if (rnd_away && mpn_add_1(rp, rp, rn, MPFR_LIMB_ONE << sh))
+      if (rnd_away && mpn_add_1 (rp, rp, rn, MPFR_LIMB_ONE << sh))
         {
           if (exp == __gmpfr_emax)
             return mpfr_overflow (r, rnd_mode, sign) >= 0 ?
