878b889ffcdfbf51d0c3428d3075e15d8526f2c3
Paul Zimmermann <Paul.Zimmermann@inria.fr>
2018-09-07 11:59:57 +0000

    [tests/tl2b.c] extend to 8-bit limb
    [From SVN r13155 (trunk)]

--- tests/tl2b.c
+++ tests/tl2b.c
@@ -26,7 +26,7 @@ http://www.gnu.org/licenses/ or write to the Free Software Foundation, Inc.,
 #include "mpfr-test.h"
 
 /* Must be a multiple of 4 */
-static const int bits2use[] = {16, 32, 64, 96, 128, 256};
+static const int bits2use[] = {8, 16, 32, 64, 96, 128, 256};
 #define size_of_bits2use (numberof (bits2use))
 
 static __mpfr_struct l2b[BASE_MAX-1][2];
