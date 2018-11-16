0fa8bb4c53fffd72ff77d07d99500b93694d858b
Vincent Lefèvre <vincent@vinc17.net>
2018-11-16 13:13:52 +0000

    [src/nbits_ulong.c] Code refactoring. Added {pre,post}condition.
    [From SVN r13269 (trunk)]

--- src/nbits_ulong.c
+++ src/nbits_ulong.c
@@ -28,39 +28,54 @@ http://www.gnu.org/licenses/ or write to the Free Software Foundation, Inc.,
 int
 mpfr_nbits_ulong (unsigned long n)
 {
-#ifdef MPFR_LONG_WITHIN_LIMB
   int cnt;
-  count_leading_zeros (cnt, (mp_limb_t) n);
-  return GMP_NUMB_BITS - cnt;
-#else
-  int nbits = 0;
 
   MPFR_ASSERTD (n > 0);
+
+#ifdef MPFR_LONG_WITHIN_LIMB
+
+  count_leading_zeros (cnt, (mp_limb_t) n);
+  cnt = GMP_NUMB_BITS - cnt;
+
+#else
+
   while (n >= 0x10000)
     {
       n >>= 16;
-      nbits += 16;
+      cnt += 16;
     }
+
   MPFR_ASSERTD (n <= 0xffff);
+
   if (n >= 0x100)
     {
       n >>= 8;
-      nbits += 8;
+      cnt += 8;
     }
+
   MPFR_ASSERTD (n <= 0xff);
+
   if (n >= 0x10)
     {
       n >>= 4;
-      nbits += 4;
+      cnt += 4;
     }
+
   MPFR_ASSERTD (n <= 0xf);
+
   if (n >= 4)
     {
       n >>= 2;
-      nbits += 2;
+      cnt += 2;
     }
+
   MPFR_ASSERTD (n <= 3);
+
   /* now n = 1, 2, or 3 */
-  return nbits + 1 + (n >= 2);
+  cnt += 1 + (n >= 2);
+
 #endif
+
+  MPFR_ASSERTD (cnt >= 0);
+  return cnt;
 }
