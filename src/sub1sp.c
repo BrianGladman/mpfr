4cc4cb0966edda990bfbbe91d22702501843d452
Vincent Lefèvre <vincent@vinc17.net>
2019-06-06 12:12:11 +0000

    [src/sub1sp.c] Added reference to GCC PR30315 about a misoptimization on x86/x86_64 (see r13506 and r13507).
    [From SVN r13508 (trunk)]

--- src/sub1sp.c
+++ src/sub1sp.c
@@ -141,6 +141,7 @@ mpfr_sub1sp1 (mpfr_ptr a, mpfr_srcptr b, mpfr_srcptr c, mpfr_rnd_t rnd_mode,
              easier to optimize than a0 >= MPFR_LIMB_HIGHBIT on processors
              with condition flags, i.e. the test should be a no-op as the
              flag should have been obtained form the subtraction above.
+             See <https://gcc.gnu.org/bugzilla/show_bug.cgi?id=30315>.
              TODO: revert the test in the future, once compilers have
              improved? */
           MPFR_SET_OPPOSITE_SIGN (a, b);
