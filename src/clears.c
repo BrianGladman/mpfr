a1507dedd8eb3eafaca527d7f35ae408c3ecabfb
Vincent Lefèvre <vincent@vinc17.net>
2020-06-10 21:26:07 +0000

    Removed '# undef HAVE_STDARG' before '# include "config.h"' when it was used, as it was not always undefined (e.g. in tests/tsprintf.c) and there are no reasons to undefine this macro specifically. Note: this code was introduced in r2665 (2004-02-06), perhaps for the MPFR build inside GMP, which has been dropped a long time ago.
    [From SVN r13969 (trunk)]

--- src/clears.c
+++ src/clears.c
@@ -22,7 +22,6 @@ https://www.gnu.org/licenses/ or write to the Free Software Foundation, Inc.,
 51 Franklin St, Fifth Floor, Boston, MA 02110-1301, USA. */
 
 #ifdef HAVE_CONFIG_H
-# undef HAVE_STDARG
 # include "config.h"
 #endif
 
