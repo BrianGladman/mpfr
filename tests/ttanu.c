e352419938b88719cadb80637ce93ca7963002ba
Vincent Lefèvre <vincent@vinc17.net>
2020-12-17 08:40:07 +0000

    [tests]   * Added data/tan2pi: hardest-to-round cases of tanu with u = 1     in double precision, interval [0,1/4].   * ttanu.c: test tan2pi values with u = 1.
    [From SVN r14219 (trunk)]

--- tests/ttanu.c
+++ tests/ttanu.c
@@ -202,6 +202,12 @@ test_regular (void)
 #endif
 #include "tgeneric.c"
 
+static int
+mpfr_tan2pi (mpfr_ptr y, mpfr_srcptr x, mpfr_rnd_t r)
+{
+  return mpfr_tanu (y, x, 1, r);
+}
+
 int
 main (void)
 {
@@ -212,6 +218,8 @@ main (void)
   test_regular ();
 
   test_generic (MPFR_PREC_MIN, 100, 1);
+
+  data_check ("data/tan2pi", mpfr_tan2pi, "mpfr_tan2pi");
 
   tests_end_mpfr ();
   return 0;
