cda880d93930932dd75b99bfbe84e0e1903f830f
Vincent Lefevre <vincent@vinc17.net>
2022-04-04 14:25:08 +0200

    [tests/troot.c] Added a comment.

--- tests/troot.c
+++ tests/troot.c
@@ -20,6 +20,7 @@ along with the GNU MPFR Library; see the file COPYING.LESSER.  If not, see
 https://www.gnu.org/licenses/ or write to the Free Software Foundation, Inc.,
 51 Franklin St, Fifth Floor, Boston, MA 02110-1301, USA. */
 
+/* Note: troot.c is included by trootn_ui.c with TF = mpfr_rootn_ui */
 #ifdef TF
 # define TF_IS_MPFR_ROOT 0
 #else
