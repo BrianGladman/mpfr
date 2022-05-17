90fd4ddd5bf71ff91e3c2c1ed0fed59777e9ee59
Vincent Lefevre <vincent@vinc17.net>
2022-05-17 23:53:59 +0200

    For version-ext.sh, use "$SED" instead of "sed".
    * tests/Makefile.am: pass $SED to version-ext.sh.
    * version-ext.sh: use $SED (default to "sed").

--- version-ext.sh
+++ version-ext.sh
@@ -16,12 +16,13 @@ if [ "x`git rev-parse --is-inside-work-tree 2> /dev/null`" != xtrue ]; then
   exit 1
 fi
 
-# Normally passed by Makefile.am with "GREP=$(GREP)".
+# Normally passed by tests/Makefile.am with "GREP=$(GREP) SED=$(SED)".
 GREP=${GREP:-grep}
+SED=${SED:-sed}
 
 git tag --contains | sed -n 's/-root$//p' > excluded-branches
 gitb=`git branch --format='%(refname:short)' --contains | \
-        sed 's,(HEAD detached at origin/\(.*\)),\1,' | \
+        $SED 's,(HEAD detached at origin/\(.*\)),\1,' | \
         $GREP -v '^(' | $GREP -v -F -f excluded-branches -x`
 rm excluded-branches
 gitc=`git rev-list --count HEAD`
