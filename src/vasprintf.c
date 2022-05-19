7a651256c056cc724f75bcc3d9e77a0b8e0a38cd
Vincent Lefevre <vincent@vinc17.net>
2022-05-19 11:17:08 +0200

    [src/vasprintf.c] Bug fix for the l length modifier with FP type.
    The l length modifier is now supported with the a, A, e, E, f, F, g, G
    conversion specifiers, as required by the ISO C standard on printf and
    other formatted output functions. As specified, it is just ignored.
    
    For that, to improve code maintainability, the CASE_LONG_ARG() macro
    was simplified:
      * The c and s conversion specifiers are always supported by this
        macro (this is a potential fix for implementations with incomplete
        wide-character handling, without any guarantee to work).
      * A MPFR_RET_NEVER_GO_HERE() for unsupported conversion specifiers
        has been added (if specinfo_is_valid() is consistent with this
        macro, one should effectively never reach this case).

--- src/vasprintf.c
+++ src/vasprintf.c
@@ -228,8 +228,11 @@ specinfo_init (struct printf_spec *specinfo)
   specinfo->pad = ' ';
 }
 
+/* Note: LONG_ARG is unusual, but is accepted (ISO C99 says "as no effect
+   on a following a, A, e, E, f, F, g, or G conversion specifier"). */
 #define FLOATING_POINT_ARG_TYPE(at) \
-  ((at) == MPFR_ARG || (at) == MPF_ARG || (at) == LONG_DOUBLE_ARG)
+  ((at) == MPFR_ARG || (at) == MPF_ARG \
+   || (at) == LONG_ARG || (at) == LONG_DOUBLE_ARG)
 
 #define INTEGER_LIKE_ARG_TYPE(at)                                       \
   ((at) == SHORT_ARG || (at) == LONG_ARG || (at) == LONG_LONG_ARG       \
@@ -412,7 +415,6 @@ parse_arg_type (const char *format, struct printf_spec *specinfo)
    to int (because wchar_t <= int).
    For wint_t, we assume that the case WINT_MAX < INT_MAX yields an
    integer promotion. */
-#ifdef HAVE_WCHAR_H
 #if defined(WINT_MAX) && WINT_MAX < INT_MAX
 typedef int    mpfr_va_wint;  /* integer promotion */
 #else
@@ -428,13 +430,14 @@ typedef wint_t mpfr_va_wint;
     (void) va_arg ((ap), mpfr_va_wint);                                 \
   else if ((specinfo).spec == 's')                                      \
     (void) va_arg ((ap), int); /* we assume integer promotion */        \
+  else if ((specinfo).spec == 'a' || (specinfo).spec == 'A'             \
+           || (specinfo).spec == 'e' || (specinfo).spec == 'E'          \
+           || (specinfo).spec == 'f' || (specinfo).spec == 'F'          \
+           || (specinfo).spec == 'g' || (specinfo).spec == 'G')         \
+    (void) va_arg ((ap), double);                                       \
+  else                                                                  \
+    MPFR_RET_NEVER_GO_HERE();                                           \
   break;
-#else
-#define CASE_LONG_ARG(specinfo, ap)             \
-  case LONG_ARG:                                \
-  (void) va_arg ((ap), long);                   \
-  break;
-#endif
 
 #if defined(_MPFR_H_HAVE_INTMAX_T)
 #define CASE_INTMAX_ARG(specinfo, ap)           \
