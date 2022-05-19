09e5ff065354fce929f7043f980f32e31f1bc163
Vincent Lefevre <vincent@vinc17.net>
2022-05-19 02:49:28 +0200

    [tests/tsubnormal.c] Simplified bug20220518.

--- tests/tsubnormal.c
+++ tests/tsubnormal.c
@@ -275,8 +275,7 @@ bug20220518 (void)
   int inex;
 
   mpfr_init2 (y, 53);
-  mpfr_set_str (y, "0x2.000000000037ep+0", 0, MPFR_RNDN);
-  mpfr_mul_2si (y, y, -1076, MPFR_RNDN);
+  mpfr_set_str (y, "0x2.000000000037ep-1076", 0, MPFR_RNDN);
   mpfr_set_emin (-1073);
   inex = mpfr_subnormalize (y, 1, MPFR_RNDN);
   /* since y = 0x2.000000000037ep-1076 > 1p-1075, we should round to 1p-1074 */
