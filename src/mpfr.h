BrianGladman/mpfr

commit 9f94e0311ed53d0c64d4fbca249d19cc4888027e

Fixed issues with the macro versions of the custom interface.
* src/mpfr.h: fixed issues with the mpfr_custom_* macros:
    - a "void *" argument instead of mpfr_ptr could make the
      compilation fail (with the function, the conversion is
      implicit, thus this should be accepted by the macro);
    - some arguments could be evaluated 0 times, which is wrong
      if their evaluation has side effects.
* tests/tstckintc.c: added various tests to check these kinds
  of issues.

src/mpfr.h
@@ -1020,21 +1020,29 @@ __MPFR_DECLSPEC int mpfr_total_order_p (mpfr_srcptr, mpfr_srcptr);
 #endif
 #endif
 
-/* Macro version of mpfr_stack interface for fast access.
-   The internal cast to mpfr_size_t will silent a warning with
+/* Macro versions of mpfr_stack interface for fast access. */
+
+/* The internal cast to mpfr_size_t will silent a warning with
    GCC's -Wsign-conversion that could occur with user code, as
    sizeof is of type size_t, which is unsigned. */
 #define mpfr_custom_get_size(p)                                             \
   ((mpfr_size_t)                                                            \
    ((mpfr_size_t) (((mpfr_prec_t)(p) + GMP_NUMB_BITS - 1) / GMP_NUMB_BITS)  \
     * sizeof (mp_limb_t)))
-#define mpfr_custom_init(m,p) do {} while (0)
-#define mpfr_custom_get_significand(x) ((mpfr_void*)((x)->_mpfr_d))
-#define mpfr_custom_get_exp(x) ((x)->_mpfr_exp)
-#define mpfr_custom_move(x,m) do { ((x)->_mpfr_d = (mp_limb_t*)(m)); } while (0)
+
+#define mpfr_custom_init(m,p) do { (void) (m); (void) (p); } while (0)
+
+#define mpfr_custom_get_significand(x) \
+  ((mpfr_void *) MPFR_VALUE_OF(MPFR_SRCPTR(x)->_mpfr_d))
+
+#define mpfr_custom_get_exp(x) MPFR_VALUE_OF(MPFR_SRCPTR(x)->_mpfr_exp)
+
+#define mpfr_custom_move(x,m) \
+  do { ((mpfr_ptr) (x))->_mpfr_d = (mp_limb_t *) (m); } while (0)
+
 #define mpfr_custom_init_set(x,k,e,p,m) do {                   \
-  mpfr_ptr _x = (x);                                           \
-  mpfr_exp_t _e;                                               \
+  mpfr_ptr _x = (mpfr_ptr) (x);                                \
+  mpfr_exp_t _e = (e);                                         \
   mpfr_kind_t _t;                                              \
   mpfr_int _s, _k;                                             \
   _k = (k);                                                    \
@@ -1045,7 +1053,7 @@ __MPFR_DECLSPEC int mpfr_total_order_p (mpfr_srcptr, mpfr_srcptr);
     _t = (mpfr_kind_t) - _k;                                   \
     _s = -1;                                                   \
   }                                                            \
-  _e = _t == MPFR_REGULAR_KIND ? (e) :                         \
+  _e = _t == MPFR_REGULAR_KIND ? _e :                          \
     _t == MPFR_NAN_KIND ? __MPFR_EXP_NAN :                     \
     _t == MPFR_INF_KIND ? __MPFR_EXP_INF : __MPFR_EXP_ZERO;    \
   _x->_mpfr_prec = (p);                                        \
@@ -1053,14 +1061,23 @@ __MPFR_DECLSPEC int mpfr_total_order_p (mpfr_srcptr, mpfr_srcptr);
   _x->_mpfr_exp  = _e;                                         \
   _x->_mpfr_d    = (mp_limb_t*) (m);                           \
  } while (0)
-#define mpfr_custom_get_kind(x)                                         \
-  ( (x)->_mpfr_exp >  __MPFR_EXP_INF ?                                  \
-    (mpfr_int) MPFR_REGULAR_KIND * MPFR_SIGN (x)                        \
-  : (x)->_mpfr_exp == __MPFR_EXP_INF ?                                  \
-    (mpfr_int) MPFR_INF_KIND * MPFR_SIGN (x)                            \
-  : (x)->_mpfr_exp == __MPFR_EXP_NAN ? (mpfr_int) MPFR_NAN_KIND         \
-  : (mpfr_int) MPFR_ZERO_KIND * MPFR_SIGN (x) )
 
+#if __GNUC__ > 2 || __GNUC_MINOR__ >= 95
+#define mpfr_custom_get_kind(x)                                         \
+  __extension__ ({                                                      \
+    mpfr_ptr _x = (x);                                                  \
+    _x->_mpfr_exp >  __MPFR_EXP_INF ?                                   \
+      (mpfr_int) MPFR_REGULAR_KIND * MPFR_SIGN (_x)                     \
+      : _x->_mpfr_exp == __MPFR_EXP_INF ?                               \
+      (mpfr_int) MPFR_INF_KIND * MPFR_SIGN (_x)                         \
+      : _x->_mpfr_exp == __MPFR_EXP_NAN ? (mpfr_int) MPFR_NAN_KIND      \
+      : (mpfr_int) MPFR_ZERO_KIND * MPFR_SIGN (_x);                     \
+  })
+#else
+#define mpfr_custom_get_kind(x) ((mpfr_custom_get_kind)(x))
+#endif
+
+/* End of the macro versions of mpfr_stack interface. */
 
 #endif /* MPFR_USE_NO_MACRO */
 

tests/tstckintc.c
@@ -117,58 +117,110 @@ reset_stack (void)
 
 /*************************************************************************/
 
- /* Alloc a new mpfr_t on the main stack */
+/* Alloc a new mpfr_t on the main stack */
 static mpfr_ptr
 new_mpfr (mpfr_prec_t p)
 {
   mpfr_ptr x = (mpfr_ptr) new_st (sizeof (mpfr_t));
   void *mantissa = new_st (mpfr_custom_get_size (p));
+
   mpfr_custom_init (mantissa, p);
   mpfr_custom_init_set (x, 0, 0, p, mantissa);
   return x;
 }
 
- /* Alloc a new mpfr_t on the main stack */
+/* Alloc a new mpfr_t on the main stack */
 static mpfr_ptr
-new_nan (mpfr_prec_t p)
+new_nan1 (mpfr_prec_t p)
 {
   mpfr_ptr x = (mpfr_ptr) new_st (sizeof (mpfr_t));
   void *mantissa = new_st ((mpfr_custom_get_size) (p));
+
   (mpfr_custom_init) (mantissa, p);
   (mpfr_custom_init_set) (x, MPFR_NAN_KIND, 0, p, mantissa);
   return x;
 }
 
- /* Alloc a new mpfr_t on the main stack */
+/* Alloc a new mpfr_t on the main stack */
+static mpfr_ptr
+new_nan2 (mpfr_prec_t p)
+{
+  mpfr_ptr x = (mpfr_ptr) new_st (sizeof (mpfr_t));
+  void *px;
+  void *mantissa = new_st ((mpfr_custom_get_size) (p));
+  int i1, i2, i3, i4, i5;
+
+  px = x;
+
+  /* Check that the type "void *" can be used, like with the function.
+     Also check side effects. */
+  i1 = i2 = 0;
+  mpfr_custom_init ((i1++, mantissa), (i2++, p));
+  MPFR_ASSERTN (i1 == 1);
+  MPFR_ASSERTN (i2 == 1);
+
+  /* Check that the type "void *" can be used, like with the function.
+     Also check side effects. */
+  i1 = i2 = i3 = i4 = i5 = 0;
+  mpfr_custom_init_set ((i1++, px),
+                        (i2++, MPFR_NAN_KIND),
+                        (i3++, 0),
+                        (i4++, p),
+                        (i5++, mantissa));
+  MPFR_ASSERTN (i1 == 1);
+  MPFR_ASSERTN (i2 == 1);
+  MPFR_ASSERTN (i3 == 1);
+  MPFR_ASSERTN (i4 == 1);
+  MPFR_ASSERTN (i5 == 1);
+
+  return x;
+}
+
+/* Alloc a new mpfr_t on the main stack */
 static mpfr_ptr
 new_inf (mpfr_prec_t p)
 {
   mpfr_ptr x = (mpfr_ptr) new_st (sizeof (mpfr_t));
   void *mantissa = new_st ((mpfr_custom_get_size) (p));
+
   (mpfr_custom_init) (mantissa, p);
   (mpfr_custom_init_set) (x, -MPFR_INF_KIND, 0, p, mantissa);
   return x;
 }
 
- /* Garbage the stack by keeping only x and save it in old_stack */
+/* Garbage the stack by keeping only x and save it in old_stack */
 static mpfr_ptr
 return_mpfr (mpfr_ptr x, char *old_stack)
 {
+  void *px = x;
   void *mantissa       = mpfr_custom_get_significand (x);
   size_t size_mantissa = mpfr_custom_get_size (mpfr_get_prec (x));
-  mpfr_ptr newx;
-  long *newx2;
+  void *newx, *newx2;
+  int i1, i2;
+
+  /* Check that the type "void *" can be used, like with the function.
+     Also check side effects. */
+  i1 = 0;
+  MPFR_ASSERTN (mpfr_custom_get_significand ((i1++, px)) == mantissa);
+  MPFR_ASSERTN (i1 == 1);
 
   memmove (old_stack, x, sizeof (mpfr_t));
   memmove (old_stack + ALIGNED (sizeof (mpfr_t)), mantissa, size_mantissa);
-  newx = (mpfr_ptr) (long *) (void *) old_stack;
-  newx2 = (long *) (void *) (old_stack + ALIGNED (sizeof (mpfr_t)));
-  mpfr_custom_move (newx, newx2);
+  newx = (void *) old_stack;
+  newx2 = (void *) (old_stack + ALIGNED (sizeof (mpfr_t)));
+
+  /* Check that the type "void *" can be used, like with the function.
+     Also check side effects. */
+  i1 = i2 = 0;
+  mpfr_custom_move ((i1++, newx), (i2++, newx2));
+  MPFR_ASSERTN (i1 == 1);
+  MPFR_ASSERTN (i2 == 1);
+
   stack = (char *) newx2 + ALIGNED (size_mantissa);
-  return newx;
+  return (mpfr_ptr) newx;
 }
 
- /* Garbage the stack by keeping only x and save it in old_stack */
+/* Garbage the stack by keeping only x and save it in old_stack */
 static mpfr_ptr
 return_mpfr_func (mpfr_ptr x, char *old_stack)
 {
@@ -241,10 +293,17 @@ test_nan_inf_zero (void)
       exit (1);
     }
 
-  val = new_nan (MPFR_PREC_MIN);
+  val = new_nan1 (MPFR_PREC_MIN);
   if (!mpfr_nan_p(val))
     {
-      printf ("Error: mpfr_custom_init_set doesn't set NAN mpfr.\n");
+      printf ("Error: mpfr_custom_init_set doesn't set NAN mpfr (1).\n");
+      exit (1);
+    }
+
+  val = new_nan2 (MPFR_PREC_MIN);
+  if (!mpfr_nan_p(val))
+    {
+      printf ("Error: mpfr_custom_init_set doesn't set NAN mpfr (2).\n");
       exit (1);
     }
 
@@ -303,11 +362,43 @@ static long *
 dummy_set_si (long si)
 {
   mpfr_t x;
+  void *px;
   long * r = dummy_new ();
-  (mpfr_custom_init_set) (x, MPFR_REGULAR_KIND, 0, p, &r[2]);
+  int i1, i2, i3, i4, i5;
+
+  px = x;
+
+  /* Check that the type "void *" can be used, like with the function.
+     Also check side effects. */
+  i1 = i2 = i3 = i4 = i5 = 0;
+  mpfr_custom_init_set ((i1++, px),
+                        (i2++, MPFR_REGULAR_KIND),
+                        (i3++, 0),
+                        (i4++, p),
+                        (i5++, &r[2]));
+  MPFR_ASSERTN (i1 == 1);
+  MPFR_ASSERTN (i2 == 1);
+  MPFR_ASSERTN (i3 == 1);
+  MPFR_ASSERTN (i4 == 1);
+  MPFR_ASSERTN (i5 == 1);
+
   mpfr_set_si (x, si, MPFR_RNDN);
   r[0] = mpfr_custom_get_kind (x);
+
+  /* Check that the type "void *" can be used, like with the function.
+     Also check side effects. */
+  i1 = 0;
+  MPFR_ASSERTN (mpfr_custom_get_kind ((i1++, px)) == r[0]);
+  MPFR_ASSERTN (i1 == 1);
+
   r[1] = mpfr_custom_get_exp (x);
+
+  /* Check that the type "void *" can be used, like with the function.
+     Also check side effects. */
+  i1 = 0;
+  MPFR_ASSERTN (mpfr_custom_get_exp ((i1++, px)) == r[1]);
+  MPFR_ASSERTN (i1 == 1);
+
   return r;
 }
 
@@ -316,6 +407,7 @@ dummy_add (long *a, long *b)
 {
   mpfr_t x, y, z;
   long *r = dummy_new ();
+
   mpfr_custom_init_set (x, 0 + MPFR_REGULAR_KIND, 0, p, &r[2]);
   (mpfr_custom_init_set) (y, a[0], a[1], p, &a[2]);
   mpfr_custom_init_set (z, 0 + b[0], b[1], p, &b[2]);

commit 0ce17bae34a6c54de31b126f969d3ddd72c6bc37

Fix mpfr_custom_get_kind() macro bug.
* src/mpfr.h: in the mpfr_custom_get_kind() macro, changed mpfr_ptr to
  mpfr_srcptr for _x to agree with the function prototype, in order to
  avoid a compilation failure of user code in some cases. This bug was
  introduced by commit 9f94e0311ed53d0c64d4fbca249d19cc4888027e, which
  introduced the temporary variable _x to avoid an incorrect number of
  evaluations of the x argument.
* tests/tstckintc.c: improved the tests to detect this bug.

This should fix mpfr bug #1.

Bug initially reported by FX Coudert:
  https://github.com/CGAL/cgal/issues/7064

It affects Fedora Linux:
  https://bugzilla.redhat.com/show_bug.cgi?id=2144197

src/mpfr.h
@@ -1064,7 +1064,7 @@ __MPFR_DECLSPEC int mpfr_total_order_p (mpfr_srcptr, mpfr_srcptr);
 #if __GNUC__ > 2 || __GNUC_MINOR__ >= 95
 #define mpfr_custom_get_kind(x)                                         \
   __extension__ ({                                                      \
-    mpfr_ptr _x = (x);                                                  \
+    mpfr_srcptr _x = (x);                                               \
     _x->_mpfr_exp >  __MPFR_EXP_INF ?                                   \
       (mpfr_int) MPFR_REGULAR_KIND * MPFR_SIGN (_x)                     \
       : _x->_mpfr_exp == __MPFR_EXP_INF ?                               \

tests/tstckintc.c
@@ -295,14 +295,16 @@ static void
 test_nan_inf_zero (void)
 {
   mpfr_ptr val;
+  mpfr_srcptr sval;  /* for compilation error checking */
   int sign;
   int kind;
 
   reset_stack ();
 
   val = new_mpfr (MPFR_PREC_MIN);
+  sval = val;
   mpfr_set_nan (val);
-  kind = (mpfr_custom_get_kind) (val);
+  kind = (mpfr_custom_get_kind) (sval);
   if (kind != MPFR_NAN_KIND)
     {
       printf ("mpfr_custom_get_kind error: ");
@@ -380,7 +382,8 @@ static long *
 dummy_set_si (long si)
 {
   mpfr_t x;
-  long * r = dummy_new ();
+  mpfr_srcptr px;  /* for compilation error checking */
+  long *r = dummy_new ();
   int i1, i2, i3, i4, i5;
 
   /* Check that the type "void *" can be used, like with the function.
@@ -405,7 +408,8 @@ dummy_set_si (long si)
   MPFR_ASSERTN (i5 == 1);
 
   mpfr_set_si (x, si, MPFR_RNDN);
-  r[0] = mpfr_custom_get_kind (x);
+  px = x;
+  r[0] = mpfr_custom_get_kind (px);
 
   /* Check that the type "void *" can be used in C, like with the function
      (forbidden in C++). Also check side effects. */