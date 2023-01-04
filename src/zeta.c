28d65d0c4b970ac13fca89572aa3202304f92661
Vincent Lefevre <vincent@vinc17.net>
2023-01-04 18:05:08 +0100

    [src/zeta.c] Updated a comment about mpfr_sinpi.

--- src/zeta.c
+++ src/zeta.c
@@ -644,7 +644,7 @@ mpfr_zeta (mpfr_ptr z, mpfr_srcptr s, mpfr_rnd_t rnd_mode)
           /* multiply z_pre by sin(Pi*s/2) */
           mpfr_mul (y, s, p, MPFR_RNDN);
           mpfr_div_2ui (p, y, 1, MPFR_RNDN);      /* p = s*Pi/2 */
-          /* FIXME: once mpfr_sinpi is available, we should replace the
+          /* FIXME: Now that mpfr_sinpi is available, we should replace the
              mpfr_sin call below by mpfr_sinpi(s/2), where s/2 will be exact.
              Can mpfr_sin underflow? Moreover, the code below should be
              improved so that the "if" condition becomes unlikely, e.g.
