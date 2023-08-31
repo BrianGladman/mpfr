2a09037c9af8df187882a1660f4c7165240f1ae5
Vincent Lefevre <vincent@vinc17.net>
2023-08-31 03:39:07 +0200

    [src] Added missing "#define int32_t int" for --enable-formally-proven-code
    Bug detected on FreeBSD/RISC-V, reported by Dennis Clarke.
    
    We didn't detect it under Linux because the GNU C Library defines
    these types when <stdlib.h> is included:
    
      #if defined __USE_MISC || defined __USE_XOPEN_EXTENDED
      # include <sys/types.h> /* we need int32_t... */
    
    Note: the code shouldn't use these standard types with #define, though
    this is probably safe as we don't use standard functions/macros here.
    
    (cherry picked from commit 9f1efb67a48b5acbbe8a2350ad7e2d4e94293965)

--- src/mul_1_extracted.c
+++ src/mul_1_extracted.c
@@ -24,6 +24,7 @@ along with the GNU MPFR Library; see the file COPYING.LESSER.  If not, see
 https://www.gnu.org/licenses/ or write to the Free Software Foundation, Inc.,
 51 Franklin St, Fifth Floor, Boston, MA 02110-1301, USA. */
 
+#define int32_t int
 #define int64_t long
 #define uint32_t unsigned int
 #define uint64_t mp_limb_t
