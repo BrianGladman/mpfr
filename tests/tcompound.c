44312659cf486f4734fcb2f0b0a4c8594fed4bdc
Paul Zimmermann <Paul.Zimmermann@inria.fr>
2024-02-15 16:30:41 +0100

    [tcompound.c] imported non-regression tests from tcompound_si.c

--- tests/tcompound.c
+++ tests/tcompound.c
@@ -261,12 +261,168 @@ check_ieee754 (void)
   mpfr_clear (z);
 }
 
+/* Failure with mpfr_compound_si from 2021-02-15 (e.g. in MPFR 4.2.0)
+   due to incorrect underflow detection. */
+static void
+bug_20230206 (void)
+{
+  if (MPFR_PREC_MIN == 1)
+    {
+      mpfr_t x, y1, y2, z;
+      int inex1, inex2;
+      mpfr_flags_t flags1, flags2;
+      mpfr_exp_t emin;
+
+      mpfr_inits2 (1, x, y1, y2, (mpfr_ptr) 0);
+      mpfr_init2 (z, 63);
+      mpfr_set_ui_2exp (x, 1, -1, MPFR_RNDN);  /* x = 1/2 */
+
+      /* This first test is useful mainly for a 32-bit mpfr_exp_t type
+         (no failure with a 64-bit mpfr_exp_t type since the underflow
+         threshold in the extended exponent range is much lower). */
+
+      mpfr_set_ui_2exp (y1, 1, -1072124363, MPFR_RNDN);
+      inex1 = -1;
+      flags1 = MPFR_FLAGS_INEXACT;
+      mpfr_clear_flags ();
+      /* -1832808704 ~= -2^30 / log2(3/2) */
+      mpfr_set_si (z, -1832808704, MPFR_RNDN);
+      inex2 = mpfr_compound (y2, x, z, MPFR_RNDN);
+      flags2 = __gmpfr_flags;
+      if (!(mpfr_equal_p (y1, y2) &&
+            SAME_SIGN (inex1, inex2) &&
+            flags1 == flags2))
+        {
+          printf ("Error in bug_20230206 (1):\n");
+          printf ("Expected ");
+          mpfr_dump (y1);
+          printf ("  with inex = %d, flags =", inex1);
+          flags_out (flags1);
+          printf ("Got      ");
+          mpfr_dump (y2);
+          printf ("  with inex = %d, flags =", inex2);
+          flags_out (flags2);
+          exit (1);
+        }
+
+      /* This second test is for a 64-bit mpfr_exp_t type
+         (it is disabled with a 32-bit mpfr_exp_t type). */
+
+      /* The "#if" makes sure that 64-bit constants are supported, avoiding
+         a compilation failure. The "if" makes sure that the constant is
+         representable in a long (this would not be the case with 32-bit
+         unsigned long and 64-bit limb). It also ensures that mpfr_exp_t
+         has at least 64 bits. */
+#if MPFR_PREC_BITS >= 64
+      emin = mpfr_get_emin ();
+      set_emin (MPFR_EMIN_MIN);
+      mpfr_set_ui_2exp (y1, 1, -4611686018427366846, MPFR_RNDN);
+      inex1 = 1;
+      flags1 = MPFR_FLAGS_INEXACT;
+      mpfr_clear_flags ();
+      /* -7883729320669216768 ~= -2^62 / log2(3/2) */
+      mpfr_set_si (z, -7883729320669216768, MPFR_RNDN);
+      inex2 = mpfr_compound (y2, x, z, MPFR_RNDN);
+      flags2 = __gmpfr_flags;
+      if (!(mpfr_equal_p (y1, y2) &&
+            SAME_SIGN (inex1, inex2) &&
+            flags1 == flags2))
+        {
+          printf ("Error in bug_20230206 (2):\n");
+          printf ("Expected ");
+          mpfr_dump (y1);
+          printf ("  with inex = %d, flags =", inex1);
+          flags_out (flags1);
+          printf ("Got      ");
+          mpfr_dump (y2);
+          printf ("  with inex = %d, flags =", inex2);
+          flags_out (flags2);
+          exit (1);
+        }
+      set_emin (emin);
+#endif
+
+      mpfr_clears (x, y1, y2, z, (mpfr_ptr) 0);
+    }
+}
+
+/* Reported by Patrick Pelissier on 2023-02-11
+   (tgeneric_ui.c with GMP_CHECK_RANDOMIZE=1412991715).
+   On a 32-bit host, one gets Inf (overflow) instead of 0.1E1071805703.
+*/
+static void
+bug_20230211 (void)
+{
+  mpfr_t x, y1, y2, z;
+  int inex1, inex2;
+  mpfr_flags_t flags1, flags2;
+
+  mpfr_inits2 (1, x, y1, y2, (mpfr_ptr) 0);
+  mpfr_init2 (z, 31);
+  mpfr_set_ui_2exp (x, 1, -1, MPFR_RNDN);  /* x = 1/2 */
+  mpfr_set_ui_2exp (y1, 1, 1071805702, MPFR_RNDN);
+  inex1 = 1;
+  flags1 = MPFR_FLAGS_INEXACT;
+  mpfr_clear_flags ();
+  mpfr_set_ui (z, 1832263949, MPFR_RNDN);
+  inex2 = mpfr_compound (y2, x, z, MPFR_RNDN);
+  flags2 = __gmpfr_flags;
+  if (!(mpfr_equal_p (y1, y2) &&
+        SAME_SIGN (inex1, inex2) &&
+        flags1 == flags2))
+    {
+      printf ("Error in bug_20230211:\n");
+      printf ("Expected ");
+      mpfr_dump (y1);
+      printf ("  with inex = %d, flags =", inex1);
+      flags_out (flags1);
+      printf ("Got      ");
+      mpfr_dump (y2);
+      printf ("  with inex = %d, flags =", inex2);
+      flags_out (flags2);
+      exit (1);
+    }
+  mpfr_clears (x, y1, y2, z, (mpfr_ptr) 0);
+}
+
+/* Integer overflow with compound.c d04caeae04c6a83276916c4fbac1fe9b0cec3c8b
+   (2023-02-23) on "n * (kx - 1) + 1". Note: if the only effect is just a
+   random value, this probably doesn't affect the result (one might enter
+   the "if" while one shouldn't, but the real check is done inside the "if").
+   This test fails if -fsanitize=undefined -fno-sanitize-recover is used or
+   if the processor emits a signal in case of integer overflow.
+   This test has been made obsolete by the "kx < ex" condition
+   in 2cb3123891dd46fe0258d4aec7f8655b8ec69aaf. */
+static void
+bug_20230517 (void)
+{
+  mpfr_exp_t old_emax;
+  mpfr_t x, z;
+
+  old_emax = mpfr_get_emax ();
+  set_emax (MPFR_EMAX_MAX);
+
+  mpfr_init2 (x, 123456);
+  mpfr_init2 (z, 64);
+  mpfr_set_ui (x, 65536, MPFR_RNDN);
+  mpfr_nextabove (x);
+  mpfr_set_ui (z, LONG_MAX >> 16, MPFR_RNDN);
+  mpfr_compound (x, x, z, MPFR_RNDN);
+  mpfr_clear (x);
+  mpfr_clear (z);
+
+  set_emax (old_emax);
+}
+
 int
 main (void)
 {
   tests_start_mpfr ();
 
   check_ieee754 ();
+  bug_20230206 ();
+  bug_20230211 ();
+  bug_20230517 ();
 
   test_generic (MPFR_PREC_MIN, 100, 100);
 
