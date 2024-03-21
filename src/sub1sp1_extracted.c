d6ef995cadd82b59bea7e0a5379e8fee20fff991
Vincent Lefevre <vincent@vinc17.net>
2024-03-21 16:12:32 +0100

    [src/sub1sp1_extracted.c] Corrected typo from the latest commit.

--- src/sub1sp1_extracted.c
+++ src/sub1sp1_extracted.c
@@ -24,7 +24,7 @@ along with the GNU MPFR Library; see the file COPYING.LESSER.  If not, see
 https://www.gnu.org/licenses/ or write to the Free Software Foundation, Inc.,
 51 Franklin St, Fifth Floor, Boston, MA 02110-1301, USA. */
 
-/* TODO: For constistency with add1sp1_extracted.c, use #define's
+/* TODO: For consistency with add1sp1_extracted.c, use #define's
    instead of <stdint.h>? */
 #include <stdint.h>
 
