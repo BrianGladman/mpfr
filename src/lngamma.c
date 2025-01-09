BrianGladman/mpfr

commit 29f957dddc4ffedd628e4b775aacc3f177ba41c0

in mpfr_lgamma, allow signp to be a NULL pointer
Rationale: we can now write mpfr_lgamma (y, NULL, x, rnd)
to compute the equivalent of the C lgamma function, without
having to declare a variable of type int and pass a pointer
to it (with a non NULL argument, mpfr_lgamma is equivalent to
the C function lgamma_r).

src/lngamma.c
@@ -760,13 +760,14 @@ int
 mpfr_lgamma (mpfr_ptr y, int *signp, mpfr_srcptr x, mpfr_rnd_t rnd)
 {
   int inex;
+  int sgn = 1; /* most common case */
 
+  /* when signp=NULL, we print 0 in MPFR_LOG_FUNC */
   MPFR_LOG_FUNC
     (("x[%Pd]=%.*Rg rnd=%d", mpfr_get_prec (x), mpfr_log_prec, x, rnd),
      ("y[%Pd]=%.*Rg signp=%d inexact=%d",
-      mpfr_get_prec (y), mpfr_log_prec, y, *signp, inex));
-
-  *signp = 1;  /* most common case */
+      mpfr_get_prec (y), mpfr_log_prec, y,
+      (signp == NULL) ? 0 : *signp, inex));
 
   if (MPFR_UNLIKELY (MPFR_IS_SINGULAR (x)))
     {
@@ -779,7 +780,8 @@ mpfr_lgamma (mpfr_ptr y, int *signp, mpfr_srcptr x, mpfr_rnd_t rnd)
         {
           if (MPFR_IS_ZERO (x))
             MPFR_SET_DIVBY0 ();
-          *signp = MPFR_INT_SIGN (x);
+          if (signp != NULL)
+            *signp = MPFR_INT_SIGN (x);
           MPFR_SET_INF (y);
           MPFR_SET_POS (y);
           MPFR_RET (0);
@@ -797,7 +799,7 @@ mpfr_lgamma (mpfr_ptr y, int *signp, mpfr_srcptr x, mpfr_rnd_t rnd)
         }
 
       if (unit_bit (x) == 0)
-        *signp = -1;
+        sgn = -1;
 
       /* For tiny negative x, we have gamma(x) = 1/x - euler + O(x),
          thus |gamma(x)| = -1/x + euler + O(x), and
@@ -865,6 +867,10 @@ mpfr_lgamma (mpfr_ptr y, int *signp, mpfr_srcptr x, mpfr_rnd_t rnd)
     }
 
   inex = mpfr_lngamma_aux (y, x, rnd);
+
+  if (signp != NULL)
+    *signp = sgn;
+
   return inex;
 }
 

doc/mpfr.texi
@@ -2634,7 +2634,8 @@ See also @code{mpfr_lgamma}.
 
 @deftypefun int mpfr_lgamma (mpfr_t @var{rop}, int *@var{signp}, const mpfr_t @var{op}, mpfr_rnd_t @var{rnd})
 Set @var{rop} to the value of the logarithm of the absolute value of the
-Gamma function on @var{op}, rounded in the direction @var{rnd}. The sign
+Gamma function on @var{op}, rounded in the direction @var{rnd}.
+If @var{signp} is not @code{NULL}, the sign
 (1 or @minus{}1) of Gamma(@var{op}) is returned in the object pointed to
 by @var{signp}.
 When @var{op} is 1 or 2, set @var{rop} to @mm{+}0 (in all rounding modes).

tests/tlgamma.c
@@ -129,6 +129,15 @@ special (void)
       exit (1);
     }
 
+  /* test with signp=NULL */
+  mpfr_set_ui (x, 2, MPFR_RNDN);
+  mpfr_lgamma (y, NULL, x, MPFR_RNDN);
+  if (MPFR_IS_NAN (y) || mpfr_cmp_ui (y, 0) || MPFR_IS_NEG (y))
+    {
+      printf ("Error for lgamma(2) with signp=NULL\n");
+      exit (1);
+    }
+
   mpfr_set_prec (x, 53);
   mpfr_set_prec (y, 53);
 

commit 947699782c3cb07a3c10f5942adf35af24065917

[src/lngamma.c] Fixed bug in mpfr_lgamma after support for signp = NULL.
Bug introduced in commit 29f957dddc4ffedd628e4b775aacc3f177ba41c0.
There was a case (tiny negative x) where *signp was no longer set.

src/lngamma.c
@@ -853,6 +853,8 @@ mpfr_lgamma (mpfr_ptr y, int *signp, mpfr_srcptr x, mpfr_rnd_t rnd)
               if (ok)
                 {
                   MPFR_SAVE_EXPO_FREE (expo);
+                  if (signp != NULL)
+                    *signp = sgn;
                   return mpfr_check_range (y, inex, rnd);
                 }
               /* if ulp(log(-x)) <= |x| there is no reason to loop,