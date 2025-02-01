241761dc91278cf221cc7f76922470f23e1e78d8
Vincent Lefevre <vincent@vinc17.net>
2025-02-02 00:49:02 +0100

    [tools/check_mparam.c] Updated the "Contributed" lines.

--- tools/check_mparam.c
+++ tools/check_mparam.c
@@ -1,7 +1,7 @@
 /* check_mparam - check a mparam.h file
 
 Copyright 2018-2025 Free Software Foundation, Inc.
-Contributed by the Arenaire and Caramel projects, INRIA.
+Contributed by the Pascaline and Caramba projects, INRIA.
 
 This program is free software; you can redistribute it and/or modify
 it under the terms of the GNU Lesser General Public License as published by
