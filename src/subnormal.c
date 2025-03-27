602226ca6744b0595fd1c1944befc8b59b5f0c55
Vincent Lefevre <vincent@vinc17.net>
2025-03-27 17:47:04 +0100

    [src/subnormal.c] Restored the FIXME, with a correction.
    In commit 0a32b92cc89b720903a7d23166af51f6d2c503a6 (in both the commit
    log and the FIXME), I actually meant "smallest positive normal number"
    instead of "smallest positive subnormal".

--- src/subnormal.c
+++ src/subnormal.c
@@ -42,17 +42,33 @@ If not, see <https://www.gnu.org/licenses/>. */
    https://sympa.inria.fr/sympa/arc/mpfr/2009-06/msg00008.html
    https://sympa.inria.fr/sympa/arc/mpfr/2009-06/msg00010.html
 
-   1. Assume that the smallest positive subnormal is obtained from below
+   FIXME: This specification for the underflow flag is a bad idea because
+   it yields an inconsistency, as described below, assuming rounding to
+   nearest or toward +infinity (or away from zero).
+
+   1. Assume that the smallest positive normal number is obtained from below
    during the first rounding. Due to the choice of the exponent range, MPFR
-   will not raise the underflow flag, and mpfr_subnormalize will do nothing
-   in this case.
+   will not raise the underflow flag (note that this is not due to the fact
+   that MPFR considers underflow after rounding, i.e. this would be the
+   same behavior if MPFR were considering underflow before rounding), and
+   mpfr_subnormalize will do nothing in this case. So the underflow flag
+   will not be set. The behavior in this case corresponds to underflow
+   after rounding.
 
    2. Now assume that the value obtained after the first rounding is the
-   MPFR number preceding the smallest positive subnormal. This number has
-   one more bit than required (it has p bits "1", while just below the
-   smallest positive subnormal, the actual precision is p-1), so that
-   mpfr_subnormalize will round it to the smallest positive subnormal.
-   Since the first rounding was subnormal, the underflow flag will be set.
+   MPFR number preceding the smallest positive normal number. This number
+   has one more bit than required (it has p bits "1", while just below the
+   smallest positive normal number, the actual precision is p-1), so that
+   mpfr_subnormalize will round it to the smallest positive normal number,
+   and due to this rounding, according to the above specification of
+   mpfr_subnormalize, the underflow flag will be set, which corresponds
+   to underflow before rounding.
+
+   In short, we have neither underflow after rounding nor underflow before
+   rounding, but a mix of them. We can change the specification to emulate
+   either of them, but the most efficient choice would be after rounding
+   (the current "do nothing" case would still be fast). If need be, we
+   could add mpfr_subnormalize_br to emulate underflow before rounding.
 */
 
 int
